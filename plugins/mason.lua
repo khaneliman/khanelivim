cc36d49e6013ccb6e8c5391120b3e65f56608857
Austin Horstman <khaneliman12@gmail.com>
2023-02-12 20:53:23 -0600

    rename sumneko to lua breaking change fix

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,69 +1,69 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = {
-				"angularls",
-				"bashls",
-				"clangd",
-				"csharp_ls",
-				"cssls",
-				"dockerls",
-				"eslint",
-				"grammarly",
-				"html",
-				"intelephense",
-				"jsonls",
-				"lemminx",
-				"marksman",
-				"neocmake",
-				"omnisharp",
-				"pyright",
-				"sqls",
-				"sumneko_lua",
-				"tsserver",
-				"yamlls",
-			}
-			return opts
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = {
-				"black",
-				"cbfmt",
-				"clang_format",
-				"csharpier",
-				"eslint_d",
-				"gitlint",
-				"hadolint",
-				"isort",
-				"jq",
-				"markdownlint",
-				"prettierd",
-				"shellcheck",
-				"shfmt",
-				"sql_formatter",
-				"stylua",
-			}
-			return opts
-		end,
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		opts = function(_, opts)
-			opts.ensure_installed = {
-				"bash",
-				"codelldb",
-				"coreclr",
-				"cppdbg",
-				"firefox",
-				"node2",
-				"python",
-				"js",
-			}
-			return opts
-		end,
-	},
+		{
+				"williamboman/mason-lspconfig.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = {
+							"angularls",
+							"bashls",
+							"clangd",
+							"csharp_ls",
+							"cssls",
+							"dockerls",
+							"eslint",
+							"grammarly",
+							"html",
+							"intelephense",
+							"jsonls",
+							"lemminx",
+							"marksman",
+							"neocmake",
+							"omnisharp",
+							"pyright",
+							"sqls",
+							"lua_ls",
+							"tsserver",
+							"yamlls",
+					}
+					return opts
+				end,
+		},
+		{
+				"jay-babu/mason-null-ls.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = {
+							"black",
+							"cbfmt",
+							"clang_format",
+							"csharpier",
+							"eslint_d",
+							"gitlint",
+							"hadolint",
+							"isort",
+							"jq",
+							"markdownlint",
+							"prettierd",
+							"shellcheck",
+							"shfmt",
+							"sql_formatter",
+							"stylua",
+					}
+					return opts
+				end,
+		},
+		{
+				"jay-babu/mason-nvim-dap.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = {
+							"bash",
+							"codelldb",
+							"coreclr",
+							"cppdbg",
+							"firefox",
+							"node2",
+							"python",
+							"js",
+					}
+					return opts
+				end,
+		},
 }
