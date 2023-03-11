41996043028ae28786aba7d0d6e3b80fd8d6d7f3
Austin Horstman <khaneliman12@gmail.com>
2023-03-11 13:23:05 -0600

    fix alpha command hint

--- plugins/alpha.lua
+++ plugins/alpha.lua
@@ -22,7 +22,7 @@ return {
 			utils.alpha_button("LDR f f", "  Find File  "),
 			utils.alpha_button("LDR f o", "  Recents  "),
 			utils.alpha_button("LDR f p", "P  Projects  "),
-			utils.alpha_button("LDR f n", "  New File  "),
+			utils.alpha_button("LDR   n", "  New File  "),
 			utils.alpha_button("LDR S l", "  Last Session  "),
 		}
 
