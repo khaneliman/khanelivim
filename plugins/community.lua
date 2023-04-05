6239916896f8bcfb2d4c6db5fe0a23468b7700d5
Austin Horstman <khaneliman12@gmail.com>
2023-04-05 12:59:09 -0500

    copilot update

--- plugins/community.lua
+++ plugins/community.lua
@@ -90,6 +90,22 @@ return {
 		},
 	},
 	{
+	 -- further customize the options set by the community
+		"copilot.lua",
+		opts = {
+			suggestion = {
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-.>",
+					prev = "<C-,>",
+					dismiss = "<C/>",
+				},
+			},
+		},
+	},
+	{
 		"echasnovski/mini.surround",
 		keys = {
 			{ 's', desc = "Surround" },
