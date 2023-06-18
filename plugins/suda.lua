d80871a7a537ccf5d102986691fd51e628078836
Austin Horstman <khaneliman12@gmail.com>
2023-06-18 17:34:59 -0500

    add suda write keymap

--- plugins/suda.lua
+++ plugins/suda.lua
@@ -1,5 +1,12 @@
 return {
   "lambdalisue/suda.vim",
+  keys = {
+    {
+      "<leader>W",
+      ":SudaWrite<CR>",
+      desc = "Suda Write",
+    },
+  },
   cmd = {
     "SudaRead",
     "SudaWrite",
