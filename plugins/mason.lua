92d515204414c5565b05731976518d4449a3c851
Austin Horstman <khaneliman12@gmail.com>
2023-06-18 09:29:39 -0500

    remove mason config stuff that is handled from community language pack

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -12,7 +12,6 @@ return {
 				"dockerls",
 				"grammarly",
 				"html",
-				"intelephense",
 				"jsonls",
 				"lemminx",
 				"lua_ls",
@@ -45,27 +44,6 @@ return {
 				"rustfmt",
 				"shfmt",
 				"sql_formatter",
-			}
-			opts.setup_handlers = {
-				prettierd = function()
-					require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with({
-						condition = function(utils)
-							return utils.root_has_file("package.json")
-									or utils.root_has_file(".prettierrc")
-									or utils.root_has_file(".prettierrc.json")
-									or utils.root_has_file(".prettierrc.js")
-						end,
-					}))
-				end,
-				eslint_d = function()
-					require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with({
-						condition = function(utils)
-							return utils.root_has_file("package.json")
-									or utils.root_has_file(".eslintrc.json")
-									or utils.root_has_file(".eslintrc.js")
-						end,
-					}))
-				end,
 			}
 			return opts
 		end,
