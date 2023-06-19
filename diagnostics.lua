5bbe62ca990b98ffa66c85e4651ad6f50b64f75f
Austin Horstman <khaneliman12@gmail.com>
2023-06-19 14:56:20 -0500

    various tweaks

--- diagnostics.lua
+++ diagnostics.lua
@@ -1,2 +1,2 @@
 -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-return { virtual_text = false, underline = true }
+return { virtual_text = false, underline = true, update_in_insert = false }
---
+++ lazy.lua
@@ -0,0 +1,1 @@
+return { diff = { cmd = "terminal_git" }, checker = { enabled = true } }
