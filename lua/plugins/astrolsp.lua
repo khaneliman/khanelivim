9041154f7a8952a8d5929f12f61999221da6816c
Austin Horstman <khaneliman12@gmail.com>
2023-09-22 09:06:34 -0500

    fix: lsp config

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -5,7 +5,30 @@ return {
     ---@type AstroLSPOpts
     opts = {
       config = {
+        clangd = { settings = { capabilities = { offsetEncoding = "utf-8" } } },
+        julials = { autostart = false },
         lua_ls = { settings = { Lua = { hint = { enable = true, arrayIndex = "Disable" } } } },
+        nil_ls = {
+          settings = {
+            ["nil"] = {
+              formatting = {
+                command = { "nixpkgs-fmt" },
+              },
+            },
+          },
+        },
+        taplo = { evenBetterToml = { schema = { catalogs = { "https://www.schemastore.org/api/json/catalog.json" } } } },
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              completion = {
+                postfix = {
+                  enable = false,
+                },
+              },
+            },
+          },
+        },
       },
       features = {
         inlay_hints = true,
@@ -33,6 +56,32 @@ return {
             vim.api.nvim_del_augroup_by_id(augroup)
           end
         end,
+      })
+    end,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+    dependencies = {
+      "AstroNvim/astrolsp",
+      opts = { handlers = { tsserver = false } },
+    },
+    opts = function()
+      return require("astrocore").extend_tbl(require("astrolsp").lsp_opts "tsserver", {
+        settings = {
+          tsserver_path = require("mason-registry").get_package("typescript-language-server"):get_install_path()
+            .. "/node_modules/typescript/lib/tsserver.js",
+          tsserver_file_preferences = {
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          },
+        },
       })
     end,
   },
