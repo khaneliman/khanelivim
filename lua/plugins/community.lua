38e9bca0458d76d60c13e523a0c15457b5d3d2c4
Austin Horstman <khaneliman12@gmail.com>
2023-09-22 19:41:36 -0500

    fix: remove duplicated mini.surround

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -8,22 +8,4 @@ return {
       disabled_filetypes = { "help" },
     },
   },
-  {
-    -- further customize the options set by the community
-    "echasnovski/mini.surround",
-    keys = {
-      { "s", desc = "Surround" },
-    },
-    opts = {
-      mappings = {
-        add = "s" .. "a", -- Add surrounding in Normal and Visual modes
-        delete = "s" .. "d", -- Delete surrounding
-        find = "s" .. "f", -- Find surrounding (to the right)
-        find_left = "s" .. "F", -- Find surrounding (to the left)
-        highlight = "s" .. "h", -- Highlight surrounding
-        replace = "s" .. "r", -- Replace surrounding
-        update_n_lines = "s" .. "n", -- Update `n_lines`
-      },
-    },
-  },
 }
