cb58948684d70230af15172acad10aaaf7a23568
Austin Horstman <khaneliman12@gmail.com>
2023-09-22 19:50:23 -0500

    refactor: self maintain smartcolumn

--- lua/plugins/smartcolumn.lua
+++ lua/plugins/smartcolumn.lua
@@ -1,8 +1,5 @@
 return {
-  -- further customize the options set by the community
   "m4xshen/smartcolumn.nvim",
-  opts = {
-    colorcolumn = 120,
-    disabled_filetypes = { "help" },
-  },
+  event = { "InsertEnter", "User AstroFile" },
+  opts = {},
 }
