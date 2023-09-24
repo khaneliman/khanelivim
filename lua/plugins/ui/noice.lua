8f5bd11aa7c4d5bca3af4e0981e18538ce4686e9
Austin Horstman <khaneliman12@gmail.com>
2023-09-23 23:47:29 -0500

    refactor: noice handle all

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -21,11 +21,28 @@ return {
           },
           format_done = {},
         },
-        hover = { enabled = false },
-        signature = { enabled = false, auto_open = { enabled = false } },
+        hover = {
+          enabled = true,
+          silent = false, -- set to true to not show a message if hover is not available
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
+        },
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+            throttle = 50, -- Debounce lsp signature help request by 50ms
+          },
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
+        },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = false,
+          ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
       },
