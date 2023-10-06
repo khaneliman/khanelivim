94a3e91ebeecab9a3aeb0ab8ad8d842fef8137e6
Austin Horstman <khaneliman12@gmail.com>
2023-10-05 19:31:17 -0500

    refactor: null-ls -> none-ls

--- lua/plugins/language-support/nix.lua
+++ lua/plugins/language-support/nix.lua
@@ -14,7 +14,7 @@ return {
     opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "nil_ls") end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require "null-ls"
       if type(opts.sources) == "table" then
