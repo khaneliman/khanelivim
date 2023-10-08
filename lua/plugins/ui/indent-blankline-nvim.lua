3720c0aae7c67a74c3678bf9b2ac61880fa3a877
Austin Horstman <khaneliman12@gmail.com>
2023-10-08 09:28:14 -0500

    refactor: blankline opts instead of vim.g

--- lua/plugins/ui/indent-blankline-nvim.lua
+++ lua/plugins/ui/indent-blankline-nvim.lua
@@ -1,10 +1,16 @@
-vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" }
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_current_context = false
-
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "User AstroFile",
-  opts = {},
+  indent = {
+    char = "│",
+  },
+  scope = {
+    enabled = false,
+  },
+  exclude = {
+    filetypes = { "help", "alpha", "dashboard", "Trouble", "lazy", "neo-tree" },
+  },
+  whitespace = {
+    remove_blankline_trail = true,
+  },
 }
