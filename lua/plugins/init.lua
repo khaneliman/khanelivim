879d14bdf7015f42261f05a40f20d42f9aee641f
Austin Horstman <khaneliman12@gmail.com>
2023-10-08 09:23:12 -0500

    refactor: move peek to separate file and switch to fork

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,20 +19,6 @@ return {
   { "jabirali/vim-tmux-yank", event = "User AstroFile" }, --tmux yank integration
   { "m-demare/hlargs.nvim", opts = {}, event = "User AstroFile" },
   {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup()
-      -- refer to `configuration to change defaults`
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-    cmd = {
-      "PeekOpen",
-      "PeekClose",
-    },
-  },
-  {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     ft = "markdown",
