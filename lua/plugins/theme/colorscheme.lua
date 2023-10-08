aa0d5f34ff352c05dfcfaa846eca2fea5a97c746
Austin Horstman <khaneliman12@gmail.com>
2023-10-07 23:56:32 -0500

    chore: clean up unused colorscheme opts

--- lua/plugins/theme/colorscheme.lua
+++ lua/plugins/theme/colorscheme.lua
@@ -3,13 +3,5 @@ return {
   ---@type AstroUIOpts
   opts = {
     colorscheme = "catppuccin",
-    style = {
-      inactive = true, -- Bool value, toggles inactive window color.
-      transparent = false, -- Bool value, toggles transperency.
-      floating = true, -- Bool value, toggles floating windows background colors.
-      popup = true, -- Bool value, toggles popup background color.
-      neotree = false, -- Bool value, toggles neo-trees background color.
-      italic_comments = true, -- Bool value, toggles italic comments.
-    },
   },
 }
