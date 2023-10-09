d634ed5febafd34ab95a6bcd9e4adca5b53d27ca
Austin Horstman <khaneliman12@gmail.com>
2023-10-08 19:04:39 -0500

    refactor: move git treesitter to language support

--- lua/plugins/language-support/git.lua
+++ lua/plugins/language-support/git.lua
@@ -1,17 +1,14 @@
--- Example customization of Treesitter
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = { "RRethy/nvim-treesitter-endwise" },
   opts = function(_, opts)
     local astrocore = require "astrocore"
-    opts.endwise = { enable = true }
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = astrocore.list_insert_unique(opts.ensure_installed, {
-      "fish",
-      "llvm",
-      "rasi",
-      "regex",
-      "vim",
+      "diff",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
     })
   end,
 }
