b91da5c1c319998ca5c055ab782a70edb5f77d38
Austin Horstman <khaneliman12@gmail.com>
2023-10-19 22:34:44 -0500

    feat: css config

---
+++ lua/plugins/language-support/css.lua
@@ -0,0 +1,36 @@
+local utils = require "astrocore"
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "css" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "cssls", "cssmodules_ls", "emmet_ls" })
+    end,
+  },
+  {
+    "luckasRanarison/nvim-devdocs",
+    opts = {
+      ensure_installed = { "css" },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    opts = {
+      config = {
+        cssmodules_ls = {
+          capabilities = {
+            definitionProvider = false,
+          },
+        },
+      },
+    },
+  },
+}
