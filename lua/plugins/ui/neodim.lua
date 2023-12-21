e22834169d53875dc483b0416d517c7eca685719
Austin Horstman <khaneliman12@gmail.com>
2023-12-21 09:25:58 -0600

    refactor: neodim blend color

--- lua/plugins/ui/neodim.lua
+++ lua/plugins/ui/neodim.lua
@@ -2,16 +2,24 @@ return {
   "zbirenbaum/neodim",
   event = "LspAttach",
   opts = {
+    refresh_delay = 75,
     alpha = 0.75,
-    blend_color = "#000000",
     update_in_insert = {
       enable = true,
       delay = 100,
     },
+    blend_color = "#24273a",
     hide = {
+      underline = true,
       virtual_text = true,
       signs = true,
-      underline = true,
     },
+    regex = {
+      "[uU]nused",
+      "[nN]ever [rR]ead",
+      "[nN]ot [rR]ead",
+    },
+    priority = 128,
+    disable = {},
   },
 }
