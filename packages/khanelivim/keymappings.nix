5e237bfc1f969acd6020dba8c49e4e914722fb76
Austin Horstman <khaneliman12@gmail.com>
2024-08-05 14:01:24 -0500

    keymappings: fix diagnostics toggle

--- packages/khanelivim/keymappings.nix
+++ packages/khanelivim/keymappings.nix
@@ -193,9 +193,9 @@
                   function ()
                     vim.b.disable_diagnostics = not vim.b.disable_diagnostics
                     if vim.b.disable_diagnostics then
-                      vim.diagnostic.hide()
+                      vim.diagnostic.disable(0)
                     else
-                      vim.diagnostic.show()
+                      vim.diagnostic.enable(0)
                     end
                     vim.notify(string.format("Buffer Diagnostics %s", bool2str(not vim.b.disable_diagnostics), "info"))
                   end'';
@@ -210,9 +210,9 @@
                   function ()
                     vim.g.disable_diagnostics = not vim.g.disable_diagnostics
                     if vim.g.disable_diagnostics then
-                      vim.diagnostic.hide()
+                      vim.diagnostic.disable()
                     else
-                      vim.diagnostic.show()
+                      vim.diagnostic.enable()
                     end
                     vim.notify(string.format("Global Diagnostics %s", bool2str(not vim.g.disable_diagnostics), "info"))
                   end'';
