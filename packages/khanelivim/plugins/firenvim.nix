cac677c4f18e384d893daa2ef57fc181d349bd92
Austin Horstman <khaneliman12@gmail.com>
2024-08-13 14:07:35 -0500

    plugins/firenvim: add localSettings

--- packages/khanelivim/plugins/firenvim.nix
+++ packages/khanelivim/plugins/firenvim.nix
@@ -2,6 +2,18 @@ _: {
   plugins = {
     firenvim = {
       enable = true;
+
+      settings = {
+        localSettings = {
+          ".*" = {
+            cmdline = "neovim";
+            content = "text";
+            priority = 0;
+            selector = "textarea";
+            takeover = "always";
+          };
+        };
+      };
     };
   };
 }
