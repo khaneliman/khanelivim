d525630b3a8474b37d24de22c47102280de8fc77
Austin Horstman <khaneliman12@gmail.com>
2024-08-29 22:41:36 -0500

    plugins/telescope: undo history styling

--- packages/khanelivim/plugins/telescope.nix
+++ packages/khanelivim/plugins/telescope.nix
@@ -135,6 +135,13 @@
 
       undo = {
         enable = true;
+        settings = {
+          side_by_side = true;
+          layout_strategy = "vertical";
+          layout_config = {
+            preview_height = 0.8;
+          };
+        };
       };
     };
 
