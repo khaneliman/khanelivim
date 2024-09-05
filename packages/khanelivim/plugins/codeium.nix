a19b2f194c60d018800f7144124e47743c8eeb29
Austin Horstman <khaneliman12@gmail.com>
2024-09-05 09:55:51 -0500

    plugins/codeium: fix deprecations

--- packages/khanelivim/plugins/codeium.nix
+++ packages/khanelivim/plugins/codeium.nix
@@ -9,15 +9,15 @@
     codeium-nvim = {
       enable = true;
 
-      extraOptions = {
+      settings = {
         enable_chat = true;
-      };
 
-      tools = {
-        curl = lib.getExe pkgs.curl;
-        gzip = lib.getExe pkgs.gzip;
-        uname = lib.getExe' pkgs.coreutils "uname";
-        uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+        tools = {
+          curl = lib.getExe pkgs.curl;
+          gzip = lib.getExe pkgs.gzip;
+          uname = lib.getExe' pkgs.coreutils "uname";
+          uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+        };
       };
     };
   };
