fbc004758de53f6b4a67e37e51fa8701b2cc9ccb
Austin Horstman <khaneliman12@gmail.com>
2024-09-08 21:43:36 -0500

    plugins/rest: add file example

--- packages/khanelivim/plugins/rest.nix
+++ packages/khanelivim/plugins/rest.nix
@@ -1,5 +1,13 @@
 {
   plugins.rest.enable = true;
 
+  # NOTE: Works with files with .http file extension / http filetype
+  # More example: https://github.com/rest-nvim/rest.nvim/tree/main/spec/examples
+  # File shape:
+  # Method Request-URI HTTP-Version
+  # Header-field: Header-value
+  #
+  # Request-Body
+
   # TODO: Add keymaps
 }
