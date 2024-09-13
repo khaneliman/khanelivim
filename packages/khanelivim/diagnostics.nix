459b49a0074b0097eafccdb947e833e5442b2936
Austin Horstman <khaneliman12@gmail.com>
2024-09-13 14:53:12 -0500

    diagnostics: add border and prioritise severity

--- packages/khanelivim/diagnostics.nix
+++ packages/khanelivim/diagnostics.nix
@@ -1,5 +1,12 @@
 {
   diagnostics = {
     update_in_insert = true;
+    severity_sort = true;
+    float = {
+      border = "rounded";
+    };
+    jump = {
+      severity.__raw = "vim.diagnostic.severity.WARN";
+    };
   };
 }
