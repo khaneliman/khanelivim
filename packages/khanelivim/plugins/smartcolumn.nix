22b83ded21b7a08264611a176635348836445fb2
Austin Horstman <khaneliman12@gmail.com>
2024-09-15 18:12:46 -0500

    plugins/smartcolumn: use new module

--- packages/khanelivim/plugins/smartcolumn.nix
+++ packages/khanelivim/plugins/smartcolumn.nix
@@ -1,29 +1,36 @@
-{ pkgs, ... }:
 {
-  extraPlugins = with pkgs.vimPlugins; [ smartcolumn-nvim ];
-
-  extraConfigLuaPre = ''
-    require("smartcolumn").setup({
-      colorcolumn = "80",
-
-      disabled_filetypes = {
-        "help",
-        "text",
-        "markdown",
-        "neo-tree",
-        "checkhealth",
-        "lspinfo",
-        "noice",
-      },
-
+  plugins.smartcolumn = {
+    enable = true;
+    settings = {
+      colorcolumn = "80";
+      disabled_filetypes = [
+        "checkhealth"
+        "help"
+        "lspinfo"
+        "markdown"
+        "neo-tree"
+        "noice"
+        "text"
+      ];
       custom_colorcolumn = {
-        go = {"100", "130"},
-        java = { "100", "140" },
-        nix = { "100", "120" },
-        rust = { "80", "100" },
-      },
-
-      scope = "file",
-    })
-  '';
+        go = [
+          "100"
+          "130"
+        ];
+        java = [
+          "100"
+          "140"
+        ];
+        nix = [
+          "100"
+          "120"
+        ];
+        rust = [
+          "80"
+          "100"
+        ];
+      };
+      scope = "file";
+    };
+  };
 }
