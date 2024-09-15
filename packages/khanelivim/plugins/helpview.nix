30b21e230abf34d3238740275ad288698dd55e6b
Austin Horstman <khaneliman12@gmail.com>
2024-09-15 18:14:12 -0500

    plugins/helpview: init

---
+++ packages/khanelivim/plugins/helpview.nix
@@ -0,0 +1,21 @@
+{
+  plugins.helpview = {
+    enable = true;
+    settings = {
+
+      buf_ignore = [ ];
+
+      mode = [
+        "n"
+        "x"
+        "i"
+        "r"
+      ];
+
+      hybrid_modes = [
+        "i"
+        "r"
+      ];
+    };
+  };
+}
