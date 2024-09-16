281c735d3287dd831c40b07ed63a7c386087c45f
Austin Horstman <khaneliman12@gmail.com>
2024-09-16 11:39:39 -0500

    plugins/lsp: add border

--- packages/khanelivim/plugins/lsp.nix
+++ packages/khanelivim/plugins/lsp.nix
@@ -32,6 +32,11 @@
       local params = vim.lsp.util.make_position_params()
       return vim.lsp.buf_request(0, 'textDocument/typeDefinition', params, preview_location_callback)
     end
+
+    require('lspconfig.ui.windows').default_options = {
+      border = "rounded"
+    }
+
   '';
 
   autoCmd = [
