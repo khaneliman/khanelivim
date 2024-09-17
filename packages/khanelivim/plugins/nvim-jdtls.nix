2e866e6b8473eb8d8e7a8920fc7cc4ea0d707f54
Austin Horstman <khaneliman12@gmail.com>
2024-09-17 12:50:20 -0500

    plugins/nvim-jdtls: remove unnecessary packaging

--- packages/khanelivim/plugins/nvim-jdtls.nix
+++ packages/khanelivim/plugins/nvim-jdtls.nix
@@ -1,25 +1,30 @@
 { pkgs, ... }:
 {
-  extraConfigLuaPre = ''
-    local jdtls = require("jdtls")
-    local jdtls_dap = require("jdtls.dap")
-    local jdtls_setup = require("jdtls.setup")
+  extraConfigLuaPre =
+    let
+      java-debug = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server";
+      java-test = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server";
+    in
+    ''
+      local jdtls = require("jdtls")
+      local jdtls_dap = require("jdtls.dap")
+      local jdtls_setup = require("jdtls.setup")
 
-    _M.jdtls = {}
-    _M.jdtls.bundles = {}
+      _M.jdtls = {}
+      _M.jdtls.bundles = {}
 
-    local java_debug_bundle = vim.split(vim.fn.glob("${pkgs.khanelivim.java-debug}" .. "/*.jar"), "\n")
-    local java_test_bundle = vim.split(vim.fn.glob("${pkgs.khanelivim.java-test}" .. "/*.jar", true), "\n")
+      local java_debug_bundle = vim.split(vim.fn.glob("${java-debug}" .. "/*.jar"), "\n")
+      local java_test_bundle = vim.split(vim.fn.glob("${java-test}" .. "/*.jar", true), "\n")
 
-    -- add jars to the bundle list if there are any
-    if java_debug_bundle[1] ~= "" then
-        vim.list_extend(_M.jdtls.bundles, java_debug_bundle)
-    end
+      -- add jars to the bundle list if there are any
+      if java_debug_bundle[1] ~= "" then
+          vim.list_extend(_M.jdtls.bundles, java_debug_bundle)
+      end
 
-    if java_test_bundle[1] ~= "" then
-        vim.list_extend(_M.jdtls.bundles, java_test_bundle)
-    end
-  '';
+      if java_test_bundle[1] ~= "" then
+          vim.list_extend(_M.jdtls.bundles, java_test_bundle)
+      end
+    '';
 
   plugins = {
     nvim-jdtls = {
@@ -81,7 +86,12 @@
           format = {
             enabled = true;
             settings = {
-              url = "${pkgs.khanelivim.eclipse-java-google-style}";
+              url = "${
+                (pkgs.fetchurl {
+                  url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml";
+                  sha256 = "sha256-51Uku2fj/8iNXGgO11JU4HLj28y7kcSgxwjc+r8r35E=";
+                })
+              }";
               profile = "GoogleStyle";
             };
           };
