05a2a0e01fba78599161c71a114e1349fbaee00c
Austin Horstman <khaneliman12@gmail.com>
2024-09-18 09:34:59 -0500

    plugins/package-info: init

---
+++ packages/khanelivim/plugins/package-info.nix
@@ -0,0 +1,19 @@
+{ config, lib, ... }:
+{
+  plugins.package-info = {
+    enable = true;
+    enableTelescope = true;
+  };
+
+  keymaps = lib.mkIf (config.plugins.telescope.enable && config.plugins.package-info.enable) [
+    {
+      mode = "n";
+      key = "<leader>fP";
+      action = ":Telescope package_info<CR>";
+      options = {
+        desc = "Find package info";
+        silent = true;
+      };
+    }
+  ];
+}
