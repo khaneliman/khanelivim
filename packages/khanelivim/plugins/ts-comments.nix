59899c47f1171e94a0afc29482d342c8b9cd86c2
Austin Horstman <khaneliman12@gmail.com>
2024-09-17 20:50:59 -0500

    plugins/ts-comments: add lang config
    Just adding to fill out the module and make it obvious how it works

--- packages/khanelivim/plugins/ts-comments.nix
+++ packages/khanelivim/plugins/ts-comments.nix
@@ -1,5 +1,76 @@
 {
   plugins.ts-comments = {
     enable = true;
+
+    settings = {
+      lang = {
+        astro = "<!-- %s -->";
+        axaml = "<!-- %s -->";
+        blueprint = "// %s";
+        c = "// %s";
+        c_sharp = "// %s";
+        clojure = [
+          ";; %s"
+          "; %s"
+        ];
+        cpp = "// %s";
+        cs_project = "<!-- %s -->";
+        cue = "// %s";
+        fsharp = "// %s";
+        fsharp_project = "<!-- %s -->";
+        gleam = "// %s";
+        glimmer = "{{! %s }}";
+        graphql = "# %s";
+        handlebars = "{{! %s }}";
+        hcl = "# %s";
+        html = "<!-- %s -->";
+        hyprlang = "# %s";
+        ini = "; %s";
+        ipynb = "# %s";
+        javascript = {
+          __unkeyed-1 = "// %s";
+          __unkeyed-2 = "/* %s */";
+          call_expression = "// %s";
+          jsx_attribute = "// %s";
+          jsx_element = "{/* %s */}";
+          jsx_fragment = "{/* %s */}";
+          spread_element = "// %s";
+          statement_block = "// %s";
+        };
+        nix = "# %s";
+        php = "// %s";
+        rego = "# %s";
+        rescript = "// %s";
+        rust = [
+          "// %s"
+          "/* %s */"
+        ];
+        sql = "-- %s";
+        styled = "/* %s */";
+        svelte = "<!-- %s -->";
+        templ = {
+          __unkeyed-1 = "// %s";
+          component_block = "<!-- %s -->";
+        };
+        terraform = "# %s";
+        tsx = {
+          __unkeyed-1 = "// %s";
+          __unkeyed-2 = "/* %s */";
+          call_expression = "// %s";
+          jsx_attribute = "// %s";
+          jsx_element = "{/* %s */}";
+          jsx_fragment = "{/* %s */}";
+          spread_element = "// %s";
+          statement_block = "// %s";
+        };
+        twig = "{# %s #}";
+        typescript = [
+          "// %s"
+          "/* %s */"
+        ];
+        vue = "<!-- %s -->";
+        xaml = "<!-- %s -->";
+      };
+    };
   };
 }
