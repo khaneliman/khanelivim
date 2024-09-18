5466d8d4163495e3bf4335b07aa52397198b9c86
Austin Horstman <khaneliman12@gmail.com>
2024-09-18 08:39:36 -0500

    plugins/mini-bufremove: fix keymap condition

--- packages/khanelivim/plugins/mini-bufremove.nix
+++ packages/khanelivim/plugins/mini-bufremove.nix
@@ -10,48 +10,50 @@
     };
   };
 
-  keymaps = lib.mkIf (lib.hasAttr "bufremove" config.plugins.mini.modules) [
-    {
-      mode = "n";
-      key = "<leader>c";
-      action.__raw = ''require("mini.bufremove").delete'';
-      options = {
-        desc = "Close buffer";
-        silent = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<C-w>";
-      action.__raw = ''require("mini.bufremove").delete'';
-      options = {
-        desc = "Close buffer";
-        silent = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>bc";
-      action.__raw = ''
-        function ()
-          local current = vim.api.nvim_get_current_buf()
+  keymaps =
+    lib.mkIf (config.plugins.mini.enable && lib.hasAttr "bufremove" config.plugins.mini.modules)
+      [
+        {
+          mode = "n";
+          key = "<leader>c";
+          action.__raw = ''require("mini.bufremove").delete'';
+          options = {
+            desc = "Close buffer";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<C-w>";
+          action.__raw = ''require("mini.bufremove").delete'';
+          options = {
+            desc = "Close buffer";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bc";
+          action.__raw = ''
+            function ()
+              local current = vim.api.nvim_get_current_buf()
 
-          local get_listed_bufs = function()
-            return vim.tbl_filter(function(bufnr)
-             return vim.api.nvim_buf_get_option(bufnr, "buflisted")
-            end, vim.api.nvim_list_bufs())
-          end
+              local get_listed_bufs = function()
+                return vim.tbl_filter(function(bufnr)
+                 return vim.api.nvim_buf_get_option(bufnr, "buflisted")
+                end, vim.api.nvim_list_bufs())
+              end
 
-          for _, bufnr in ipairs(get_listed_bufs()) do
-            if bufnr ~= current
-            then require("mini.bufremove").delete(bufnr)
+              for _, bufnr in ipairs(get_listed_bufs()) do
+                if bufnr ~= current
+                then require("mini.bufremove").delete(bufnr)
+                end
+              end
             end
-          end
-        end
-      '';
-      options = {
-        desc = "Close all buffers but current";
-      };
-    }
-  ];
+          '';
+          options = {
+            desc = "Close all buffers but current";
+          };
+        }
+      ];
 }
