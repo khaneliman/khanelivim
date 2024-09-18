1f60d9c1a7cdcc5a0ea9bbf9ea41401f640eb1fd
Austin Horstman <khaneliman12@gmail.com>
2024-09-17 21:00:17 -0500

    plugins/yanky: use sqlite

--- packages/khanelivim/plugins/yanky.nix
+++ packages/khanelivim/plugins/yanky.nix
@@ -2,6 +2,17 @@
   plugins = {
     yanky = {
       enable = true;
+      settings = {
+        ring = {
+          history_length = 100;
+          storage = "sqlite";
+          storage_path.__raw = "vim.fn.stdpath('data') .. '/databases/yanky.db'";
+          sync_with_numbered_registers = true;
+          cancel_event = "update";
+          ignore_registers = [ "_" ];
+          update_register_on_cycle = false;
+        };
+      };
     };
   };
 }
