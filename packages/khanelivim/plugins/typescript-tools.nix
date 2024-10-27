4adf4e44bda9887a93539535a89ff86a95f99c0e
Austin Horstman <khaneliman12@gmail.com>
2024-10-27 16:52:16 -0500

    plugins/typescript-tools: remove deprecations

--- packages/khanelivim/plugins/typescript-tools.nix
+++ packages/khanelivim/plugins/typescript-tools.nix
@@ -3,20 +3,22 @@
     typescript-tools = {
       enable = true;
       settings = {
-        separateDiagnosticServer = true;
-        publishDiagnosticOn = "insert_leave";
-        tsserverMaxMemory = "auto";
-        tsserverLocale = "en";
-        completeFunctionCalls = false;
-        includeCompletionsWithInsertText = true;
-        codeLens = "off";
-        disableMemberCodeLens = true;
-        jsxCloseTag = {
-          enable = false;
-          filetypes = [
-            "javascriptreact"
-            "typescriptreact"
-          ];
+        settings = {
+          separate_diagnostic_server = true;
+          publish_diagnostic_on = "insert_leave";
+          tsserver_max_memory = "auto";
+          tsserver_locale = "en";
+          complete_function_calls = false;
+          include_completions_with_insert_text = true;
+          code_lens = "off";
+          disable_member_code_lens = true;
+          jsx_close_tag = {
+            enable = false;
+            filetypes = [
+              "javascriptreact"
+              "typescriptreact"
+            ];
+          };
         };
       };
     };
