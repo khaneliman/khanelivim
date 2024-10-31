d577647f08275e668a258d139d24d83fb848f9d6
Austin Horstman <khaneliman12@gmail.com>
2024-10-31 14:38:32 -0500

    plugins/noice: fix deprecations

--- packages/khanelivim/plugins/noice.nix
+++ packages/khanelivim/plugins/noice.nix
@@ -4,133 +4,135 @@
     noice = {
       enable = true;
 
-      # Hides the title above noice boxes
-      cmdline = {
-        format = {
-          cmdline = {
-            pattern = "^:";
-            icon = "";
-            lang = "vim";
-            opts = {
-              border = {
-                text = {
-                  top = "Cmd";
+      settings = {
+        # Hides the title above noice boxes
+        cmdline = {
+          format = {
+            cmdline = {
+              pattern = "^:";
+              icon = "";
+              lang = "vim";
+              opts = {
+                border = {
+                  text = {
+                    top = "Cmd";
+                  };
                 };
               };
             };
-          };
-          search_down = {
-            kind = "search";
-            pattern = "^/";
-            icon = " ";
-            lang = "regex";
-          };
-          search_up = {
-            kind = "search";
-            pattern = "^%?";
-            icon = " ";
-            lang = "regex";
-          };
-          filter = {
-            pattern = "^:%s*!";
-            icon = "";
-            lang = "bash";
-            opts = {
-              border = {
-                text = {
-                  top = "Bash";
+            search_down = {
+              kind = "search";
+              pattern = "^/";
+              icon = " ";
+              lang = "regex";
+            };
+            search_up = {
+              kind = "search";
+              pattern = "^%?";
+              icon = " ";
+              lang = "regex";
+            };
+            filter = {
+              pattern = "^:%s*!";
+              icon = "";
+              lang = "bash";
+              opts = {
+                border = {
+                  text = {
+                    top = "Bash";
+                  };
                 };
               };
             };
+            lua = {
+              pattern = "^:%s*lua%s+";
+              icon = "";
+              lang = "lua";
+            };
+            help = {
+              pattern = "^:%s*he?l?p?%s+";
+              icon = "󰋖";
+            };
+            input = { };
           };
-          lua = {
-            pattern = "^:%s*lua%s+";
-            icon = "";
-            lang = "lua";
-          };
-          help = {
-            pattern = "^:%s*he?l?p?%s+";
-            icon = "󰋖";
-          };
-          input = { };
-        };
-      };
-
-      messages = {
-        view = "mini";
-        viewError = "mini";
-        viewWarn = "mini";
-      };
-
-      lsp = {
-        override = {
-          "vim.lsp.util.convert_input_to_markdown_lines" = true;
-          "vim.lsp.util.stylize_markdown" = true;
-          "cmp.entry.get_documentation" = true;
         };
 
-        progress.enabled = true;
-        signature.enabled = !config.plugins.lsp-signature.enable;
-      };
+        messages = {
+          view = "mini";
+          viewError = "mini";
+          viewWarn = "mini";
+        };
 
-      popupmenu.backend = "nui";
-      # Doesn't support the standard cmdline completions
-      # popupmenu.backend = "cmp";
-
-      presets = {
-        bottom_search = false;
-        command_palette = true;
-        long_message_to_split = true;
-        inc_rename = true;
-        lsp_doc_border = true;
-      };
-
-      routes = [
-        {
-          filter = {
-            event = "msg_show";
-            kind = "search_count";
+        lsp = {
+          override = {
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+            "cmp.entry.get_documentation" = true;
           };
-          opts = {
-            skip = true;
-          };
-        }
-        {
-          # skip progress messages from noisy servers
-          filter = {
-            event = "lsp";
-            kind = "progress";
-            cond.__raw = ''
-              function(message)
-                local client = vim.tbl_get(message.opts, 'progress', 'client')
-                local servers = { 'jdtls' }
 
-                for index, value in ipairs(servers) do
-                    if value == client then
-                        return true
-                    end
+          progress.enabled = true;
+          signature.enabled = !config.plugins.lsp-signature.enable;
+        };
+
+        popupmenu.backend = "nui";
+        # Doesn't support the standard cmdline completions
+        # popupmenu.backend = "cmp";
+
+        presets = {
+          bottom_search = false;
+          command_palette = true;
+          long_message_to_split = true;
+          inc_rename = true;
+          lsp_doc_border = true;
+        };
+
+        routes = [
+          {
+            filter = {
+              event = "msg_show";
+              kind = "search_count";
+            };
+            opts = {
+              skip = true;
+            };
+          }
+          {
+            # skip progress messages from noisy servers
+            filter = {
+              event = "lsp";
+              kind = "progress";
+              cond.__raw = ''
+                function(message)
+                  local client = vim.tbl_get(message.opts, 'progress', 'client')
+                  local servers = { 'jdtls' }
+
+                  for index, value in ipairs(servers) do
+                      if value == client then
+                          return true
+                      end
+                  end
                 end
-              end
-            '';
-          };
-          opts = {
-            skip = true;
-          };
-        }
-      ];
+              '';
+            };
+            opts = {
+              skip = true;
+            };
+          }
+        ];
 
-      views = {
-        cmdline_popup = {
-          border = {
-            style = "single";
+        views = {
+          cmdline_popup = {
+            border = {
+              style = "single";
+            };
           };
-        };
 
-        confirm = {
-          border = {
-            style = "single";
-            text = {
-              top = "";
+          confirm = {
+            border = {
+              style = "single";
+              text = {
+                top = "";
+              };
             };
           };
         };
