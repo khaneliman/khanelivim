cf94eada9a4cfb3682ce42f02be0848d12bc52ad
Austin Horstman <khaneliman12@gmail.com>
2024-11-12 21:40:21 -0600

    plugins/copilot-chat: float chat window

--- packages/khanelivim/plugins/copilot.nix
+++ packages/khanelivim/plugins/copilot.nix
@@ -8,6 +8,16 @@
 
     copilot-chat = {
       enable = true;
+
+      settings = {
+        window = {
+          layout = "float";
+          relative = "cursor";
+          width = 1;
+          height = 0.5;
+          row = 1;
+        };
+      };
     };
   };
 }
