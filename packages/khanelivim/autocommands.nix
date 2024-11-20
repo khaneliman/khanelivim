2afadcf003873292215ff8fcbd6161da2c3bcb3b
Austin Horstman <khaneliman12@gmail.com>
2024-11-20 08:52:43 -0600

    plugins/snacks: init with bigfile

--- packages/khanelivim/autocommands.nix
+++ packages/khanelivim/autocommands.nix
@@ -8,41 +8,47 @@
     }
 
     # Handle performance on large files
-    {
-      event = "BufEnter";
-      pattern = [ "*" ];
-      callback.__raw = ''
-        function()
-          local buf_size_limit = 1024 * 1024 -- 1MB size limit
-          if vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0)) > buf_size_limit then
-            ${lib.optionalString config.plugins.indent-blankline.enable ''require("ibl").setup_buffer(0, { enabled = false })''}
-            ${lib.optionalString (lib.hasAttr "indentscope" config.plugins.mini.modules) ''vim.b.miniindentscope_disable = true''}
-            ${lib.optionalString config.plugins.illuminate.enable ''require("illuminate").pause_buf()''}
+    (lib.mkIf
+      (
+        (!config.plugins.snacks.enable)
+        || (config.plugins.snacks.enable && (!config.plugins.snacks.settings.bigfile.enabled))
+      )
+      {
+        event = "BufEnter";
+        pattern = [ "*" ];
+        callback.__raw = ''
+          function()
+            local buf_size_limit = 1024 * 1024 -- 1MB size limit
+            if vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0)) > buf_size_limit then
+              ${lib.optionalString config.plugins.indent-blankline.enable ''require("ibl").setup_buffer(0, { enabled = false })''}
+              ${lib.optionalString (lib.hasAttr "indentscope" config.plugins.mini.modules) ''vim.b.miniindentscope_disable = true''}
+              ${lib.optionalString config.plugins.illuminate.enable ''require("illuminate").pause_buf()''}
 
-            -- Disable line numbers and relative line numbers
-            vim.cmd("setlocal nonumber norelativenumber")
+              -- Disable line numbers and relative line numbers
+              vim.cmd("setlocal nonumber norelativenumber")
 
-            -- Disable syntax highlighting
-            -- vim.cmd("syntax off")
+              -- Disable syntax highlighting
+              -- vim.cmd("syntax off")
 
-            -- Disable matchparen
-            vim.cmd("let g:loaded_matchparen = 1")
+              -- Disable matchparen
+              vim.cmd("let g:loaded_matchparen = 1")
 
-            -- Disable cursor line and column
-            vim.cmd("setlocal nocursorline nocursorcolumn")
+              -- Disable cursor line and column
+              vim.cmd("setlocal nocursorline nocursorcolumn")
 
-            -- Disable folding
-            vim.cmd("setlocal nofoldenable")
+              -- Disable folding
+              vim.cmd("setlocal nofoldenable")
 
-            -- Disable sign column
-            vim.cmd("setlocal signcolumn=no")
+              -- Disable sign column
+              vim.cmd("setlocal signcolumn=no")
 
-            -- Disable swap file and undo file
-            vim.cmd("setlocal noswapfile noundofile")
+              -- Disable swap file and undo file
+              vim.cmd("setlocal noswapfile noundofile")
+            end
           end
-        end
-      '';
-    }
+        '';
+      }
+    )
 
     # Open minimap on first buffer
     # {
---
+++ packages/khanelivim/plugins/snacks.nix
@@ -0,0 +1,13 @@
+{
+  plugins = {
+    snacks = {
+      enable = true;
+
+      settings = {
+        bigfile = {
+          enabled = true;
+        };
+      };
+    };
+  };
+}
