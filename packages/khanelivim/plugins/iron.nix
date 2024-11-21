31952eb2978954cb6ff91e6c4b64f91695e3dff9
Austin Horstman <khaneliman12@gmail.com>
2024-11-21 10:55:18 -0600

    plugins/iron: fix highlighting
    for some reason needs this otherwise breaks with nix interpolation

--- packages/khanelivim/plugins/iron.nix
+++ packages/khanelivim/plugins/iron.nix
@@ -4,55 +4,54 @@
     iron-nvim
   ];
 
-  extraConfigLua = ''
-    local iron = require ("iron.core")
-    iron.setup {
-      config = {
-          -- Whether a repl should be discarded or not
-          scratch_repl = true,
-          -- Your repl definitions come here
-          repl_definition = {
-            -- sh = {
-            --   -- Can be a table or a function that
-            --   -- returns a table (see below)
-            --   command = {"zsh"}
-            -- },
-            python = {
-              command = { "${lib.getExe pkgs.python3}" },
-              format = require("iron.fts.common").bracketed_paste_python
+  extraConfigLua = # Lua
+    ''
+      local iron = require ("iron.core")
+      iron.setup {
+        config = {
+            -- Whether a repl should be discarded or not
+            scratch_repl = true,
+            -- Your repl definitions come here
+            repl_definition = {
+              -- sh = {
+              --   command = {"zsh"}
+              -- },
+              python = {
+                command = { "${lib.getExe pkgs.python3}" },
+                format = require("iron.fts.common").bracketed_paste_python
+              },
+              nix = {
+                command = { "nix", "repl" }
+              }
             },
-            nix = {
-              command = { "nix", "repl" }
-            }
+            -- How the repl window will be displayed
+            -- See below for more information
+            -- repl_open_cmd = require('iron.view').bottom(40),
           },
-          -- How the repl window will be displayed
-          -- See below for more information
-          -- repl_open_cmd = require('iron.view').bottom(40),
-        },
-        -- Iron doesn't set keymaps by default anymore.
-        -- You can set them here or manually add keymaps to the functions in iron.core
-        keymaps = {
-          send_motion = "<space>sc",
-          visual_send = "<space>sc",
-          send_file = "<space>sf",
-          send_line = "<space>sl",
-          send_paragraph = "<space>sp",
-          send_until_cursor = "<space>su",
-          send_mark = "<space>sm",
-          mark_motion = "<space>mc",
-          mark_visual = "<space>mc",
-          remove_mark = "<space>md",
-          cr = "<space>s<cr>",
-          interrupt = "<space>s<space>",
-          exit = "<space>sq",
-          clear = "<space>cl",
-        },
-        -- If the highlight is on, you can change how it looks
-        -- For the available options, check nvim_set_hl
-        highlight = {
-          italic = true
-        },
-        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-    }
-  '';
+          -- Iron doesn't set keymaps by default anymore.
+          -- You can set them here or manually add keymaps to the functions in iron.core
+          keymaps = {
+            send_motion = "<space>sc",
+            visual_send = "<space>sc",
+            send_file = "<space>sf",
+            send_line = "<space>sl",
+            send_paragraph = "<space>sp",
+            send_until_cursor = "<space>su",
+            send_mark = "<space>sm",
+            mark_motion = "<space>mc",
+            mark_visual = "<space>mc",
+            remove_mark = "<space>md",
+            cr = "<space>s<cr>",
+            interrupt = "<space>s<space>",
+            exit = "<space>sq",
+            clear = "<space>cl",
+          },
+          -- If the highlight is on, you can change how it looks
+          -- For the available options, check nvim_set_hl
+          highlight = {
+            italic = true
+          },
+          ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      }
+    '';
 }
