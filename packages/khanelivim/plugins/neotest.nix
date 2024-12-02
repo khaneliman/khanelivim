eb21d3097f204b9fc5be5f7a1632ee2deda5efa6
Austin Horstman <khaneliman12@gmail.com>
2024-12-02 09:57:04 -0600

    neotest: lazy load

--- packages/khanelivim/plugins/neotest.nix
+++ packages/khanelivim/plugins/neotest.nix
@@ -6,8 +6,76 @@
 }:
 {
   plugins = {
+    lz-n = {
+      enable = true;
+    };
+
     neotest = {
       enable = true;
+      lazyLoad = {
+        enable = true;
+        settings = {
+          keys = [
+            {
+              __unkeyed-1 = "<leader>nt";
+              __unkeyed-3 = "<CMD>Neotest summary<CR>";
+              desc = "Summary toggle";
+            }
+            {
+              __unkeyed-1 = "<leader>dn";
+              __unkeyed-3 = ''
+                function()
+                  require("neotest").run.run({strategy = "dap"})
+                end
+              '';
+              desc = "Neotest Debug";
+            }
+            {
+              __unkeyed-1 = "<leader>na";
+              __unkeyed-3 = "<CMD>Neotest attach<CR>";
+              desc = "Attach";
+            }
+            {
+              __unkeyed-1 = "<leader>nd";
+              __unkeyed-3 = ''
+                function()
+                  require("neotest").run.run({strategy = "dap"})
+                end
+              '';
+              desc = "Debug";
+            }
+            {
+              __unkeyed-1 = "<leader>nh";
+              __unkeyed-3 = "<CMD>Neotest output<CR>";
+              desc = "Output";
+            }
+            {
+              __unkeyed-1 = "<leader>no";
+              __unkeyed-3 = "<CMD>Neotest output-panel<CR>";
+              desc = "Output Panel toggle";
+            }
+            {
+              __unkeyed-1 = "<leader>nr";
+              __unkeyed-3 = "<CMD>Neotest run<CR>";
+              desc = "Run (Nearest Test)";
+            }
+            {
+              __unkeyed-1 = "<leader>nR";
+              __unkeyed-3 = ''
+                function()
+                  require("neotest").run.run(vim.fn.expand("%"))
+                end
+              '';
+              desc = "Run (File)";
+            }
+            {
+              __unkeyed-1 = "<leader>ns";
+              __unkeyed-3 = "<CMD>Neotest stop<CR>";
+              desc = "Stop";
+            }
+          ];
+        };
+      };
 
       settings = {
         adapters = lib.optionals config.plugins.rustaceanvim.enable [
@@ -62,7 +130,7 @@
     ];
   };
 
-  keymaps = lib.mkIf config.plugins.neotest.enable [
+  keymaps = lib.mkIf (config.plugins.neotest.enable && !config.plugins.lz-n.enable) [
     {
       mode = "n";
       key = "<leader>dn";
