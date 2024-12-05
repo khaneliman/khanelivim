be13db70ad2610b0e1f765605432d0da36dd768c
Austin Horstman <khaneliman12@gmail.com>
2024-12-05 08:41:21 -0600

    spectre: lazy load

--- packages/khanelivim/plugins/spectre.nix
+++ packages/khanelivim/plugins/spectre.nix
@@ -3,10 +3,22 @@
   plugins = {
     spectre = {
       enable = true;
+      lazyLoad = {
+        enable = true;
+        settings = {
+          keys = [
+            {
+              __unkeyed-1 = "<leader>rs";
+              __unkeyed-2 = "<cmd>Spectre<CR>";
+              desc = "Spectre toggle";
+            }
+          ];
+        };
+      };
     };
   };
 
-  keymaps = lib.mkIf config.plugins.spectre.enable [
+  keymaps = lib.mkIf (config.plugins.spectre.enable && !config.plugins.lz-n.enable) [
     {
       mode = "n";
       key = "<leader>rs";
