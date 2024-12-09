988da28acfa673bccba308c6ed0038981df2b3f8
Austin Horstman <khaneliman12@gmail.com>
2024-12-08 22:22:37 -0600

    flash: add search excludes

--- packages/khanelivim/plugins/flash.nix
+++ packages/khanelivim/plugins/flash.nix
@@ -3,6 +3,26 @@
   plugins = {
     flash = {
       enable = true;
+
+      settings = {
+        search = {
+          exclude = [
+            "notify"
+            "cmp_menu"
+            "noice"
+            "flash_prompt"
+            # "NeogitStatus"
+            {
+              __raw = ''
+                function(win)
+                  -- exclude non-focusable windows
+                  return not vim.api.nvim_win_get_config(win).focusable
+                end
+              '';
+            }
+          ];
+        };
+      };
     };
   };
 
