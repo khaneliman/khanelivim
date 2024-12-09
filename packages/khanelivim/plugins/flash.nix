a394aca3d102185b5c683b2147c0d2b77d4e1143
Austin Horstman <khaneliman12@gmail.com>
2024-12-08 23:01:37 -0600

    flash: add telescope integration

--- packages/khanelivim/plugins/flash.nix
+++ packages/khanelivim/plugins/flash.nix
@@ -29,6 +29,39 @@
         };
       };
     };
+
+    telescope.settings.defaults.mappings =
+      let
+        flash.__raw = ''
+          function(prompt_bufnr)
+            require("flash").jump({
+              pattern = "^",
+              label = { after = { 0, 0 } },
+              search = {
+                mode = "search",
+                exclude = {
+                  function(win)
+                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "TelescopeResults"
+                  end,
+                },
+              },
+              action = function(match)
+                local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+                picker:set_selection(match.pos[1] - 1)
+              end,
+            })
+          end
+        '';
+      in
+      lib.mkIf config.plugins.flash.enable {
+        n = {
+          s = flash;
+        };
+        i = {
+          "<c-s>" = flash;
+        };
+      };
+
   };
 
   keymaps = lib.mkIf config.plugins.flash.enable [
@@ -68,70 +101,6 @@
       ];
       options.desc = "Treesitter Search";
     }
-    # NOTE: not needed?
-    # {
-    #   key = "f";
-    #   mode = [
-    #     "n"
-    #     "x"
-    #     "o"
-    #   ];
-    #   action.__raw = ''
-    #     function()
-    #         require("flash").jump(
-    #             {
-    #                 search = {mode = "search"},
-    #                 label = {
-    #                     after = false,
-    #                     before = {0, 0},
-    #                     uppercase = false,
-    #                     format = function(opts)
-    #                         -- always show first and second label
-    #                         return {
-    #                             {opts.match.label1, "FlashMatch"},
-    #                             {opts.match.label2, "FlashLabel"}
-    #                         }
-    #                     end
-    #                 },
-    #                 pattern = [[\<]],
-    #                 action = function(match, state)
-    #                     state:hide()
-    #                     require("flash").jump(
-    #                         {
-    #                             search = {max_length = 0},
-    #                             highlight = {matches = false},
-    #                             label = {format = format},
-    #                             matcher = function(win)
-    #                                 -- limit matches to the current label
-    #                                 return vim.tbl_filter(
-    #                                     function(m)
-    #                                         return m.label == match.label and m.win == win
-    #                                     end,
-    #                                     state.results
-    #                                 )
-    #                             end,
-    #                             labeler = function(matches)
-    #                                 for _, m in ipairs(matches) do
-    #                                     m.label = m.label2 -- use the second label
-    #                                 end
-    #                             end
-    #                         }
-    #                     )
-    #                 end,
-    #                 labeler = function(matches, state)
-    #                     local labels = state:labels()
-    #                     for m, match in ipairs(matches) do
-    #                         match.label1 = labels[math.floor((m - 1) / #labels) + 1]
-    #                         match.label2 = labels[(m - 1) % #labels + 1]
-    #                         match.label = match.label1
-    #                     end
-    #                 end
-    #             }
-    #         )
-    #       end
-    #   '';
-    #   options.desc = "Flash hop";
-    # }
     {
       key = "gl";
       action.__raw = ''
@@ -152,5 +121,4 @@
     }
     # { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   ];
-
 }
