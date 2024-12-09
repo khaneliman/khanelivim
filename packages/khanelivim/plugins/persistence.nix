cc31f1430bc048772525301904d12e226804368e
Austin Horstman <khaneliman12@gmail.com>
2024-12-09 15:42:57 -0600

    persistence: add keymaps

--- packages/khanelivim/plugins/persistence.nix
+++ packages/khanelivim/plugins/persistence.nix
@@ -1,5 +1,43 @@
+{ config, lib, ... }:
 {
-  plugins.persistence = {
-    enable = true;
+  plugins = {
+    persistence = {
+      enable = true;
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.persistence.enable [
+      {
+        __unkeyed = "<leader>P";
+        group = "Persistence";
+        icon = "󰘛";
+      }
+    ];
   };
+
+  keymaps = lib.optionals config.plugins.persistence.enable [
+    {
+      mode = "n";
+      key = "<leader>Pl";
+      action.__raw = "function() require('persistence').load() end";
+      options.desc = "Load the session for the current directory";
+    }
+    {
+      mode = "n";
+      key = "<leader>Ps";
+      action.__raw = "function() require('persistence').select() end";
+      options.desc = "Select a session to load";
+    }
+    {
+      mode = "n";
+      key = "<leader>PL";
+      action.__raw = "function() require('persistence').load({ last = true }) end";
+      options.desc = "Load the last session";
+    }
+    {
+      mode = "n";
+      key = "<leader>PS";
+      action.__raw = "function() require('persistence').stop() end";
+      options.desc = "Stop Persistence";
+    }
+  ];
 }
