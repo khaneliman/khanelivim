f6e81cfa5520b94b14a5059f486738035ab6031a
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-12-09 18:49:25 -0300

    Fix condition for "Remove trailing whitespace on save" autocommand

--- packages/khanelivim/autocommands.nix
+++ packages/khanelivim/autocommands.nix
@@ -2,7 +2,7 @@
 {
   autoCmd = [
     # Remove trailing whitespace on save
-    (lib.mkIf (lib.elem "trim_whitespace" config.plugins.conform-nvim.settings.formatters_by_ft."_") {
+    (lib.mkIf (!lib.elem "trim_whitespace" config.plugins.conform-nvim.settings.formatters_by_ft."_") {
       event = "BufWrite";
       command = "%s/\\s\\+$//e";
     })
