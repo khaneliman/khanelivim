e344fd3055de0b8fc2606f3a2d666c90aa296a4e
Austin Horstman <khaneliman12@gmail.com>
2024-12-08 22:43:57 -0600

    flash: 2-char hop

--- packages/khanelivim/plugins/flash.nix
+++ packages/khanelivim/plugins/flash.nix
@@ -64,6 +64,69 @@
       options.desc = "Treesitter Search";
     }
     {
+      key = "f";
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
+      action.__raw = ''
+        function()
+            require("flash").jump(
+                {
+                    search = {mode = "search"},
+                    label = {
+                        after = false,
+                        before = {0, 0},
+                        uppercase = false,
+                        format = function(opts)
+                            -- always show first and second label
+                            return {
+                                {opts.match.label1, "FlashMatch"},
+                                {opts.match.label2, "FlashLabel"}
+                            }
+                        end
+                    },
+                    pattern = [[\<]],
+                    action = function(match, state)
+                        state:hide()
+                        require("flash").jump(
+                            {
+                                search = {max_length = 0},
+                                highlight = {matches = false},
+                                label = {format = format},
+                                matcher = function(win)
+                                    -- limit matches to the current label
+                                    return vim.tbl_filter(
+                                        function(m)
+                                            return m.label == match.label and m.win == win
+                                        end,
+                                        state.results
+                                    )
+                                end,
+                                labeler = function(matches)
+                                    for _, m in ipairs(matches) do
+                                        m.label = m.label2 -- use the second label
+                                    end
+                                end
+                            }
+                        )
+                    end,
+                    labeler = function(matches, state)
+                        local labels = state:labels()
+                        for m, match in ipairs(matches) do
+                            match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                            match.label2 = labels[(m - 1) % #labels + 1]
+                            match.label = match.label1
+                        end
+                    end
+                }
+            )
+          end
+      '';
+      options.desc = "Flash hop";
+    }
+    {
       key = "gl";
       action.__raw = ''
         function()
