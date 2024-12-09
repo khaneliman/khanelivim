f099de84db7b29a0e964e99f2a9d9c0695a7a53e
Austin Horstman <khaneliman12@gmail.com>
2024-12-08 22:29:42 -0600

    flash: go to line

--- packages/khanelivim/plugins/flash.nix
+++ packages/khanelivim/plugins/flash.nix
@@ -63,6 +63,24 @@
       ];
       options.desc = "Treesitter Search";
     }
+    {
+      key = "gl";
+      action.__raw = ''
+        function()
+          require("flash").jump {
+            search = { mode = 'search', max_length = 0 },
+            label = { after = { 0, 0 } },
+            pattern = '^',
+          }
+        end
+      '';
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
+      options.desc = "Flash Line";
+    }
     # { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   ];
 
