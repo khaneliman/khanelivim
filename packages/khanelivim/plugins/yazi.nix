058274a310e20e74f7bd274c3f2d2af13a189a04
Austin Horstman <khaneliman12@gmail.com>
2024-12-10 14:50:13 -0600

    yazi: lazy load and keymap tweak

--- packages/khanelivim/plugins/yazi.nix
+++ packages/khanelivim/plugins/yazi.nix
@@ -1,19 +1,26 @@
 { config, lib, ... }:
 {
-  plugins.yazi.enable = true;
+  plugins.yazi = {
+    enable = true;
+
+    lazyLoad = {
+      settings = {
+        cmd = [
+          "Yazi"
+          "Yazi cwd"
+          "Yazi toggle"
+        ];
+      };
+    };
+  };
 
   keymaps = lib.optionals config.plugins.yazi.enable [
     {
       mode = "n";
       key = "<leader>e";
-      action.__raw = ''
-        function()
-          require('yazi').yazi()
-        end
-      '';
+      action = "<CMD>Yazi toggle<CR>";
       options = {
         desc = "Yazi toggle";
-        silent = true;
       };
     }
   ];
