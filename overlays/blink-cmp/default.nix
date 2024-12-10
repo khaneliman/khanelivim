aa15fc5a6b625a70d4e6008fa6b5c9b76c2ab910
Austin Horstman <khaneliman12@gmail.com>
2024-12-09 19:38:49 -0600

    overlays/blink-cmp: 0.7.3 -> 0.7.4

--- overlays/blink-cmp/default.nix
+++ overlays/blink-cmp/default.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   vimPlugins = prev.vimPlugins // {
     blink-cmp = prev.vimPlugins.blink-cmp.overrideAttrs {
-      version = "0.7.3";
+      version = "0.7.4";
       src = prev.fetchFromGitHub {
         owner = "Saghen";
         repo = "blink.cmp";
-        rev = "v0.7.3";
-        hash = "sha256-nxiODLKgGeXzN5sqkLWU0PcsuSSB1scSzTC5qyCxLCI=";
+        rev = "v0.7.4";
+        hash = "sha256-xFTCftqz47/6m2h/dOmMI2T5aEvOvs7ixs46vKBAdDY=";
       };
     };
   };
