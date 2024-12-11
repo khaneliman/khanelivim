617293c15291448d88c0fcfe0206a1a3dbff64fb
Austin Horstman <khaneliman12@gmail.com>
2024-12-11 12:48:30 -0600

    overseer: lazy load on cmd

--- packages/khanelivim/plugins/overseer.nix
+++ packages/khanelivim/plugins/overseer.nix
@@ -1,5 +1,23 @@
 {
   plugins = {
-    overseer.enable = true;
+    overseer = {
+      enable = true;
+
+      lazyLoad.settings.cmd = [
+        "OverseerOpen"
+        "OverseerClose"
+        "OverseerToggle"
+        "OverseerSaveBundle"
+        "OverseerLoadBundle"
+        "OverseerDeleteBundle"
+        "OverseerRunCmd"
+        "OverseerRun"
+        "OverseerInfo"
+        "OverseerBuild"
+        "OverseerQuickAction"
+        "OverseerTaskAction"
+        "OverseerClearCache"
+      ];
+    };
   };
 }
