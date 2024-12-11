522c9135987f617a93a1e68f4e2ea361fb2c5f17
Austin Horstman <khaneliman12@gmail.com>
2024-12-11 14:28:53 -0600

    todo-comments: lazy load on buf enter

--- packages/khanelivim/plugins/todo-comments.nix
+++ packages/khanelivim/plugins/todo-comments.nix
@@ -3,6 +3,10 @@
   plugins.todo-comments = {
     enable = true;
 
+    lazyLoad.settings.event = [
+      "BufEnter"
+    ];
+
     keymaps = {
       todoTrouble.key = lib.mkIf config.plugins.trouble.enable "<leader>xq";
       todoTelescope = lib.mkIf config.plugins.telescope.enable {
