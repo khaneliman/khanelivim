32a9f77b32c4b0db8480e0568c3200711b92a390
Austin Horstman <khaneliman12@gmail.com>
2024-12-10 23:08:35 -0600

    snacks: tweak extraConfigLuaPre

--- packages/khanelivim/plugins/snacks.nix
+++ packages/khanelivim/plugins/snacks.nix
@@ -5,22 +5,23 @@
   ...
 }:
 {
-  extraConfigLuaPre = lib.mkOrder 1 (
-    lib.optionalString
-      (config.plugins.snacks.enable && config.plugins.snacks.settings.profiler.enabled) # Lua
-      ''
-        if vim.env.PROF then
-          local snacks = "${pkgs.vimPlugins.snacks-nvim}"
-          vim.opt.rtp:append(snacks)
-          require("snacks.profiler").startup({
-            startup = {
-              -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
-              event = "UIEnter",
-              -- event = "VeryLazy",
-            },
-          })
-        end
-      ''
+  extraConfigLuaPre = lib.mkIf config.plugins.snacks.enable (
+    lib.mkOrder 1 (
+      lib.optionalString config.plugins.snacks.settings.profiler.enabled # Lua
+        ''
+          if vim.env.PROF then
+            local snacks = "${pkgs.vimPlugins.snacks-nvim}"
+            vim.opt.rtp:append(snacks)
+            require("snacks.profiler").startup({
+              startup = {
+                -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+                event = "UIEnter",
+                -- event = "VeryLazy",
+              },
+            })
+          end
+        ''
+    )
   );
 
   plugins = {
