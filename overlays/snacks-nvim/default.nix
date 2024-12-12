28b3914339fd13b53cbafebb5faad0b556944cb5
Austin Horstman <khaneliman12@gmail.com>
2024-12-12 09:21:30 -0600

    overlays/snacks-nvim: bump to latest release

--- overlays/snacks-nvim/default.nix
+++ overlays/snacks-nvim/default.nix
@@ -2,12 +2,12 @@ _: _final: prev: {
   vimPlugins = prev.vimPlugins // {
     snacks-nvim = prev.vimPlugins.snacks-nvim.overrideAttrs {
       pname = "snacks.nvim";
-      version = "2024-12-10";
+      version = "2.9.0";
       src = prev.fetchFromGitHub {
         owner = "folke";
         repo = "snacks.nvim";
-        rev = "c9880ce872ca000d17ae8d62b10e913045f54735";
-        sha256 = "100vzbx2h6w939r8wdis8i2zyqnfc64rg4zaiifz6yj3h17fja6a";
+        tag = "v2.9.0";
+        hash = "sha256-szno2o0qhclp/uVqSME0ssSDORwLkpHWqom27EvDklU=";
       };
       meta.homepage = "https://github.com/folke/snacks.nvim/";
     };
