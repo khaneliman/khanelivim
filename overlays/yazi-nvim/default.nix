68e35c77b6e18ec38f2431e4347a824d89ab7d3b
Austin Horstman <khaneliman12@gmail.com>
2024-12-12 09:19:29 -0600

    overlays/yazi-nvim: bump to latest release

--- overlays/yazi-nvim/default.nix
+++ overlays/yazi-nvim/default.nix
@@ -2,12 +2,12 @@ _: _final: prev: {
   vimPlugins = prev.vimPlugins // {
     yazi-nvim = prev.vimPlugins.yazi-nvim.overrideAttrs {
       pname = "yazi.nvim";
-      version = "2024-12-10";
+      version = "6.9.0";
       src = prev.fetchFromGitHub {
         owner = "mikavilpas";
         repo = "yazi.nvim";
-        rev = "ce6b5b249cde9e4a27dfce18a6adb57c170d4325";
-        sha256 = "0n4bz8iv5nxfm1gmn02jy0ixfj7lfb8rkli7l95c81h7v84wrxga";
+        tag = "v6.9.0";
+        hash = "sha256-GIBXiHGW+7aKOR6c3mkJz05CdewFE9gAhbbvIZcxiEc=";
       };
       meta.homepage = "https://github.com/mikavilpas/yazi.nvim/";
     };
