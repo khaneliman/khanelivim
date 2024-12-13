c3888b0315c93734b6f4f2bea09b22d4f105d7c0
Austin Horstman <khaneliman12@gmail.com>
2024-12-12 19:52:18 -0600

    yazi: add keymap

--- packages/khanelivim/plugins/yazi.nix
+++ packages/khanelivim/plugins/yazi.nix
@@ -16,9 +16,17 @@
     {
       mode = "n";
       key = "<leader>e";
+      action = "<CMD>Yazi<CR>";
+      options = {
+        desc = "Yazi (current file)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>E";
       action = "<CMD>Yazi toggle<CR>";
       options = {
-        desc = "Yazi toggle";
+        desc = "Yazi (resume)";
       };
     }
   ];
