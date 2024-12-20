01421b3f98bdd73c3e609307f7723abab6f43c9b
Austin Horstman <khaneliman12@gmail.com>
2024-12-20 16:57:36 -0600

    fzf-lua: init

---
+++ packages/khanelivim/plugins/fzf-lua.nix
@@ -0,0 +1,214 @@
+{ lib, config, ... }:
+{
+  plugins = {
+    fzf-lua = {
+      enable = true;
+      profile = "telescope";
+
+      lazyLoad.settings.cmd = "FzfLua";
+
+      settings = {
+        winopts = {
+          preview = {
+            default = "bat";
+          };
+        };
+      };
+    };
+  };
+
+  keymaps = lib.mkIf config.plugins.fzf-lua.enable [
+    {
+      mode = "n";
+      key = "<leader>f'";
+      action = ''<cmd>FzfLua marks<CR>'';
+      options = {
+        desc = "Find marks";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>f/";
+      action = ''<cmd>FzfLua lines<CR>'';
+      options = {
+        desc = "Fuzzy find in current buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>f<CR>";
+      action = ''<cmd>FzfLua resume<CR>'';
+      options = {
+        desc = "Resume find";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fa";
+      action = ''<cmd>FzfLua autocmds<CR>'';
+      options = {
+        desc = "Find autocmds";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fb";
+      action = ''<cmd>FzfLua buffers<CR>'';
+      options = {
+        desc = "Find buffers";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fc";
+      action = ''<cmd>FzfLua commands<CR>'';
+      options = {
+        desc = "Find commands";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fC";
+      action.__raw = ''
+        function()
+          require("fzf-lua").files {
+            prompt_title = "Config Files",
+            cwd = vim.fn.stdpath "config",
+            follow = true,
+          }
+        end
+      '';
+      options = {
+        desc = "Find config files";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fd";
+      action = ''<cmd>FzfLua diagnostics_document<CR>'';
+      options = {
+        desc = "Find buffer diagnostics";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fD";
+      action = ''<cmd>FzfLua diagnostics_workspace<CR>'';
+      options = {
+        desc = "Find workspace diagnostics";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ff";
+      action = ''<cmd>FzfLua files<CR>'';
+      options = {
+        desc = "Find files";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gB";
+      action = ''<cmd>FzfLua git_branches<CR>'';
+      options = {
+        desc = "Find git branches";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gC";
+      action = ''<cmd>FzfLua git_commits<CR>'';
+      options = {
+        desc = "Find git commits";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fh";
+      action = ''<cmd>FzfLua helptags<CR>'';
+      options = {
+        desc = "Find help tags";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fk";
+      action = ''<cmd>FzfLua keymaps<CR>'';
+      options = {
+        desc = "Find keymaps";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fm";
+      action = ''<cmd>FzfLua manpages<CR>'';
+      options = {
+        desc = "Find man pages";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fo";
+      action = ''<cmd>FzfLua oldfiles<CR>'';
+      options = {
+        desc = "Find old files";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fq";
+      action = ''<cmd>FzfLua quickfix<CR>'';
+      options = {
+        desc = "Find quickfix";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fr";
+      action = ''<cmd>FzfLua registers<CR>'';
+      options = {
+        desc = "Find registers";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fs";
+      action = ''<cmd>FzfLua lsp_document_symbols<CR>'';
+      options = {
+        desc = "Find lsp document symbols";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fT";
+      action = ''<cmd>FzfLua colorschemes<CR>'';
+      options = {
+        desc = "Find theme";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fW";
+      action.__raw = ''
+        function()
+          require("telescope.builtin").live_grep {
+            additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+          }
+        end
+      '';
+      options = {
+        desc = "Find words in all files";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>fw";
+      action = "<cmd>FzfLua live_grep<CR>";
+      options = {
+        desc = "Live grep";
+      };
+    }
+  ];
+}
