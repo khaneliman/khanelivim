45c9625664e77dc48fe57b2e2e523d04ae8c21a4
Austin Horstman <khaneliman12@gmail.com>
2024-12-23 13:23:03 -0600

    blink: tbl extend instead of replace capabilities

--- packages/khanelivim/plugins/blink.nix
+++ packages/khanelivim/plugins/blink.nix
@@ -150,7 +150,7 @@
 
       lsp.capabilities = # Lua
         ''
-          capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+          capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
         '';
     })
   ];
