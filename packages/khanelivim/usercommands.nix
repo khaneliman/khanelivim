7c9af65d0e8fbb4894c21ca7225009f09ebec7af
Austin Horstman <khaneliman12@gmail.com>
2024-12-26 23:10:00 -0600

    usercommands: create PeekDefinition

--- packages/khanelivim/usercommands.nix
+++ packages/khanelivim/usercommands.nix
@@ -45,5 +45,28 @@
       '';
       desc = "Toggle automatic formatting on save";
     };
+
+    # Takes an argument that determines what method to use for buf_request
+    PeekDefinition = {
+      nargs = 1;
+      command.__raw = ''
+        function(opts)
+          local function preview_location_callback(_, result)
+            if result == nil or vim.tbl_isempty(result) then
+              vim.notify('No location found to preview')
+              return nil
+            end
+          local buf, _ = vim.lsp.util.preview_location(result[1])
+            if buf then
+              local cur_buf = vim.api.nvim_get_current_buf()
+              vim.bo[buf].filetype = vim.bo[cur_buf].filetype
+            end
+          end
+
+          local params = vim.lsp.util.make_position_params()
+          return vim.lsp.buf_request(0, opts.fargs[1], params, preview_location_callback)
+        end
+      '';
+    };
   };
 }
