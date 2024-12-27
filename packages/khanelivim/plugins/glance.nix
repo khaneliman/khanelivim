1c1ee9cc216fbffccb09c655e5ecd9d8e0ef27ff
Austin Horstman <khaneliman12@gmail.com>
2024-12-27 09:17:39 -0600

    glance: init

---
+++ packages/khanelivim/plugins/glance.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+{
+  # TODO: upstream module
+  extraPlugins = [ pkgs.vimPlugins.glance-nvim ];
+  extraConfigLua = ''
+    require('glance').setup()
+  '';
+
+  keymaps = [
+    {
+      action = "<CMD>Glance definitions<CR>";
+      mode = "n";
+      key = "<leader>lgd";
+      options = {
+        desc = "Glance definition";
+      };
+    }
+    {
+      action = "<CMD>Glance implementations<CR>";
+      mode = "n";
+      key = "<leader>lgi";
+      options = {
+        desc = "Glance implementation";
+      };
+    }
+    {
+      action = "<CMD>Glance references<CR>";
+      mode = "n";
+      key = "<leader>lgr";
+      options = {
+        desc = "Glance reference";
+      };
+    }
+    {
+      action = "<CMD>Glance type_definitions<CR>";
+      mode = "n";
+      key = "<leader>lgt";
+      options = {
+        desc = "Glance type definition";
+      };
+    }
+  ];
+}
