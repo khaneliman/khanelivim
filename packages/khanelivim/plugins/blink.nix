5b18413de386e4eef6e6679ed0bbcead08c3f6c0
Austin Horstman <khaneliman12@gmail.com>
2025-01-06 15:09:30 -0600

    blink: use mini icons

--- packages/khanelivim/plugins/blink.nix
+++ packages/khanelivim/plugins/blink.nix
@@ -48,6 +48,22 @@
                   }
                   { __unkeyed-1 = "source_name"; }
                 ];
+                components = {
+                  kind_icon = {
+                    ellipsis = false;
+                    text.__raw = ''
+                      function(ctx)
+                        local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return kind_icon
+                      end,
+                      -- Optionally, you may also use the highlights from mini.icons
+                      highlight = function(ctx)
+                        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return hl
+                      end
+                    '';
+                  };
+                };
               };
             };
           };
