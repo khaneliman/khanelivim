3e6d859a98ff814328c04dc19f62bc0961f4a292
Austin Horstman <khaneliman12@gmail.com>
2025-01-07 22:36:33 -0600

    mini: enable mini snippets

--- packages/khanelivim/plugins/mini.nix
+++ packages/khanelivim/plugins/mini.nix
@@ -1,3 +1,4 @@
+{ config, lib, ... }:
 {
   plugins = {
     mini = {
@@ -12,6 +13,14 @@
         git = { };
         icons = { };
         pairs = { };
+        snippets = {
+          snippets = {
+            __unkeyed-1.__raw =
+              lib.mkIf config.plugins.friendly-snippets.enable # Lua
+                "require('mini.snippets').gen_loader.from_file('${config.plugins.friendly-snippets.package}/snippets/global.json')";
+            __unkeyed-2.__raw = "require('mini.snippets').gen_loader.from_lang()";
+          };
+        };
       };
     };
   };
