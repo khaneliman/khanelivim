f5c9f67d66b16f175f14fe9c83477fb534fa0a8b
Austin Horstman <khaneliman12@gmail.com>
2025-01-09 23:36:28 -0600

    mini-pairs: add keymaps for toggles

--- modules/nixvim/plugins/mini-pairs.nix
+++ modules/nixvim/plugins/mini-pairs.nix
@@ -1,3 +1,4 @@
+{ config, lib, ... }:
 {
   plugins = {
     mini = {
@@ -8,4 +9,31 @@
       };
     };
   };
+
+  keymaps = lib.mkIf (config.plugins.mini.enable && lib.hasAttr "pairs" config.plugins.mini.modules) [
+    {
+      key = "<leader>up";
+      mode = "n";
+      action.__raw = ''
+        function ()
+          vim.b.minipairs_disable = not vim.b.minipairs_disable
+          vim.notify(string.format("Buffer AutoPairs %s", bool2str(not vim.b[0].minipairs_disable), "info"))
+        end'';
+      options = {
+        desc = "Buffer AutoPairs toggle";
+      };
+    }
+    {
+      key = "<leader>uP";
+      mode = "n";
+      action.__raw = ''
+        function ()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          vim.notify(string.format("Global AutoPairs %s", bool2str(not vim.g.minipairs_disable), "info"))
+        end'';
+      options = {
+        desc = "Global AutoPairs toggle";
+      };
+    }
+  ];
 }
