6dd941a1bcf998f6a8db48a5be1c1ec61faaec36
Austin Horstman <khaneliman12@gmail.com>
2025-01-12 21:30:01 -0600

    lsp: clangd add configuration

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -110,6 +110,27 @@
 
         clangd = {
           enable = true;
+
+          extraOptions = {
+            capabilities = {
+              offsetEncoding = [ "utf-16" ];
+            };
+
+            init_options = {
+              usePlaceholders = true;
+              completeUnimported = true;
+              clangdFileStatus = true;
+            };
+          };
+          cmd = [
+            "clangd"
+            "--background-index"
+            "--clang-tidy"
+            "--header-insertion=iwyu"
+            "--completion-style=detailed"
+            "--function-arg-placeholders"
+            "--fallback-style=llvm"
+          ];
         };
 
         csharp_ls = {
