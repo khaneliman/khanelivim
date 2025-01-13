88a3535a0426d644a231fe54ceea44c95197e9ac
Austin Horstman <khaneliman12@gmail.com>
2025-01-12 21:36:07 -0600

    lsp/harper-ls: move to separate module

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     ./lsp/clangd.nix
+    ./lsp/harper-ls.nix
   ];
 
   # TODO: migrate to mkneovimplugin
@@ -145,25 +146,6 @@
         gdscript = {
           enable = true;
           package = pkgs.gdtoolkit_4;
-        };
-
-        harper_ls = {
-          enable = true;
-          settings = {
-            "harper-ls" = {
-              # Defaults https://github.com/Automattic/harper/blob/7bdc753b54cec79702807fb81a8defafc4a3f1be/harper-core/src/linting/lint_group.rs#L152C1-L187C3
-              linters = {
-                boring_words = true;
-                linking_verbs = true;
-                # Rarely useful with coding
-                sentence_capitalization = false;
-                spell_check = false;
-              };
-              codeActions = {
-                forceStable = true;
-              };
-            };
-          };
         };
 
         helm_ls = {
