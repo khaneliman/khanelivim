f4c9d545d405fb2251b4cc9127bbe520455a801c
Austin Horstman <khaneliman12@gmail.com>
2025-01-12 21:42:20 -0600

    lsp/rust-analyzer: move to separate module

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -214,42 +214,6 @@ in
           enable = true;
         };
 
-        rust_analyzer = {
-          enable = !config.plugins.rustaceanvim.enable;
-          installCargo = true;
-          installRustc = true;
-
-          settings = {
-            diagnostics = {
-              enable = true;
-              # experimental.enable = true;
-              styleLints.enable = true;
-            };
-
-            files = {
-              excludeDirs = [
-                ".direnv"
-                "rust/.direnv"
-              ];
-            };
-
-            inlayHints = {
-              bindingModeHints.enable = true;
-              closureStyle = "rust_analyzer";
-              closureReturnTypeHints.enable = "always";
-              discriminantHints.enable = "always";
-              expressionAdjustmentHints.enable = "always";
-              implicitDrops.enable = true;
-              lifetimeElisionHints.enable = "always";
-              rangeExclusiveHints.enable = true;
-            };
-
-            procMacro = {
-              enable = true;
-            };
-          };
-        };
-
         sqls = {
           enable = true;
         };
