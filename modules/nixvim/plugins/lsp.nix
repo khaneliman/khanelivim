747aa9559360cb1a6311d4f3354509d236f9db02
Austin Horstman <khaneliman12@gmail.com>
2025-01-14 19:10:10 -0600

    lsp: use internal lib.readAllFiles

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -2,26 +2,11 @@
   config,
   lib,
   pkgs,
+  self,
   ...
 }:
-let
-  # Function to recursively read all files in a directory
-  readAllFiles =
-    dir:
-    let
-      entries = builtins.attrNames (builtins.readDir dir);
-      files = builtins.filter (entry: entry != "default.nix") entries;
-    in
-    builtins.concatMap (
-      entry:
-      let
-        path = "${dir}/${entry}";
-      in
-      if builtins.pathExists path && lib.pathIsDirectory path then readAllFiles path else [ path ]
-    ) files;
-in
 {
-  imports = readAllFiles ./lsp;
+  imports = self.lib.khanelivim.readAllFiles ./lsp;
 
   # TODO: migrate to mkneovimplugin
   extraConfigLuaPre = ''
