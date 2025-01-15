f974d16c6d1befa05aab578bc46900836a161ba1
Austin Horstman <khaneliman12@gmail.com>
2025-01-15 13:16:46 -0600

    snacks/zen: fix mkIf

--- modules/nixvim/plugins/snacks/zen.nix
+++ modules/nixvim/plugins/snacks/zen.nix
@@ -14,14 +14,21 @@
     };
   };
 
-  keymaps = lib.mkIf (config.plugins.snacks.enable && config.plugins.snacks.settings.zen.enabled) [
-    {
-      mode = "n";
-      key = "<leader>uZ";
-      action = "<cmd>lua Snacks.zen()<CR>";
-      options = {
-        desc = "Zen Toggle";
-      };
-    }
-  ];
+  keymaps =
+    lib.mkIf
+      (
+        config.plugins.snacks.enable
+        && lib.hasAttr "zen" config.plugins.snacks.settings
+        && config.plugins.snacks.settings.zen.enabled
+      )
+      [
+        {
+          mode = "n";
+          key = "<leader>uZ";
+          action = "<cmd>lua Snacks.zen()<CR>";
+          options = {
+            desc = "Zen Toggle";
+          };
+        }
+      ];
 }
