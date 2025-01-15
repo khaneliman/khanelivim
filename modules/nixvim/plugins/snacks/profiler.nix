23bca59eb9eb50bc4bc37751d3b0789a4ab0834b
Austin Horstman <khaneliman12@gmail.com>
2025-01-15 13:16:51 -0600

    snacks/profiler: fix mkIf

--- modules/nixvim/plugins/snacks/profiler.nix
+++ modules/nixvim/plugins/snacks/profiler.nix
@@ -5,24 +5,30 @@
   ...
 }:
 {
-  extraConfigLuaPre = lib.mkIf config.plugins.snacks.enable (
-    lib.mkOrder 1 (
-      lib.optionalString config.plugins.snacks.settings.profiler.enabled # Lua
-        ''
-          if vim.env.PROF then
-            local snacks = "${pkgs.vimPlugins.snacks-nvim}"
-            vim.opt.rtp:append(snacks)
-            require("snacks.profiler").startup({
-              startup = {
-                -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
-                event = "UIEnter",
-                -- event = "VeryLazy",
-              },
-            })
-          end
-        ''
-    )
-  );
+  extraConfigLuaPre =
+    lib.mkIf
+      (
+        config.plugins.snacks.enable
+        && lib.hasAttr "profiler" config.plugins.snacks.settings
+        && config.plugins.snacks.settings.profiler.enabled
+      )
+      (
+        lib.mkOrder 1 # Lua
+          ''
+            if vim.env.PROF then
+              local snacks = "${pkgs.vimPlugins.snacks-nvim}"
+              vim.opt.rtp:append(snacks)
+              require("snacks.profiler").startup({
+                startup = {
+                  -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
+                  event = "UIEnter",
+                  -- event = "VeryLazy",
+                },
+              })
+            end
+          ''
+
+      );
 
   plugins = {
     snacks = {
