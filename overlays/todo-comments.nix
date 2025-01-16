164680cfc5ffbe9d4de37e83b9b741fb07c0ea05
Austin Horstman <khaneliman12@gmail.com>
2025-01-16 17:56:33 -0600

    overlays/todo-comments: update

--- overlays/todo-comments.nix
+++ overlays/todo-comments.nix
@@ -1,13 +1,13 @@
 _: _final: prev: {
   vimPlugins = prev.vimPlugins // {
-    todo-comments-nvim = prev.vimPlugins.todo-comments-nvim.overrideAttrs (_oldAttrs: {
-      version = "2025-01-16";
+    todo-comments-nvim = prev.vimPlugins.todo-comments-nvim.overrideAttrs {
+      version = "2025-01-14";
       src = prev.fetchFromGitHub {
         owner = "folke";
         repo = "todo-comments.nvim";
         rev = "304a8d204ee787d2544d8bc23cd38d2f929e7cc5";
-        sha256 = "at9OSBtQqyiDdxKdNn2x6z4k8xrDD90sACKEK7uKNUM=";
+        sha256 = "0hrmiaxjp11200nds3y33brj8gpbn5ykd78jfy1jiash3d44xpva";
       };
-    });
+    };
   };
 }
