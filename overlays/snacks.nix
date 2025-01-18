4ae786f4c4aaf7910bb23b1ae3a1067db8abc22c
Austin Horstman <khaneliman12@gmail.com>
2025-01-18 00:48:53 -0600

    overlays/snacks: bump

--- overlays/snacks.nix
+++ overlays/snacks.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   vimPlugins = prev.vimPlugins // {
     snacks-nvim = prev.vimPlugins.snacks-nvim.overrideAttrs (oldAttrs: {
-      version = "2025-01-17";
+      version = "2025-01-18";
       src = prev.fetchFromGitHub {
         owner = "folke";
         repo = "snacks.nvim";
-        rev = "706b1abc1697ca050314dc667e0900d53cad8aa4";
-        sha256 = "sha256-yqdHNqBu0KByIhmCD/Gnt6T+CQ6H/iUT7X/kPoJ0esM=";
+        rev = "74feefc52284e2ebf93ad815ec5aaeec918d4dc2";
+        sha256 = "sha256-TM/d8PSatlnQNw1DA8QgJaAcnjugNAj5Uim87W7vwjM=";
       };
       nvimSkipModule = oldAttrs.nvimSkipModule ++ [
         "snacks.picker.config.highlights"
