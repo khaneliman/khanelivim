708e871cd47ca646278d14532cfb32a984b8aeab
Austin Horstman <khaneliman12@gmail.com>
2025-01-20 11:59:20 -0600

    iron: fix key conflicts

--- modules/nixvim/plugins/iron/default.nix
+++ modules/nixvim/plugins/iron/default.nix
@@ -36,20 +36,20 @@
           -- Iron doesn't set keymaps by default anymore.
           -- You can set them here or manually add keymaps to the functions in iron.core
           keymaps = {
-            send_motion = "<space>sc",
-            visual_send = "<space>sc",
+            send_motion = "<space>sm",
+            visual_send = "<space>sv",
             send_file = "<space>sf",
             send_line = "<space>sl",
             send_paragraph = "<space>sp",
             send_until_cursor = "<space>su",
-            send_mark = "<space>sm",
+            send_mark = "<space>ms",
             mark_motion = "<space>mc",
             mark_visual = "<space>mc",
             remove_mark = "<space>md",
             cr = "<space>s<cr>",
             interrupt = "<space>s<space>",
             exit = "<space>sq",
-            clear = "<space>cl",
+            clear = "<space>sc",
           },
           -- If the highlight is on, you can change how it looks
           -- For the available options, check nvim_set_hl
