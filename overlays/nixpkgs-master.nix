c6942acdafead11a48dd7db16f66debb62231b67
Austin Horstman <khaneliman12@gmail.com>
2025-01-19 19:16:45 -0600

    overlays/nixpkgs-master: add comments

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -1,12 +1,16 @@
 { flake }:
 _self: super: {
   inherit (flake.inputs.nixpkgs-master.legacyPackages.${super.stdenv.system})
+    # Fixes codelldb
     llvmPackages_14
     llvmPackages_15
     llvmPackages_16
     llvmPackages_17
+    # Fix ruff
     ruff
+    # Fixes hard freeze
     tree-sitter
+    # Latest bump
     vimPlugins
     ;
 }
