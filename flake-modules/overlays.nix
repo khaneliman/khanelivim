b1032035fe4fa817af4beb21cd4999ad670bf084
Austin Horstman <khaneliman12@gmail.com>
2025-01-19 19:12:13 -0600

    flake-modules/overlays: inline reading all files

--- flake-modules/overlays.nix
+++ flake-modules/overlays.nix
@@ -1,9 +1,19 @@
 { inputs, lib, ... }:
+let
+  children =
+    dir:
+    with builtins;
+    lib.pipe dir [
+      readDir
+      attrNames
+      (map (name: dir + "/${name}"))
+    ];
+in
 {
   flake.overlays = lib.listToAttrs (
     map (file: {
-      name = "${lib.removeSuffix ".nix" (builtins.hashString "sha256" file)}-overlay";
+      name = "${lib.removeSuffix ".nix" (builtins.hashString "sha256" (builtins.readFile file))}-overlay";
       value = import file { flake = inputs.self; };
-    }) (inputs.self.lib.khanelivim.readAllFiles ../overlays)
+    }) (children ../overlays)
   );
 }
