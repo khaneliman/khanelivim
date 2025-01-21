6bb316f9ebe2023382e0b4ac653950b97f99877f
Austin Horstman <khaneliman12@gmail.com>
2025-01-20 21:45:49 -0600

    overseer: add lazy load trigger for compiler

--- modules/nixvim/plugins/overseer/default.nix
+++ modules/nixvim/plugins/overseer/default.nix
@@ -1,23 +1,28 @@
+{ lib, config, ... }:
 {
   plugins = {
     overseer = {
       enable = true;
 
-      lazyLoad.settings.cmd = [
-        "OverseerOpen"
-        "OverseerClose"
-        "OverseerToggle"
-        "OverseerSaveBundle"
-        "OverseerLoadBundle"
-        "OverseerDeleteBundle"
-        "OverseerRunCmd"
-        "OverseerRun"
-        "OverseerInfo"
-        "OverseerBuild"
-        "OverseerQuickAction"
-        "OverseerTaskAction"
-        "OverseerClearCache"
-      ];
+      lazyLoad.settings.cmd =
+        [
+          "OverseerOpen"
+          "OverseerClose"
+          "OverseerToggle"
+          "OverseerSaveBundle"
+          "OverseerLoadBundle"
+          "OverseerDeleteBundle"
+          "OverseerRunCmd"
+          "OverseerRun"
+          "OverseerInfo"
+          "OverseerBuild"
+          "OverseerQuickAction"
+          "OverseerTaskAction"
+          "OverseerClearCache"
+        ]
+        ++ lib.optionals config.plugins.compiler.enable [
+          "CompilerOpen"
+        ];
     };
   };
 }
