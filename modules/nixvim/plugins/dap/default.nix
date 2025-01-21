8b3776c9e3f0eba53b5e3dfa4968d55f28f36e47
Austin Horstman <khaneliman12@gmail.com>
2025-01-20 21:48:23 -0600

    dap: add dap-view

--- modules/nixvim/plugins/dap/default.nix
+++ modules/nixvim/plugins/dap/default.nix
@@ -78,6 +78,19 @@ let
     env = { };
     terminalKind = "integrated";
   };
+
+  nvim-dap-view = pkgs.vimUtils.buildVimPlugin {
+    pname = "nvim-dap-view";
+    version = "2025-01-19";
+    src = pkgs.fetchFromGitHub {
+      owner = "igorlfs";
+      repo = "nvim-dap-view";
+      rev = "b807d96c3c5ceaeacde7bb482135365827341201";
+      sha256 = "08m5mr9zs4drkilqc7albl8fvmknx84j8gzgbyml25ghqhffwddl";
+    };
+    dependencies = [ pkgs.vimPlugins.nvim-dap ];
+    meta.homepage = "https://github.com/igorlfs/nvim-dap-view/";
+  };
 in
 {
   extraPackages =
@@ -91,6 +104,10 @@ in
       pkgs.gdb
       pkgs.bashdb
     ];
+
+  extraPlugins = [
+    nvim-dap-view
+  ];
 
   #   extraPlugins = with pkgs.vimPlugins; [ nvim-gdb ];
 
@@ -232,7 +249,8 @@ in
 
       extensions = {
         dap-ui = {
-          enable = true;
+          # Trying out dap-view
+          # enable = true;
         };
 
         dap-virtual-text = {
@@ -398,6 +416,22 @@ in
           function()
             require('dap.ext.vscode').load_launchjs(nil, {})
             require("dapui").toggle()
+          end
+        '';
+        options = {
+          desc = "Toggle Debugger UI";
+          silent = true;
+        };
+      }
+    ]
+    ++ lib.optionals (builtins.elem nvim-dap-view config.extraPlugins) [
+      {
+        mode = "n";
+        key = "<leader>du";
+        action.__raw = ''
+          function()
+            require('dap.ext.vscode').load_launchjs(nil, {})
+            require("dap-view").toggle()
           end
         '';
         options = {
