2ec1145003c5ec7a6325f4fb0b57ac3b6c6aa752
Austin Horstman <khaneliman12@gmail.com>
2025-01-21 19:05:53 -0600

    dap: add dap-lldb

--- modules/nixvim/plugins/dap/default.nix
+++ modules/nixvim/plugins/dap/default.nix
@@ -286,6 +286,14 @@ in
       };
     };
 
+    dap-lldb = {
+      enable = true;
+
+      settings = {
+        codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+      };
+    };
+
     which-key.settings.spec = lib.optionals config.plugins.dap.enable [
       {
         __unkeyed-1 = "<leader>d";
