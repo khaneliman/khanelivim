4d4e15a6e8046e5c3e5b63cb598c37adf6bcaf07
Austin Horstman <khaneliman12@gmail.com>
2025-01-21 21:26:37 -0600

    overlays/nixpkgs-master: remove llvmPackages

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -1,11 +1,6 @@
 { flake }:
 _self: super: {
   inherit (flake.inputs.nixpkgs-master.legacyPackages.${super.stdenv.system})
-    # Fixes codelldb
-    llvmPackages_14
-    llvmPackages_15
-    llvmPackages_16
-    llvmPackages_17
     # Latest bump
     vimPlugins
     tree-sitter
