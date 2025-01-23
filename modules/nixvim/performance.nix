7ed48c8453bd31ce124da62e379c52e92d5b9319
Austin Horstman <khaneliman12@gmail.com>
2025-01-23 10:59:01 -0600

    performance: disable with lazy loading and snacks picker
    Snacks picker can't handle configs and plugins being byte compiled.
    Disable those when using it.
    
    Also, try not byte compiling anything when using lazy loading to see raw
    lazy loading performance.

--- modules/nixvim/performance.nix
+++ modules/nixvim/performance.nix
@@ -1,10 +1,19 @@
+{ config, lib, ... }:
+let
+  snacksPickerEnabled =
+    config.plugins.snacks.enable
+    && lib.hasAttr "profiler" config.plugins.snacks.settings
+    && config.plugins.snacks.settings.profiler.enabled;
+
+  lazyEnabled = config.plugins.lz-n.enable || config.plugins.lazy.enable;
+in
 {
   performance = {
     byteCompileLua = {
-      enable = true;
-      nvimRuntime = true;
-      configs = true;
-      plugins = true;
+      enable = !lazyEnabled;
+      nvimRuntime = !lazyEnabled;
+      configs = !snacksPickerEnabled;
+      plugins = !snacksPickerEnabled;
     };
   };
 }
