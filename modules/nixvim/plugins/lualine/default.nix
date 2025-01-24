28adbb6fa5afd051a32bcadc160bf2aad4bad681
Austin Horstman <khaneliman12@gmail.com>
2025-01-23 19:12:16 -0600

    lualine: ignore more filetypes

--- modules/nixvim/plugins/lualine/default.nix
+++ modules/nixvim/plugins/lualine/default.nix
@@ -22,6 +22,16 @@ in
         disabled_filetypes = {
           __unkeyed-1 = "startify";
           __unkeyed-2 = "neo-tree";
+          __unkeyed-3 = "copilot-chat";
+          __unkeyed-4 = "ministarter";
+          __unkeyed-5 = "Avante";
+          __unkeyed-6 = "AvanteInput";
+          __unkeyed-7 = "trouble";
+          __unkeyed-8 = "dapui_scopes";
+          __unkeyed-9 = "dapui_breakpoints";
+          __unkeyed-10 = "dapui_stacks";
+          __unkeyed-11 = "dapui_watches";
+          __unkeyed-12 = "dapui_console";
           winbar = [
             "aerial"
             "dap-repl"
