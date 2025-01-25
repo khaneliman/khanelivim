5355c18ae3db18855ed739c88fa16e0530294add
Austin Horstman <khaneliman12@gmail.com>
2025-01-25 16:08:56 -0600

    overlays/nixpkgs-master: remove luaPackages

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -2,13 +2,5 @@
 _final: super: {
   inherit (flake.inputs.nixpkgs-master.legacyPackages.${super.stdenv.system})
     vimPlugins
-    # TODO: remove after https://nixpk.gs/pr-tracker.html?pr=375948 reaches nixpkgs-unstable
-    luaPackages
-
-    lua51Packages
-    lua52Packages
-    lua53Packages
-    lua54Packages
-    luajitPackage
     ;
 }
