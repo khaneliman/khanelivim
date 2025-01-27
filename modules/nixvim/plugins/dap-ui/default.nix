6e6f967da0050fef893b33ef870bc5cdf46a782f
Austin Horstman <khaneliman12@gmail.com>
2025-01-27 13:04:34 -0600

    dap-ui: break off from dap

---
+++ modules/nixvim/plugins/dap-ui/default.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  plugins = {
+    dap-ui = {
+      enable = true;
+
+      lazyLoad.settings = {
+        before.__raw = ''
+          function()
+            require('lz.n').trigger_load('nvim-dap')
+          end
+        '';
+        keys = [
+          {
+            __unkeyed-1 = "<leader>du";
+            __unkeyed-2.__raw = ''
+              function()
+                require('dap.ext.vscode').load_launchjs(nil, {})
+                require("dapui").toggle()
+              end
+            '';
+            desc = "Toggle Debugger UI";
+          }
+        ];
+      };
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.dap.enable [
+      {
+        __unkeyed-1 = "<leader>d";
+        mode = "n";
+        desc = "Debug";
+        # icon = " ";
+      }
+    ];
+  };
+
+  keymaps = lib.optionals config.plugins.dap-ui.enable [
+    {
+      mode = "v";
+      key = "<leader>de";
+      action.__raw = ''
+        function() require("dapui").eval() end
+      '';
+      options = {
+        desc = "Evaluate Input";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>de";
+      action.__raw = ''
+        function()
+          vim.ui.input({ prompt = "Expression: " }, function(expr)
+            if expr then require("dapui").eval(expr, { enter = true }) end
+          end)
+        end
+      '';
+      options = {
+        desc = "Evaluate Input";
+        silent = true;
+      };
+    }
+    (lib.mkIf (!config.plugins.lz-n.enable) {
+      mode = "n";
+      key = "<leader>du";
+      action.__raw = ''
+        function()
+          require('dap.ext.vscode').load_launchjs(nil, {})
+          require("dapui").toggle()
+        end
+      '';
+      options = {
+        desc = "Toggle Debugger UI";
+        silent = true;
+      };
+    })
+  ];
+}
--- modules/nixvim/plugins/dap/default.nix
+++ modules/nixvim/plugins/dap/default.nix
@@ -281,30 +281,6 @@ in
       };
     };
 
-    dap-ui = {
-      enable = true;
-
-      lazyLoad.settings = {
-        before.__raw = ''
-          function()
-            require('lz.n').trigger_load('nvim-dap')
-          end
-        '';
-        keys = [
-          {
-            __unkeyed-1 = "<leader>du";
-            __unkeyed-2.__raw = ''
-              function()
-                require('dap.ext.vscode').load_launchjs(nil, {})
-                require("dapui").toggle()
-              end
-            '';
-            desc = "Toggle Debugger UI";
-          }
-        ];
-      };
-    };
-
     which-key.settings.spec = lib.optionals config.plugins.dap.enable [
       {
         __unkeyed-1 = "<leader>d";
@@ -383,69 +359,26 @@ in
         };
       }
     ]
-    ++ lib.optionals config.plugins.dap-ui.enable [
-      {
-        mode = "v";
-        key = "<leader>de";
-        action.__raw = ''
-          function() require("dapui").eval() end
-        '';
-        options = {
-          desc = "Evaluate Input";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>de";
-        action.__raw = ''
-          function()
-            vim.ui.input({ prompt = "Expression: " }, function(expr)
-              if expr then require("dapui").eval(expr, { enter = true }) end
-            end)
-          end
-        '';
-        options = {
-          desc = "Evaluate Input";
-          silent = true;
-        };
-      }
-      (lib.mkIf (!config.plugins.lz-n.enable) {
-        mode = "n";
-        key = "<leader>du";
-        action.__raw = ''
-          function()
-            require('dap.ext.vscode').load_launchjs(nil, {})
-            require("dapui").toggle()
-          end
-        '';
-        options = {
-          desc = "Toggle Debugger UI";
-          silent = true;
-        };
-      })
-    ]
-    ++
-      lib.optionals
-        (
-          (builtins.elem nvim-dap-view config.extraPlugins)
-          && !config.plugins.dap.extensions.dap-ui.enable
-          && !config.plugins.lz-n.enable
-        )
-        [
-          {
-            mode = "n";
-            key = "<leader>du";
-            action.__raw = ''
-              function()
-                require('dap.ext.vscode').load_launchjs(nil, {})
-                require("dap-view").toggle()
-              end
-            '';
-            options = {
-              desc = "Toggle Debugger UI";
-              silent = true;
-            };
-          }
-        ];
+    ++ lib.optionals
+      (
+        (builtins.elem nvim-dap-view config.extraPlugins)
+        && !config.plugins.dap.extensions.dap-ui.enable
+        && !config.plugins.lz-n.enable
+      )
+      [
+        {
+          mode = "n";
+          key = "<leader>du";
+          action.__raw = ''
+            function()
+              require('dap.ext.vscode').load_launchjs(nil, {})
+              require("dap-view").toggle()
+            end
+          '';
+          options = {
+            desc = "Toggle Debugger UI";
+            silent = true;
+          };
+        }
+      ];
 }
