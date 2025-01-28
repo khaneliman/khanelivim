60798b1fb560ece7028d765d70df4f69e8f6db71
Austin Horstman <khaneliman12@gmail.com>
2025-01-28 08:23:55 -0600

    performance: enable again
    Not freezing anymore on profiler and lazy loading seems unaffected.

--- modules/nixvim/performance.nix
+++ modules/nixvim/performance.nix
@@ -1,19 +1,10 @@
-{ config, lib, ... }:
-let
-  snacksPickerEnabled =
-    config.plugins.snacks.enable
-    && lib.hasAttr "profiler" config.plugins.snacks.settings
-    && config.plugins.snacks.settings.profiler.enabled;
-
-  lazyEnabled = config.plugins.lz-n.enable || config.plugins.lazy.enable;
-in
 {
   performance = {
     byteCompileLua = {
-      enable = !lazyEnabled;
-      nvimRuntime = !lazyEnabled;
-      configs = !snacksPickerEnabled;
-      plugins = !snacksPickerEnabled;
+      enable = true;
+      nvimRuntime = true;
+      configs = true;
+      plugins = true;
     };
   };
 }
