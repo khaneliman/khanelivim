3a77f383d5ec4c21f1c610ebfdbcace79493c3f1
Austin Horstman <khaneliman12@gmail.com>
2025-01-28 08:22:12 -0600

    markview: override to use latest

--- modules/nixvim/plugins/markview/default.nix
+++ modules/nixvim/plugins/markview/default.nix
@@ -1,26 +1,35 @@
+{ pkgs, ... }:
 {
   plugins.markview = {
     enable = true;
-
-    lazyLoad = {
-      settings = {
-        ft = "markdown";
+    package = pkgs.vimPlugins.markview-nvim.overrideAttrs {
+      version = "2025-01-27";
+      src = pkgs.fetchFromGitHub {
+        owner = "OXY2DEV";
+        repo = "markview.nvim";
+        rev = "81b40bd8c8c9e239bd14f7dace29f64fe20cbb98";
+        sha256 = "1rc4l1gwva0qhdhdnqn2l1nmwh8s86vij45j27xkq9fgxpffbx2i";
+        fetchSubmodules = true;
       };
+      doCheck = false;
     };
+
     settings = {
       buf_ignore = [ ];
 
-      modes = [
-        "n"
-        "x"
-        "i"
-        "r"
-      ];
+      preview = {
+        modes = [
+          "n"
+          "x"
+          "i"
+          "r"
+        ];
 
-      hybrid_modes = [
-        "i"
-        "r"
-      ];
+        hybrid_modes = [
+          "i"
+          "r"
+        ];
+      };
     };
   };
 }
