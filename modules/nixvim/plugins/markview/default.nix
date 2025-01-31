b0e8b768b8cdfe912f6050c63a66e41f37a62ae3
Austin Horstman <khaneliman12@gmail.com>
2025-01-30 20:21:06 -0600

    markview: bump to latest

--- modules/nixvim/plugins/markview/default.nix
+++ modules/nixvim/plugins/markview/default.nix
@@ -3,12 +3,12 @@
   plugins.markview = {
     enable = true;
     package = pkgs.vimPlugins.markview-nvim.overrideAttrs {
-      version = "2025-01-27";
+      version = "2025-01-30";
       src = pkgs.fetchFromGitHub {
         owner = "OXY2DEV";
         repo = "markview.nvim";
-        rev = "81b40bd8c8c9e239bd14f7dace29f64fe20cbb98";
-        sha256 = "1rc4l1gwva0qhdhdnqn2l1nmwh8s86vij45j27xkq9fgxpffbx2i";
+        rev = "f933b4597738fec4014d25f11511bcbe2d1e1a32";
+        hash = "sha256-V3imWAzPtlrC89CYigDvnye12CctM7RJioigc57Rn/8=";
         fetchSubmodules = true;
       };
       doCheck = false;
