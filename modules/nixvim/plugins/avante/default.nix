c407a004844ed0393fe3ee269c563ef8a132bdee
Austin Horstman <khaneliman12@gmail.com>
2025-01-31 12:49:14 -0600

    avante: use git plugin

--- modules/nixvim/plugins/avante/default.nix
+++ modules/nixvim/plugins/avante/default.nix
@@ -1,22 +1,24 @@
 {
   config,
   lib,
-  pkgs,
+  self,
+  system,
   ...
 }:
 {
   plugins = {
     avante = {
       enable = true;
-      package = pkgs.vimPlugins.avante-nvim.overrideAttrs {
-        patches = [
-          # Patch blink support
-          (pkgs.fetchpatch {
-            url = "https://github.com/doodleEsc/avante.nvim/commit/a5438d0f16208b7ae9e97ae354bed5ec16b4f9ed.patch";
-            hash = "sha256-KyfO9dE27yMXOQhpit7jmzkvnfM7b5kr2Acoh011lXA=";
-          })
-        ];
-      };
+      package = self.packages.${system}.avante-nvim;
+      # package = pkgs.vimPlugins.avante-nvim.overrideAttrs {
+      #   patches = [
+      #     # Patch blink support
+      #     (pkgs.fetchpatch {
+      #       url = "https://github.com/doodleEsc/avante.nvim/commit/a5438d0f16208b7ae9e97ae354bed5ec16b4f9ed.patch";
+      #       hash = "sha256-KyfO9dE27yMXOQhpit7jmzkvnfM7b5kr2Acoh011lXA=";
+      #     })
+      #   ];
+      # };
 
       lazyLoad.settings.event = [ "DeferredUIEnter" ];
 
