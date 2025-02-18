d78e3b28a3d3067f7e8027dee79eb9177c9eb5ae
Austin Horstman <khaneliman12@gmail.com>
2025-02-18 08:03:51 -0600

    lint: flatten

--- modules/nixvim/plugins/lint/default.nix
+++ modules/nixvim/plugins/lint/default.nix
@@ -47,73 +47,29 @@
       };
 
       linters = {
-        biomejs = {
-          cmd = lib.getExe pkgs.biome;
-        };
-        checkmake = {
-          cmd = lib.getExe pkgs.checkmake;
-        };
-        checkstyle = {
-          cmd = lib.getExe pkgs.checkstyle;
-        };
-        clangtidy = {
-          cmd = lib.getExe' pkgs.clang-tools "clang-tidy";
-        };
-        cmakelint = {
-          cmd = lib.getExe' pkgs.cmake-format "cmake-lint";
-        };
-        clippy = {
-          cmd = lib.getExe pkgs.rust-analyzer;
-        };
-        deadnix = {
-          cmd = lib.getExe pkgs.deadnix;
-        };
-        fish = {
-          cmd = lib.getExe pkgs.fish;
-        };
-        gdlint = {
-          cmd = lib.getExe' pkgs.gdtoolkit_4 "gdlint";
-        };
-        golangcilint = {
-          cmd = lib.getExe pkgs.golangci-lint;
-        };
-        htmlhint = {
-          cmd = lib.getExe pkgs.htmlhint;
-        };
-        jsonlint = {
-          cmd = lib.getExe pkgs.nodePackages.jsonlint;
-        };
-        luacheck = {
-          cmd = lib.getExe pkgs.luaPackages.luacheck;
-        };
-        markdownlint = {
-          cmd = lib.getExe pkgs.markdownlint-cli;
-        };
-        nix = {
-          cmd = lib.getExe' pkgs.nix "nix-instantiate";
-        };
-        pylint = {
-          cmd = lib.getExe pkgs.pylint;
-        };
-        shellcheck = {
-          cmd = lib.getExe pkgs.shellcheck;
-        };
-        sqlfluff = {
-          cmd = lib.getExe pkgs.sqlfluff;
-        };
-        statix = {
-          cmd = lib.getExe pkgs.statix;
-        };
-        stylelint = {
-          cmd = lib.getExe pkgs.stylelint;
-        };
+        biomejs.cmd = lib.getExe pkgs.biome;
+        checkmake.cmd = lib.getExe pkgs.checkmake;
+        checkstyle.cmd = lib.getExe pkgs.checkstyle;
+        clangtidy.cmd = lib.getExe' pkgs.clang-tools "clang-tidy";
+        clippy.cmd = lib.getExe pkgs.rust-analyzer;
+        cmakelint.cmd = lib.getExe' pkgs.cmake-format "cmake-lint";
+        deadnix.cmd = lib.getExe pkgs.deadnix;
+        fish.cmd = lib.getExe pkgs.fish;
+        gdlint.cmd = lib.getExe' pkgs.gdtoolkit_4 "gdlint";
+        golangcilint.cmd = lib.getExe pkgs.golangci-lint;
+        htmlhint.cmd = lib.getExe pkgs.htmlhint;
+        jsonlint.cmd = lib.getExe pkgs.nodePackages.jsonlint;
+        luacheck.cmd = lib.getExe pkgs.luaPackages.luacheck;
+        markdownlint.cmd = lib.getExe pkgs.markdownlint-cli;
+        nix.cmd = lib.getExe' pkgs.nix "nix-instantiate";
+        pylint.cmd = lib.getExe pkgs.pylint;
+        shellcheck.cmd = lib.getExe pkgs.shellcheck;
+        sqlfluff.cmd = lib.getExe pkgs.sqlfluff;
+        statix.cmd = lib.getExe pkgs.statix;
+        stylelint.cmd = lib.getExe pkgs.stylelint;
         # FIXME: throwing error for some reason
-        # swiftlint = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-        #   cmd = lib.getExe pkgs.swiftlint;
-        # };
-        yamllint = {
-          cmd = lib.getExe pkgs.yamllint;
-        };
+        # swiftlint.cmd = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin (lib.getExe pkgs.swiftlint);
+        yamllint.cmd = lib.getExe pkgs.yamllint;
       };
     };
   };
