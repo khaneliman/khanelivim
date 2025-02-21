b8618fed0f1980a7bcdfec78ba1f3e4390cff93b
Austin Horstman <khaneliman12@gmail.com>
2025-02-20 23:12:55 -0600

    typescript-tools: expose all code actions

--- modules/nixvim/plugins/typescript-tools/default.nix
+++ modules/nixvim/plugins/typescript-tools/default.nix
@@ -13,15 +13,16 @@
 
       settings = {
         settings = {
-          separate_diagnostic_server = true;
-          publish_diagnostic_on = "insert_leave";
-          tsserver_path = "${pkgs.typescript}/lib/node_modules/typescript/lib/tsserver.js";
-          tsserver_max_memory = "auto";
-          tsserver_locale = "en";
-          complete_function_calls = false;
-          include_completions_with_insert_text = true;
           code_lens = "off";
+          complete_function_calls = false;
           disable_member_code_lens = true;
+          expose_as_code_action = "all";
+          include_completions_with_insert_text = true;
+          publish_diagnostic_on = "insert_leave";
+          separate_diagnostic_server = true;
+          tsserver_locale = "en";
+          tsserver_max_memory = "auto";
+          tsserver_path = "${pkgs.typescript}/lib/node_modules/typescript/lib/tsserver.js";
           jsx_close_tag = {
             enable = false;
             filetypes = [
