af3413b849cf7b1ea78794df7b4ab95405232c2a
Austin Horstman <khaneliman12@gmail.com>
2025-02-28 09:00:16 -0500

    whitespace: use upstream pkg

--- modules/nixvim/plugins/whitespace/default.nix
+++ modules/nixvim/plugins/whitespace/default.nix
@@ -2,23 +2,9 @@
   pkgs,
   ...
 }:
-let
-  whitespace-nvim = pkgs.vimUtils.buildVimPlugin {
-    pname = "whitespace.nvim";
-    version = "2024-08-11";
-    src = pkgs.fetchFromGitHub {
-      owner = "johnfrankmorgan";
-      repo = "whitespace.nvim";
-      rev = "406cd69216dd7847b0cb38486603a8ed5c4f8c77";
-      sha256 = "sha256-LSK8Im42z8mvS/WbDQfE+ytl0wUYYpRDvAbO83OqGa8=";
-    };
-    meta.homepage = "https://github.com/johnfrankmorgan/whitespace.nvim/";
-  };
-in
 {
   extraPlugins = [
-    # pkgs.vimPlugins.whitespace-nvim
-    whitespace-nvim
+    pkgs.vimPlugins.whitespace-nvim
   ];
 
   # TODO: upstream module
