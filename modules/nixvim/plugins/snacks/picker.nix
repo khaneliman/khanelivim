aab0f0ee09472d593e875a240d99345b9e297417
Austin Horstman <khaneliman12@gmail.com>
2025-03-10 14:44:03 -0500

    snacks/picker: move profiler keymap

--- modules/nixvim/plugins/snacks/picker.nix
+++ modules/nixvim/plugins/snacks/picker.nix
@@ -180,7 +180,7 @@
           # Profiler
           {
             mode = "n";
-            key = "<leader>D";
+            key = "<leader>X";
             action = ''<cmd>lua Snacks.profiler.toggle()<cr>'';
             options = {
               desc = "Toggle Neovim profiler";
