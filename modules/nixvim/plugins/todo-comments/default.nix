937fa3feaaba152d27671bc419faee1dcd633ad1
Austin Horstman <khaneliman12@gmail.com>
2025-03-14 18:23:17 -0500

    todo-comments: disable lazy loading
    Want the TODO highlights.

--- modules/nixvim/plugins/todo-comments/default.nix
+++ modules/nixvim/plugins/todo-comments/default.nix
@@ -3,47 +3,52 @@
   plugins.todo-comments = {
     enable = true;
 
-    lazyLoad.settings = {
-      # TODO: see if we can split this up somehow
-      before.__raw =
-        ''
-          function()
-        ''
-        + lib.optionalString config.plugins.fzf-lua.enable ''
-          require('lz.n').trigger_load('fzf-lua')
-        ''
-        + lib.optionalString config.plugins.trouble.enable ''
-          require('lz.n').trigger_load('trouble.nvim')
-        ''
-        + lib.optionalString config.plugins.telescope.enable ''
-          require('lz.n').trigger_load('telescope')
-        ''
-        +
-          lib.optionalString
-            (config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings)
-            ''
-              require('lz.n').trigger_load('snacks.nvim')
-            ''
-        + ''
-          end
-        '';
-      keys =
-        lib.mkIf (config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings)
+    lazyLoad = {
+      # NOTE: Plugin highlighting feature not used until loaded.
+      # Will just not lazy load since I love the highlighting feature.
+      enable = false;
+      settings = {
+        # TODO: see if we can split this up somehow
+        before.__raw =
+          ''
+            function()
+          ''
+          + lib.optionalString config.plugins.fzf-lua.enable ''
+            require('lz.n').trigger_load('fzf-lua')
+          ''
+          + lib.optionalString config.plugins.trouble.enable ''
+            require('lz.n').trigger_load('trouble.nvim')
+          ''
+          + lib.optionalString config.plugins.telescope.enable ''
+            require('lz.n').trigger_load('telescope')
+          ''
+          +
+            lib.optionalString
+              (config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings)
+              ''
+                require('lz.n').trigger_load('snacks.nvim')
+              ''
+          + ''
+            end
+          '';
+        keys =
+          lib.mkIf (config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings)
+            [
+              {
+                __unkeyed-1 = "<leader>ft";
+                __unkeyed-2 = ''<CMD>lua Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" }})<CR>'';
+                desc = "Find TODOs";
+              }
+            ];
+        cmd =
           [
-            {
-              __unkeyed-1 = "<leader>ft";
-              __unkeyed-2 = ''<CMD>lua Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" }})<CR>'';
-              desc = "Find TODOs";
-            }
-          ];
-      cmd =
-        [
-          "TodoLocList"
-          "TodoQuickFix"
-        ]
-        ++ lib.optional config.plugins.fzf-lua.enable "TodoFzfLua"
-        ++ lib.optional config.plugins.telescope.enable "TodoTelescope"
-        ++ lib.optional config.plugins.trouble.enable "TodoTrouble";
+            "TodoLocList"
+            "TodoQuickFix"
+          ]
+          ++ lib.optional config.plugins.fzf-lua.enable "TodoFzfLua"
+          ++ lib.optional config.plugins.telescope.enable "TodoTelescope"
+          ++ lib.optional config.plugins.trouble.enable "TodoTrouble";
+      };
     };
 
     keymaps = {
