bab06888a19d774073b44686d287839695805696
Austin Horstman <khaneliman12@gmail.com>
2025-03-19 09:28:56 -0500

    markview: v25 update

--- modules/nixvim/plugins/markview/default.nix
+++ modules/nixvim/plugins/markview/default.nix
@@ -1,25 +1,13 @@
-{ pkgs, ... }:
 {
   plugins.markview = {
     enable = true;
-    package = pkgs.vimPlugins.markview-nvim.overrideAttrs {
-      version = "2025-01-30";
-      src = pkgs.fetchFromGitHub {
-        owner = "OXY2DEV";
-        repo = "markview.nvim";
-        rev = "f933b4597738fec4014d25f11511bcbe2d1e1a32";
-        hash = "sha256-V3imWAzPtlrC89CYigDvnye12CctM7RJioigc57Rn/8=";
-        fetchSubmodules = true;
-      };
-      doCheck = false;
-    };
 
     lazyLoad.settings.ft = "markdown";
 
     settings = {
-      buf_ignore = [ ];
-
       preview = {
+        buf_ignore = [ ];
+
         modes = [
           "n"
           "x"
