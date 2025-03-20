d2fce3faff0eaf4f987938a4055c20176268fef4
Austin Horstman <khaneliman12@gmail.com>
2025-03-19 19:10:34 -0500

    overlays/nixpkgs-master: overlay git-worktree-nvim
    Add a patch to fix the crash

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -1,7 +1,25 @@
 { flake }:
-_final: super: {
-  inherit (flake.inputs.nixpkgs-master.legacyPackages.${super.stdenv.system})
+_final: super:
+let
+  nixpkgs-master-packages = flake.inputs.nixpkgs-master.legacyPackages.${super.stdenv.system};
+in
+{
+  inherit (nixpkgs-master-packages)
     luaPackages
-    vimPlugins
     ;
+  vimPlugins = nixpkgs-master-packages.vimPlugins // {
+    #
+    # Specific package overlays need to go in here to not get ignored
+    #
+
+    git-worktree-nvim = nixpkgs-master-packages.vimPlugins.git-worktree-nvim.overrideAttrs {
+      patches = [
+        (super.fetchpatch {
+          name = "fix-hl-error";
+          url = "https://patch-diff.githubusercontent.com/raw/ThePrimeagen/git-worktree.nvim/pull/132.patch";
+          hash = "sha256-RF+SijBfI7KlN2bsx/qYDGQfXc18cydm7x6BijCIfYM=";
+        })
+      ];
+    };
+  };
 }
