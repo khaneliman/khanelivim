f1b1e8f868f722858fa6af5585c927266224eaf4
Austin Horstman <khaneliman12@gmail.com>
2025-03-19 22:14:22 -0500

    git-worktree: add hooks for switching

--- modules/nixvim/plugins/git-worktree/default.nix
+++ modules/nixvim/plugins/git-worktree/default.nix
@@ -7,11 +7,25 @@
 let
   # cfg = config.plugins.git-worktree;
 
-  worktreeTelescopeEnabled =
-    (builtins.elem pkgs.vimPlugins.git-worktree-nvim config.extraPlugins)
-    && config.plugins.telescope.enable;
+  worktreeEnabled = builtins.elem pkgs.vimPlugins.git-worktree-nvim config.extraPlugins;
+  worktreeTelescopeEnabled = worktreeEnabled && config.plugins.telescope.enable;
 in
 {
+  extraConfigLua = ''
+    local Hooks = require("git-worktree.hooks")
+    local config = require('git-worktree.config')
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+    Hooks.register(Hooks.type.SWITCH, function (path, prev_path)
+    	vim.notify("Moved from " .. prev_path .. " to " .. path)
+    	update_on_switch(path, prev_path)
+    end)
+
+    Hooks.register(Hooks.type.DELETE, function ()
+    	vim.cmd(config.update_on_change_command)
+    end)
+  '';
+
   extraPlugins = with pkgs.vimPlugins; [ git-worktree-nvim ];
 
   plugins = {
