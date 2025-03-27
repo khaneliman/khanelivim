27d1f2d5d6ea33241554dc18de82abff8dabcee0
Austin Horstman <khaneliman12@gmail.com>
2025-03-26 22:41:11 -0500

    copilot: add lsp_binary
    New version will need it with nixos setup.

--- modules/nixvim/plugins/copilot/default.nix
+++ modules/nixvim/plugins/copilot/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ./migrateNixvimPlugin.nix
@@ -14,6 +19,7 @@
       settings = {
         panel.enabled = !config.plugins.blink-cmp-copilot.enable;
         suggestion.enabled = !config.plugins.blink-cmp-copilot.enable;
+        lsp_binary = lib.getExe pkgs.copilot-language-server;
       };
     };
 
