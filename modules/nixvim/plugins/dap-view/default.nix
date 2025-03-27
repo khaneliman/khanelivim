c6f2c345faccefb21eb75ceafc833a37c2acd0ed
Austin Horstman <khaneliman12@gmail.com>
2025-03-26 21:10:38 -0500

    dap-view: update with new package

--- modules/nixvim/plugins/dap-view/default.nix
+++ modules/nixvim/plugins/dap-view/default.nix
@@ -4,32 +4,14 @@
   pkgs,
   ...
 }:
-let
-  nvim-dap-view = pkgs.vimUtils.buildVimPlugin {
-    pname = "nvim-dap-view";
-    version = "2025-01-19";
-    src = pkgs.fetchFromGitHub {
-      owner = "igorlfs";
-      repo = "nvim-dap-view";
-      rev = "b807d96c3c5ceaeacde7bb482135365827341201";
-      sha256 = "08m5mr9zs4drkilqc7albl8fvmknx84j8gzgbyml25ghqhffwddl";
-    };
-    dependencies = [ pkgs.vimPlugins.nvim-dap ];
-    meta.homepage = "https://github.com/igorlfs/nvim-dap-view/";
-  };
-in
 {
   # extraPlugins = [
-  #   nvim-dap-view
+  #   pkgs.vimPlugins.nvim-dap-view
   # ];
 
   keymaps =
     lib.optionals
-      (
-        (builtins.elem nvim-dap-view config.extraPlugins)
-        && !config.plugins.dap.extensions.dap-ui.enable
-        && !config.plugins.lz-n.enable
-      )
+      ((builtins.elem pkgs.vimPlugins.nvim-dap-view config.extraPlugins) && !config.plugins.dap-ui.enable)
       [
         {
           mode = "n";
@@ -43,6 +25,14 @@ in
           options = {
             desc = "Toggle Debugger UI";
             silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>dw";
+          action = "<CMD>DapViewWatch<CR>";
+          options = {
+            desc = "Add Watch";
           };
         }
       ];
