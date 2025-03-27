8ccfedb227eaceb9d6d8b891b0bbf02cf6a7f879
Austin Horstman <khaneliman12@gmail.com>
2025-03-27 16:00:55 -0500

    easy-dotnet: add rebuild_project command

--- modules/nixvim/plugins/easy-dotnet/default.nix
+++ modules/nixvim/plugins/easy-dotnet/default.nix
@@ -13,4 +13,23 @@
       picker = "fzf";
     };
   };
+
+  extraConfigLua = ''
+    local function rebuild_project(co, path)
+      local spinner = require("easy-dotnet.ui-modules.spinner").new()
+      spinner:start_spinner("Building")
+      vim.fn.jobstart(string.format("dotnet build %s", path), {
+        on_exit = function(_, return_code)
+          if return_code == 0 then
+            spinner:stop_spinner("Built successfully")
+          else
+            spinner:stop_spinner("Build failed with exit code " .. return_code, vim.log.levels.ERROR)
+            error("Build failed")
+          end
+          coroutine.resume(co)
+        end,
+      })
+      coroutine.yield()
+    end
+  '';
 }
