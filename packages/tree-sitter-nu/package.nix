711c65c6154c315b87c7cce08afe5c8b5338c680
Austin Horstman <khaneliman12@gmail.com>
2025-03-26 22:08:55 -0500

    packages/tree-sitter-nu: bump to latest

--- packages/tree-sitter-nu/package.nix
+++ packages/tree-sitter-nu/package.nix
@@ -1,13 +1,13 @@
 { pkgs }:
 pkgs.tree-sitter.buildGrammar rec {
   language = "nu";
-  version = "9822fc63a62ff87939c88ead9f381f951f092dee";
+  version = "c10340b5bb3789f69182acf8f34c3d4fc24d2fe1";
 
   src = pkgs.fetchFromGitHub {
     owner = "nushell";
     repo = "tree-sitter-nu";
     rev = version;
-    hash = "sha256-fcwWrM1KpK1V+TeGqe/iMICOIv7/lA1WZW/8jJXL7WA=";
+    hash = "sha256-EyaFrO9NE2Ivo8YTXZ6nmC31PB7WFbFdz7AMRw0ooHo=";
   };
 
   meta.homepage = "https://github.com/nushell/tree-sitter-nu";
