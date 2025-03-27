2ef9e0dd1a0d977edf64ba45175f2ae1b5bdc3ee
Austin Horstman <khaneliman12@gmail.com>
2025-03-27 16:00:43 -0500

    easy-dotnet: lazy load

--- modules/nixvim/plugins/easy-dotnet/default.nix
+++ modules/nixvim/plugins/easy-dotnet/default.nix
@@ -2,6 +2,12 @@
   plugins.easy-dotnet = {
     enable = true;
 
+    lazyLoad.settings.ft = [
+      "cs"
+      "fsharp"
+      "xml"
+    ];
+
     # TODO: https://github.com/GustavEikaas/easy-dotnet.nvim?tab=readme-ov-file#nvim-dap-configuration
     settings = {
       picker = "fzf";
