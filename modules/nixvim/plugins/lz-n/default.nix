a49829a649dbf420ea1fee4875c54980dd1ccd2f
Austin Horstman <khaneliman12@gmail.com>
2025-03-28 22:32:02 -0500

    lz-n: add lzn-auto-require
    Too many issues with lazy loading not working how I would expect it to
    behave.

--- modules/nixvim/plugins/lz-n/default.nix
+++ modules/nixvim/plugins/lz-n/default.nix
@@ -1,3 +1,10 @@
+{ lib, pkgs, ... }:
 {
+  extraPlugins = [ pkgs.vimPlugins.lzn-auto-require ];
+
+  extraConfigLuaPost = lib.mkOrder 5000 ''
+    require('lzn-auto-require').enable()
+  '';
+
   plugins.lz-n.enable = true;
 }
