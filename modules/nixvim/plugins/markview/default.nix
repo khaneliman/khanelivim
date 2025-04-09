d21024eaf62f18eab8115bef4fd03eb09ac30824
Austin Horstman <khaneliman12@gmail.com>
2025-04-09 10:06:51 -0500

    markview: try to support other filetypes

--- modules/nixvim/plugins/markview/default.nix
+++ modules/nixvim/plugins/markview/default.nix
@@ -1,28 +1,48 @@
 { config, lib, ... }:
 {
-  plugins.markview = {
-    enable = true;
+  plugins.markview =
+    let
+      filetypes = [
+        "Avante"
+        "codecompanion"
+        "latex"
+        "markdown"
+        "md"
+        "norg"
+        "org"
+        "quarto"
+        "rmd"
+        "typst"
+        "vimwiki"
+      ];
+    in
+    {
+      enable = true;
 
-    lazyLoad.settings.ft = "markdown";
+      lazyLoad.settings.ft = filetypes;
 
-    settings = {
-      preview = {
-        buf_ignore = [ ];
+      settings = {
+        preview = {
+          inherit filetypes;
 
-        modes = [
-          "n"
-          "x"
-          "i"
-          "r"
-        ];
+          ignore_buftypes = [ ];
 
-        hybrid_modes = [
-          "i"
-          "r"
-        ];
+          condition.__raw = ''
+            function (buffer)
+               local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt;
+
+               if bt == "nofile" and (ft == "Avante" or ft == "codecompanion") then
+                    return true;
+               elseif bt == "nofile" then
+                    return false;
+               else
+                    return true;
+               end
+            end
+          '';
+        };
       };
     };
-  };
 
   keymaps = lib.mkIf config.plugins.markview.enable [
     {
