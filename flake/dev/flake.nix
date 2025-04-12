202f172267992041cd428fbd3b5925e996a24a96
Austin Horstman <khaneliman12@gmail.com>
2025-04-11 23:58:41 -0500

    flake/dev: init dev flake

---
+++ flake/dev/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Private inputs for development purposes. These are used by the top level flake in the `dev` partition, but do not appear in consumers' lock files.";
+
+  inputs = {
+    # root.url = "path:../../";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        # Optional inputs removed
+        gitignore.follows = "";
+        flake-compat.follows = "";
+      };
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = _inputs: { };
+}
