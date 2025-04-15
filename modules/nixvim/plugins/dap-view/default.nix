e02a4ce6a2235252ec1a52d9b0dde5536ed7823d
Austin Horstman <khaneliman12@gmail.com>
2025-04-14 22:15:19 -0500

    dap-view: use option for enabling

--- modules/nixvim/plugins/dap-view/default.nix
+++ modules/nixvim/plugins/dap-view/default.nix
@@ -5,35 +5,39 @@
   ...
 }:
 {
-  # extraPlugins = [
-  #   pkgs.vimPlugins.nvim-dap-view
-  # ];
+  # TODO: Upstream module
+  options.plugins.nvim-dap-view.enable = lib.mkEnableOption "nvim-dap-view" // {
+    default = !config.plugins.dap-ui.enable;
+  };
 
-  keymaps =
-    lib.optionals
-      ((builtins.elem pkgs.vimPlugins.nvim-dap-view config.extraPlugins) && !config.plugins.dap-ui.enable)
-      [
-        {
-          mode = "n";
-          key = "<leader>du";
-          action.__raw = ''
-            function()
-              require('dap.ext.vscode').load_launchjs(nil, {})
-              require("dap-view").toggle()
-            end
-          '';
-          options = {
-            desc = "Toggle Debugger UI";
-            silent = true;
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>dw";
-          action = "<CMD>DapViewWatch<CR>";
-          options = {
-            desc = "Add Watch";
-          };
-        }
-      ];
+  config = lib.mkIf config.plugins.nvim-dap-view.enable {
+    extraPlugins = [
+      pkgs.vimPlugins.nvim-dap-view
+    ];
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>du";
+        action.__raw = ''
+          function()
+            require('dap.ext.vscode').load_launchjs(nil, {})
+            require("dap-view").toggle()
+          end
+        '';
+        options = {
+          desc = "Toggle Debugger UI";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>dw";
+        action = "<CMD>DapViewWatch<CR>";
+        options = {
+          desc = "Add Watch";
+        };
+      }
+    ];
+  };
 }
