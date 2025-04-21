f6037590155cc2e1cb868a62ec47ee4da42707bf
Austin Horstman <khaneliman12@gmail.com>
2025-04-21 11:33:14 -0500

    claude-code: init

---
+++ modules/nixvim/plugins/claude-code/default.nix
@@ -0,0 +1,58 @@
+{
+  pkgs,
+  ...
+}:
+{
+  extraPackages = [
+    pkgs.claude-code
+  ];
+
+  # TODO: upstream module
+  extraPlugins = [
+    {
+      # TODO: use upstreamed package
+      plugin = pkgs.vimUtils.buildVimPlugin {
+        pname = "claude-code.nvim";
+        version = "2025-03-29";
+        src = pkgs.fetchFromGitHub {
+          owner = "greggh";
+          repo = "claude-code.nvim";
+          rev = "b5c64c42832e5c6c7a02e8e8aa44cfa38a4ae0b2";
+          sha256 = "10s4bn1vcmvkgfxdcilqw85zzlfm2qipw25aqw7jjarys5y3jfik";
+        };
+        meta.homepage = "https://github.com/greggh/claude-code.nvim/";
+        meta.hydraPlatforms = [ ];
+      };
+      optional = true;
+    }
+  ];
+
+  plugins = {
+    lz-n = {
+      enable = true;
+      plugins = [
+        {
+          __unkeyed-1 = "claude-code.nvim";
+          cmd = [ "ClaudeCode" ];
+          after = # Lua
+            ''
+              function()
+                require ("claude-code").setup({})
+              end
+            '';
+        }
+      ];
+    };
+  };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>uc";
+      action = "<cmd>ClaudeCode<CR>";
+      options = {
+        desc = "Claude Code";
+      };
+    }
+  ];
+}
