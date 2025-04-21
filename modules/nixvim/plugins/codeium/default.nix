33b7d0e0f5ef32387999a4d36c39e0f2ed681fea
Austin Horstman <khaneliman12@gmail.com>
2025-04-21 10:54:41 -0500

    codeium: rename to windsurf

--- modules/nixvim/plugins/codeium/default.nix
+++ modules/nixvim/plugins/codeium/default.nix
@@ -6,7 +6,7 @@
 }:
 {
   plugins = {
-    codeium-nvim = {
+    windsurf-nvim = {
       # TODO: properly disable only with copilot enabled
       enable = false;
 
@@ -23,7 +23,7 @@
     };
   };
 
-  keymaps = lib.mkIf config.plugins.codeium-nvim.enable [
+  keymaps = lib.mkIf config.plugins.windsurf-nvim.enable [
     {
       mode = "n";
       key = "<leader>uc";
