439fd2cf665ef6f389eb488f44e0f71b9087ec40
Austin Horstman <khaneliman12@gmail.com>
2025-04-21 22:01:52 -0500

    noice: add snacks and fzf mappings

--- modules/nixvim/plugins/noice/default.nix
+++ modules/nixvim/plugins/noice/default.nix
@@ -147,11 +147,26 @@
     };
   };
 
-  keymaps = lib.mkIf (config.plugins.telescope.enable && config.plugins.noice.enable) [
+  keymaps = lib.mkIf config.plugins.noice.enable [
     {
       mode = "n";
       key = "<leader>fn";
-      action = "<cmd>Telescope noice<CR>";
+      action =
+        let
+          # Determine the action based on priority
+          hasSnacksPicker =
+            config.plugins.snacks.enable && lib.hasAttr "picker" config.plugins.snacks.settings;
+          hasFzfLua = config.plugins.fzf-lua.enable;
+          hasTelescope = config.plugins.telescope.enable;
+        in
+        if hasSnacksPicker then
+          "<cmd>Noice snacks<CR>"
+        else if hasFzfLua then
+          "<cmd>Noice fzf<CR>"
+        else if hasTelescope then
+          "<cmd>Telescope noice<CR>"
+        else
+          "<cmd>Noice<CR>"; # Fallback to basic Noice command
       options = {
         desc = "Find notifications";
       };
