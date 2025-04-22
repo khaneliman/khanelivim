21ffcae12310468301a3360bc71375bf89a6432f
Austin Horstman <khaneliman12@gmail.com>
2025-04-21 21:44:46 -0500

    telescope: fallback last choice keymaps

--- modules/nixvim/plugins/telescope/default.nix
+++ modules/nixvim/plugins/telescope/default.nix
@@ -26,93 +26,102 @@
 
     highlightTheme = "Catppuccin Macchiato";
 
-    # Better version available through fzf-lua
-    keymaps = lib.mkIf (!config.plugins.fzf-lua.enable) {
-      "<leader>f'" = {
-        action = "marks";
-        options.desc = "View marks";
-      };
-      "<leader>f/" = {
-        action = "current_buffer_fuzzy_find";
-        options.desc = "Fuzzy find in current buffer";
-      };
-      "<leader>f<CR>" = {
-        action = "resume";
-        options.desc = "Resume action";
-      };
-      "<leader>fa" = {
-        action = "autocommands";
-        options.desc = "View autocommands";
-      };
-      "<leader>fc" = {
-        action = "commands";
-        options.desc = "View commands";
-      };
-      "<leader>fb" = {
-        action = "buffers";
-        options.desc = "View buffers";
-      };
-      # "<leader>fc" = {
-      #   action = "grep_string";
-      #   options.desc = "Grep string";
-      # };
-      "<leader>fd" = {
-        action = "diagnostics";
-        options.desc = "View diagnostics";
-      };
-      "<leader>ff" = {
-        action = "find_files";
-        options.desc = "Find files";
-      };
-      "<leader>fh" = {
-        action = "help_tags";
-        options.desc = "View help tags";
-      };
-      "<leader>fk" = {
-        action = "keymaps";
-        options.desc = "View keymaps";
-      };
-      "<leader>fm" = {
-        action = "man_pages";
-        options.desc = "View man pages";
-      };
-      "<leader>fo" = {
-        action = "oldfiles";
-        options.desc = "View old files";
-      };
-      "<leader>fr" = {
-        action = "registers";
-        options.desc = "View registers";
-      };
-      "<leader>fs" = {
-        action = "lsp_document_symbols";
-        options.desc = "Search symbols";
-      };
-      "<leader>fq" = {
-        action = "quickfix";
-        options.desc = "Search quickfix";
-      };
-      # "<leader>gC" = {
-      #   action = "git_bcommits";
-      #   options.desc = "View git bcommits";
-      # };
-      "<leader>gB" = {
-        action = "git_branches";
-        options.desc = "View git branches";
-      };
-      "<leader>gC" = {
-        action = "git_commits";
-        options.desc = "View git commits";
-      };
-      "<leader>gs" = {
-        action = "git_status";
-        options.desc = "View git status";
-      };
-      "<leader>gS" = {
-        action = "git_stash";
-        options.desc = "View git stashes";
-      };
-    };
+    # Only use as the last fallback after snacks and fzf-lua
+    keymaps =
+      lib.mkIf
+        (
+          !config.plugins.fzf-lua.enable
+          && (
+            !config.plugins.snacks.enable
+            || (config.plugins.snacks.enable && !lib.hasAttr "picker" config.plugins.snacks.settings)
+          )
+        )
+        {
+          "<leader>f'" = {
+            action = "marks";
+            options.desc = "View marks";
+          };
+          "<leader>f/" = {
+            action = "current_buffer_fuzzy_find";
+            options.desc = "Fuzzy find in current buffer";
+          };
+          "<leader>f<CR>" = {
+            action = "resume";
+            options.desc = "Resume action";
+          };
+          "<leader>fa" = {
+            action = "autocommands";
+            options.desc = "View autocommands";
+          };
+          "<leader>fc" = {
+            action = "commands";
+            options.desc = "View commands";
+          };
+          "<leader>fb" = {
+            action = "buffers";
+            options.desc = "View buffers";
+          };
+          # "<leader>fc" = {
+          #   action = "grep_string";
+          #   options.desc = "Grep string";
+          # };
+          "<leader>fd" = {
+            action = "diagnostics";
+            options.desc = "View diagnostics";
+          };
+          "<leader>ff" = {
+            action = "find_files";
+            options.desc = "Find files";
+          };
+          "<leader>fh" = {
+            action = "help_tags";
+            options.desc = "View help tags";
+          };
+          "<leader>fk" = {
+            action = "keymaps";
+            options.desc = "View keymaps";
+          };
+          "<leader>fm" = {
+            action = "man_pages";
+            options.desc = "View man pages";
+          };
+          "<leader>fo" = {
+            action = "oldfiles";
+            options.desc = "View old files";
+          };
+          "<leader>fr" = {
+            action = "registers";
+            options.desc = "View registers";
+          };
+          "<leader>fs" = {
+            action = "lsp_document_symbols";
+            options.desc = "Search symbols";
+          };
+          "<leader>fq" = {
+            action = "quickfix";
+            options.desc = "Search quickfix";
+          };
+          # "<leader>gC" = {
+          #   action = "git_bcommits";
+          #   options.desc = "View git bcommits";
+          # };
+          "<leader>gB" = {
+            action = "git_branches";
+            options.desc = "View git branches";
+          };
+          "<leader>gC" = {
+            action = "git_commits";
+            options.desc = "View git commits";
+          };
+          "<leader>gs" = {
+            action = "git_status";
+            options.desc = "View git status";
+          };
+          "<leader>gS" = {
+            action = "git_stash";
+            options.desc = "View git stashes";
+          };
+        };
 
     settings = {
       defaults = {
@@ -164,47 +173,56 @@
         };
       }
     ]
-    # Better version available through fzf-lua
-    ++ lib.optionals (!config.plugins.fzf-lua.enable) [
-      {
-        mode = "n";
-        key = "<leader>fC";
-        action.__raw = ''
-          function()
-            vim.cmd(string.format('Telescope find_files prompt_title="Config Files" cwd="%s" follow=true', vim.fn.stdpath("config")))
-          end
-        '';
-        options = {
-          desc = "Find config files";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>fT";
-        action.__raw = ''
-          function()
-            vim.cmd('Telescope colorscheme enable_preview=true')
-          end
-        '';
-        options = {
-          desc = "Find theme";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>f?";
-        action.__raw = ''
-          function()
-            vim.cmd('Telescope live_grep grep_open_files=true')
-          end
-        '';
-        options = {
-          desc = "Find words in all open buffers";
-          silent = true;
-        };
-      }
-    ]
+    # Only use as the last fallback after snacks and fzf-lua
+    ++
+      lib.optionals
+        (
+          !config.plugins.fzf-lua.enable
+          && (
+            !config.plugins.snacks.enable
+            || (config.plugins.snacks.enable && !lib.hasAttr "picker" config.plugins.snacks.settings)
+          )
+        )
+        [
+          {
+            mode = "n";
+            key = "<leader>fC";
+            action.__raw = ''
+              function()
+                vim.cmd(string.format('Telescope find_files prompt_title="Config Files" cwd="%s" follow=true', vim.fn.stdpath("config")))
+              end
+            '';
+            options = {
+              desc = "Find config files";
+              silent = true;
+            };
+          }
+          {
+            mode = "n";
+            key = "<leader>fT";
+            action.__raw = ''
+              function()
+                vim.cmd('Telescope colorscheme enable_preview=true')
+              end
+            '';
+            options = {
+              desc = "Find theme";
+              silent = true;
+            };
+          }
+          {
+            mode = "n";
+            key = "<leader>f?";
+            action.__raw = ''
+              function()
+                vim.cmd('Telescope live_grep grep_open_files=true')
+              end
+            '';
+            options = {
+              desc = "Find words in all open buffers";
+              silent = true;
+            };
+          }
+        ]
   );
 }
