a54d9caf9da64bb98aaa144da363bb10cce82d19
Austin Horstman <khaneliman12@gmail.com>
2025-04-25 11:57:26 -0500

    snacks/picker: auto truncate width
    Better automatic truncating system instead of a fixed width. Solution
    copied from https://github.com/folke/snacks.nvim/issues/1217

--- modules/nixvim/plugins/snacks/picker.nix
+++ modules/nixvim/plugins/snacks/picker.nix
@@ -15,9 +15,33 @@
 
       settings = {
         picker = {
-          formatters = {
-            file = {
-              truncate = 100;
+          actions = {
+            calculate_file_truncate_width.__raw = ''
+              function(self)
+                  local width = self.list.win:size().width
+                  self.opts.formatters.file.truncate = width - 6
+              end
+            '';
+          };
+          win = {
+            list = {
+              on_buf.__raw = ''
+                function(self)
+                    self:execute 'calculate_file_truncate_width'
+                end
+              '';
+            };
+            preview = {
+              on_buf.__raw = ''
+                function(self)
+                    self:execute 'calculate_file_truncate_width'
+                end
+              '';
+              on_close.__raw = ''
+                function(self)
+                    self:execute 'calculate_file_truncate_width'
+                end
+              '';
             };
           };
           layouts = {
