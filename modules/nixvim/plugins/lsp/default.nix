4b46452a9b17748afb574fe9860ec02fc4bdbb83
Austin Horstman <khaneliman12@gmail.com>
2025-04-29 09:36:06 -0500

    lsp: init module
    Migrate from old lspconfig to new lsp module

--- modules/nixvim/plugins/lsp/default.nix
+++ modules/nixvim/plugins/lsp/default.nix
@@ -1,14 +1,11 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   imports = [
     ./ccls.nix
-    ./clangd.nix
-    ./harper-ls.nix
     ./helm-ls.nix
     ./nil-ls.nix
     ./nixd.nix
@@ -29,45 +26,12 @@
 
     lsp = {
       enable = true;
-      inlayHints = true;
 
       keymaps = {
         silent = true;
         diagnostic = {
           "<leader>lH" = "open_float";
         };
-
-        extra =
-          [
-            (lib.mkIf (!config.plugins.conform-nvim.enable) {
-              action.__raw = ''vim.lsp.buf.format'';
-              mode = "v";
-              key = "<leader>lf";
-              options = {
-                silent = true;
-                buffer = false;
-                desc = "Format selection";
-              };
-            })
-          ]
-          ++ lib.optionals (!config.plugins.glance.enable) [
-            {
-              action = "<CMD>PeekDefinition textDocument/definition<CR>";
-              mode = "n";
-              key = "<leader>lp";
-              options = {
-                desc = "Preview definition";
-              };
-            }
-            {
-              action = "<CMD>PeekDefinition textDocument/typeDefinition<CR>";
-              mode = "n";
-              key = "<leader>lP";
-              options = {
-                desc = "Preview type definition";
-              };
-            }
-          ];
 
         lspBuf =
           lib.optionalAttrs (!config.plugins.conform-nvim.enable) { "<leader>lf" = "format"; }
@@ -86,99 +50,6 @@
                 "<leader>lt" = "type_definition";
               };
       };
-
-      servers = {
-        bashls.enable = true;
-        biome.enable = true;
-        cmake.enable = true;
-        csharp_ls.enable = true;
-        cssls.enable = true;
-        dockerls.enable = true;
-        eslint.enable = true;
-        fish_lsp.enable = true;
-        fsautocomplete.enable = true;
-
-        fsharp_language_server = {
-          enable = false;
-          # TODO: package FSharpLanguageServer
-          # cmd = [ "${pkgs.fsharp-language-server}/FSharpLanguageServer.dll" ];
-        };
-
-        gdscript = {
-          enable = true;
-          package = pkgs.gdtoolkit_4;
-        };
-
-        gopls.enable = true;
-        html.enable = true;
-        java_language_server.enable = !config.plugins.jdtls.enable;
-        jdtls.enable = !config.plugins.jdtls.enable;
-        jsonls.enable = true;
-        lua_ls.enable = true;
-        marksman.enable = true;
-        nushell.enable = true;
-        pyright.enable = true;
-        ruff.enable = true;
-        sqls.enable = true;
-        statix.enable = true;
-        stylelint_lsp.enable = true;
-        tailwindcss.enable = true;
-        taplo.enable = true;
-        ts_ls.enable = !config.plugins.typescript-tools.enable;
-        yamlls.enable = true;
-      };
     };
-
-    which-key.settings.spec = [
-      {
-        __unkeyed-1 = "<leader>l";
-        group = "LSP";
-        icon = " ";
-      }
-      {
-        __unkeyed-1 = "<leader>la";
-        desc = "Code Action";
-      }
-      {
-        __unkeyed-1 = "<leader>ld";
-        desc = "Definition";
-      }
-      {
-        __unkeyed-1 = "<leader>lD";
-        desc = "References";
-      }
-      {
-        __unkeyed-1 = "<leader>lf";
-        desc = "Format";
-      }
-      {
-        __unkeyed-1 = "<leader>l[";
-        desc = "Prev";
-      }
-      {
-        __unkeyed-1 = "<leader>l]";
-        desc = "Next";
-      }
-      {
-        __unkeyed-1 = "<leader>lt";
-        desc = "Type Definition";
-      }
-      {
-        __unkeyed-1 = "<leader>li";
-        desc = "Implementation";
-      }
-      {
-        __unkeyed-1 = "<leader>lh";
-        desc = "Lsp Hover";
-      }
-      {
-        __unkeyed-1 = "<leader>lH";
-        desc = "Diagnostic Hover";
-      }
-      {
-        __unkeyed-1 = "<leader>lr";
-        desc = "Rename";
-      }
-    ];
   };
 }
