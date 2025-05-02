bb7e12787486e81ab565e442b301216df1af723a
Austin Horstman <khaneliman12@gmail.com>
2025-05-02 13:03:47 -0500

    flake.lock: update

--- modules/nixvim/plugins/iron/default.nix
+++ modules/nixvim/plugins/iron/default.nix
@@ -1,87 +1,68 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
 {
-  # TODO: Upstream module
-  options.plugins.iron.enable = lib.mkEnableOption "iron" // {
-    default = true;
-  };
+  plugins = {
+    iron = {
+      enable = true;
 
-  config = lib.mkIf config.plugins.iron.enable {
-    extraPlugins = [
+      lazyLoad.settings.cmd = [
+        "IronRepl"
+        "IronReplHere"
+      ];
+
+      settings = {
+        scratch_repl = true;
+        repl_definition = {
+          python = {
+            command = [ "${lib.getExe pkgs.python3}" ];
+            format.__raw = ''
+              require("iron.fts.common").bracketed_paste_python
+            '';
+          };
+          nix = {
+            command = [
+              "nix"
+              "repl"
+            ];
+          };
+        };
+        keymaps = {
+          send_motion = "<space>sm";
+          visual_send = "<space>sv";
+          send_file = "<space>sf";
+          send_line = "<space>sl";
+          send_paragraph = "<space>sp";
+          send_until_cursor = "<space>su";
+          send_mark = "<space>ms";
+          mark_motion = "<space>mc";
+          mark_visual = "<space>mc";
+          remove_mark = "<space>md";
+          cr = "<space>s<cr>";
+          interrupt = "<space>s<space>";
+          exit = "<space>sq";
+          clear = "<space>sc";
+        };
+        highlight = {
+          italic = true;
+        };
+        ignore_blank_lines = true;
+      };
+    };
+
+    which-key.settings.spec = [
       {
-        plugin = pkgs.vimPlugins.iron-nvim;
-        optional = true;
+        __unkeyed-1 = "<leader>s";
+        group = "REPL Send";
+        icon = "󱠥";
+      }
+      {
+        __unkeyed-1 = "<leader>m";
+        group = "REPL Mark";
+        icon = "󱠥";
       }
     ];
-
-    plugins = {
-      lz-n = {
-        plugins = [
-          {
-            __unkeyed-1 = "iron.nvim";
-            cmd = [
-              "IronRepl"
-              "IronReplHere"
-            ];
-            after = # Lua
-              ''
-                function()
-                  require ("iron.core").setup({
-                    config = {
-                        scratch_repl = true,
-                        repl_definition = {
-                          python = {
-                            command = { "${lib.getExe pkgs.python3}" },
-                            format = require("iron.fts.common").bracketed_paste_python
-                          },
-                          nix = {
-                            command = { "nix", "repl" }
-                          }
-                        },
-                      },
-                      keymaps = {
-                        send_motion = "<space>sm",
-                        visual_send = "<space>sv",
-                        send_file = "<space>sf",
-                        send_line = "<space>sl",
-                        send_paragraph = "<space>sp",
-                        send_until_cursor = "<space>su",
-                        send_mark = "<space>ms",
-                        mark_motion = "<space>mc",
-                        mark_visual = "<space>mc",
-                        remove_mark = "<space>md",
-                        cr = "<space>s<cr>",
-                        interrupt = "<space>s<space>",
-                        exit = "<space>sq",
-                        clear = "<space>sc",
-                      },
-                      highlight = {
-                        italic = true
-                      },
-                      ignore_blank_lines = true,
-                  })
-                end
-              '';
-          }
-        ];
-      };
-
-      which-key.settings.spec = [
-        {
-          __unkeyed-1 = "<leader>s";
-          group = "REPL Send";
-          icon = "󱠥";
-        }
-        {
-          __unkeyed-1 = "<leader>m";
-          group = "REPL Mark";
-          icon = "󱠥";
-        }
-      ];
-    };
   };
 }
