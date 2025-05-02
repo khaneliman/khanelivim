3470b1705b2cb3a9e208f1c2b9b6ead22cae540e
Austin Horstman <khaneliman12@gmail.com>
2025-05-01 21:21:52 -0500

    img-clip: support not lazy loading
    Also don't enable lz-n from this module

--- modules/nixvim/plugins/img-clip/default.nix
+++ modules/nixvim/plugins/img-clip/default.nix
@@ -1,36 +1,44 @@
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
+let
+  luaConfig = # Lua
+    ''
+      require ("img-clip").setup({
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        }
+      })
+    '';
+in
 {
   extraPlugins = [
     {
       plugin = pkgs.vimPlugins.img-clip-nvim;
-      optional = true;
+      optional = config.plugins.lz-n.enable;
     }
   ];
 
+  extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
+
   plugins = {
     lz-n = {
-      enable = true;
       plugins = [
         {
           __unkeyed-1 = "img-clip.nvim";
           event = [ "DeferredUIEnter" ];
-          after = # Lua
-            ''
-              function()
-                require ("img-clip").setup({
-                  default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                      insert_mode = true,
-                    },
-                  }
-                })
-              end
-            '';
+          after = ''
+            function()
+              ${luaConfig}
+            end
+          '';
         }
       ];
     };
