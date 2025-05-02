7d94f3ee068623be9955e0acb606dcc03bf0b899
Austin Horstman <khaneliman12@gmail.com>
2025-05-01 21:35:25 -0500

    img-clip: add enable option
    easier config management

--- modules/nixvim/plugins/img-clip/default.nix
+++ modules/nixvim/plugins/img-clip/default.nix
@@ -4,43 +4,51 @@
   pkgs,
   ...
 }:
-let
-  luaConfig = # Lua
-    ''
-      require ("img-clip").setup({
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        }
-      })
-    '';
-in
 {
-  extraPlugins = [
-    {
-      plugin = pkgs.vimPlugins.img-clip-nvim;
-      optional = config.plugins.lz-n.enable;
-    }
-  ];
+  # TODO: Upstream module
+  options.plugins.img-clip.enable = lib.mkEnableOption "img-clip" // {
+    default = true;
+  };
 
-  extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
-
-  plugins = {
-    lz-n = {
-      plugins = [
+  config =
+    let
+      luaConfig = # Lua
+        ''
+          require ("img-clip").setup({
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+            }
+          })
+        '';
+    in
+    lib.mkIf config.plugins.img-clip.enable {
+      extraPlugins = [
         {
-          __unkeyed-1 = "img-clip.nvim";
-          event = [ "DeferredUIEnter" ];
-          after = ''
-            function()
-              ${luaConfig}
-            end
-          '';
+          plugin = pkgs.vimPlugins.img-clip-nvim;
+          optional = config.plugins.lz-n.enable;
         }
       ];
+
+      extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
+
+      plugins = {
+        lz-n = {
+          plugins = [
+            {
+              __unkeyed-1 = "img-clip.nvim";
+              event = [ "DeferredUIEnter" ];
+              after = ''
+                function()
+                  ${luaConfig}
+                end
+              '';
+            }
+          ];
+        };
+      };
     };
-  };
 }
