ede9f61c602d32498fbbfd1f1459cda5f6c53f31
Austin Horstman <khaneliman12@gmail.com>
2025-05-01 21:35:35 -0500

    claude-code: add enable option
    easier config management

--- modules/nixvim/plugins/claude-code/default.nix
+++ modules/nixvim/plugins/claude-code/default.nix
@@ -4,53 +4,60 @@
   pkgs,
   ...
 }:
-let
-  luaConfig = # Lua
-    ''
-      require ("claude-code").setup({
-        window = { position = "vertical"}
-      })
-    '';
-in
 {
-  extraPackages = [
-    pkgs.claude-code
-  ];
-
   # TODO: upstream module
-  extraPlugins = [
-    {
-      plugin = pkgs.vimPlugins.claude-code-nvim;
-      optional = config.plugins.lz-n.enable;
-    }
-  ];
+  options.plugins.claude-code.enable = lib.mkEnableOption "claude-code" // {
+    default = true;
+  };
 
-  extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
+  config =
+    let
+      luaConfig = # Lua
+        ''
+          require ("claude-code").setup({
+            window = { position = "vertical"}
+          })
+        '';
+    in
+    lib.mkIf config.plugins.claude-code.enable {
+      extraPackages = [
+        pkgs.claude-code
+      ];
 
-  plugins = {
-    lz-n = {
-      plugins = [
+      extraPlugins = [
         {
-          __unkeyed-1 = "claude-code.nvim";
-          cmd = [ "ClaudeCode" ];
-          after = ''
-            function()
-              ${luaConfig}
-            end
-          '';
+          plugin = pkgs.vimPlugins.claude-code-nvim;
+          optional = config.plugins.lz-n.enable;
+        }
+      ];
+
+      extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
+
+      plugins = {
+        lz-n = {
+          plugins = [
+            {
+              __unkeyed-1 = "claude-code.nvim";
+              cmd = [ "ClaudeCode" ];
+              after = ''
+                function()
+                  ${luaConfig}
+                end
+              '';
+            }
+          ];
+        };
+      };
+
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>ac";
+          action = "<cmd>ClaudeCode<CR>";
+          options = {
+            desc = "Claude Code";
+          };
         }
       ];
     };
-  };
-
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>ac";
-      action = "<cmd>ClaudeCode<CR>";
-      options = {
-        desc = "Claude Code";
-      };
-    }
-  ];
 }
