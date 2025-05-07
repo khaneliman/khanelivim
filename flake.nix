d487166d7748eb684e99da8afc6254a1393a3859
Austin Horstman <khaneliman12@gmail.com>
2025-05-06 22:27:35 -0500

    flake.nix: nix-community cachix and disable IFD

--- flake.nix
+++ flake.nix
@@ -33,6 +33,14 @@
     nixpkgs-master.url = "github:nixos/nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    allow-import-from-derivation = false;
+  };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
