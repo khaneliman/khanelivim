b1d51b31603fead4dd03c566fb0db29752998601
Austin Horstman <khaneliman12@gmail.com>
2025-05-08 13:51:29 -0500

    dap-view: migrate to upstreamed module
    Available after https://github.com/nix-community/nixvim/pull/3299

--- modules/nixvim/plugins/dap-view/default.nix
+++ modules/nixvim/plugins/dap-view/default.nix
@@ -5,63 +5,53 @@
   ...
 }:
 {
-  # TODO: Upstream module
-  options.plugins.nvim-dap-view.enable = lib.mkEnableOption "nvim-dap-view" // {
-    default = !config.plugins.dap-ui.enable;
+  plugins = {
+    dap-view = {
+      enable = !config.plugins.dap-ui.enable;
+
+      # TODO: remove once updated upstream
+      package = pkgs.vimPlugins.nvim-dap-view.overrideAttrs {
+        version = "2025-05-01";
+        src = pkgs.fetchFromGitHub {
+          owner = "igorlfs";
+          repo = "nvim-dap-view";
+          rev = "2d68f421fbcf495a5127486bdd5322adf11efe68";
+          sha256 = "0bbrzzvzqjz2cgiiypmhgkwxazsrfnnbicwiklhx9b7xdk8kqxkd";
+        };
+      };
+
+      settings = {
+        winbar = {
+          controls = {
+            enabled = true;
+          };
+        };
+      };
+    };
   };
 
-  config =
-    let
-      luaConfig = # Lua
-        ''
-          require("dap-view").setup({
-            winbar = {
-              controls = {
-                enabled = true,
-              }
-            }
-          })
-        '';
-    in
-    lib.mkIf config.plugins.nvim-dap-view.enable {
-      extraConfigLua = luaConfig;
-
-      extraPlugins = [
-        # TODO: remove once upstreamed
-        (pkgs.vimPlugins.nvim-dap-view.overrideAttrs {
-          version = "2025-05-01";
-          src = pkgs.fetchFromGitHub {
-            owner = "igorlfs";
-            repo = "nvim-dap-view";
-            rev = "2d68f421fbcf495a5127486bdd5322adf11efe68";
-            sha256 = "0bbrzzvzqjz2cgiiypmhgkwxazsrfnnbicwiklhx9b7xdk8kqxkd";
-          };
-        })
-      ];
-
-      keymaps = [
-        {
-          mode = "n";
-          key = "<leader>du";
-          action.__raw = ''
-            function()
-              require('dap.ext.vscode').load_launchjs(nil, {})
-              require("dap-view").toggle()
-            end
-          '';
-          options = {
-            desc = "Toggle Debugger UI";
-            silent = true;
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>dw";
-          action = "<CMD>DapViewWatch<CR>";
-          options = {
-            desc = "Add Watch";
-          };
-        }
-      ];
-    };
+  keymaps = lib.optionals config.plugins.dap-view.enable [
+    {
+      mode = "n";
+      key = "<leader>du";
+      action.__raw = ''
+        function()
+          require('dap.ext.vscode').load_launchjs(nil, {})
+          require("dap-view").toggle()
+        end
+      '';
+      options = {
+        desc = "Toggle Debugger UI";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>dw";
+      action = "<CMD>DapViewWatch<CR>";
+      options = {
+        desc = "Add Watch";
+      };
+    }
+  ];
 }
