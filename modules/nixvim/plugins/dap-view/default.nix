a630890e80d6c21dabc98973c5e33e935b2c8e47
Austin Horstman <khaneliman12@gmail.com>
2025-05-07 22:34:55 -0500

    dap-view: add winbar controls
    Requires version override that needs to be removed after next batch
    update.

--- modules/nixvim/plugins/dap-view/default.nix
+++ modules/nixvim/plugins/dap-view/default.nix
@@ -10,34 +10,58 @@
     default = !config.plugins.dap-ui.enable;
   };
 
-  config = lib.mkIf config.plugins.nvim-dap-view.enable {
-    extraPlugins = [
-      pkgs.vimPlugins.nvim-dap-view
-    ];
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>du";
-        action.__raw = ''
-          function()
-            require('dap.ext.vscode').load_launchjs(nil, {})
-            require("dap-view").toggle()
-          end
+  config =
+    let
+      luaConfig = # Lua
+        ''
+          require("dap-view").setup({
+            winbar = {
+              controls = {
+                enabled = true,
+              }
+            }
+          })
         '';
-        options = {
-          desc = "Toggle Debugger UI";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>dw";
-        action = "<CMD>DapViewWatch<CR>";
-        options = {
-          desc = "Add Watch";
-        };
-      }
-    ];
-  };
+    in
+    lib.mkIf config.plugins.nvim-dap-view.enable {
+      extraConfigLua = luaConfig;
+
+      extraPlugins = [
+        # TODO: remove once upstreamed
+        (pkgs.vimPlugins.nvim-dap-view.overrideAttrs {
+          version = "2025-05-01";
+          src = pkgs.fetchFromGitHub {
+            owner = "igorlfs";
+            repo = "nvim-dap-view";
+            rev = "2d68f421fbcf495a5127486bdd5322adf11efe68";
+            sha256 = "0bbrzzvzqjz2cgiiypmhgkwxazsrfnnbicwiklhx9b7xdk8kqxkd";
+          };
+        })
+      ];
+
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>du";
+          action.__raw = ''
+            function()
+              require('dap.ext.vscode').load_launchjs(nil, {})
+              require("dap-view").toggle()
+            end
+          '';
+          options = {
+            desc = "Toggle Debugger UI";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>dw";
+          action = "<CMD>DapViewWatch<CR>";
+          options = {
+            desc = "Add Watch";
+          };
+        }
+      ];
+    };
 }
