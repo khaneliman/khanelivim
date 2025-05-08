f555d1328957af7921d8e349e36ba3f7565a999d
Austin Horstman <khaneliman12@gmail.com>
2025-05-08 08:02:13 -0500

    claude-code: use upstreamed module
    Available after https://github.com/nix-community/nixvim/pull/3298

--- modules/nixvim/plugins/claude-code/default.nix
+++ modules/nixvim/plugins/claude-code/default.nix
@@ -1,63 +1,30 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
-  # TODO: upstream module
-  options.plugins.claude-code.enable = lib.mkEnableOption "claude-code" // {
-    default = true;
-  };
+  plugins = {
+    claude-code = {
+      enable = true;
+      lazyLoad.settings.cmd = [ "ClaudeCode" ];
 
-  config =
-    let
-      luaConfig = # Lua
-        ''
-          require ("claude-code").setup({
-            window = { position = "vertical"}
-          })
-        '';
-    in
-    lib.mkIf config.plugins.claude-code.enable {
-      extraPackages = [
-        pkgs.claude-code
-      ];
-
-      extraPlugins = [
-        {
-          plugin = pkgs.vimPlugins.claude-code-nvim;
-          optional = config.plugins.lz-n.enable;
-        }
-      ];
-
-      extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
-
-      plugins = {
-        lz-n = {
-          plugins = [
-            {
-              __unkeyed-1 = "claude-code.nvim";
-              cmd = [ "ClaudeCode" ];
-              after = ''
-                function()
-                  ${luaConfig}
-                end
-              '';
-            }
-          ];
+      settings = {
+        window = {
+          position = "vertical";
         };
       };
-
-      keymaps = [
-        {
-          mode = "n";
-          key = "<leader>ac";
-          action = "<cmd>ClaudeCode<CR>";
-          options = {
-            desc = "Claude Code";
-          };
-        }
-      ];
     };
+  };
+
+  keymaps = lib.mkIf config.plugins.claude-code.enable [
+    {
+      mode = "n";
+      key = "<leader>ac";
+      action = "<cmd>ClaudeCode<CR>";
+      options = {
+        desc = "Claude Code";
+      };
+    }
+  ];
 }
