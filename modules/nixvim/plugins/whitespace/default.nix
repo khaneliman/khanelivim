b358d8c90ed05ed6f16f14da6d5f35f9156f206f
Austin Horstman <khaneliman12@gmail.com>
2025-05-08 13:57:22 -0500

    whitespace: migrate to upstreamed module
    Available after https://github.com/nix-community/nixvim/pull/3300

--- modules/nixvim/plugins/whitespace/default.nix
+++ modules/nixvim/plugins/whitespace/default.nix
@@ -1,41 +1,35 @@
 {
-  pkgs,
+  lib,
+  config,
   ...
 }:
 {
-  extraPlugins = [
-    pkgs.vimPlugins.whitespace-nvim
-  ];
+  plugins.whitespace = {
+    enable = true;
+    settings = {
+      highlight = "DiffDelete";
+      ignored_filetypes = [
+        "Avante"
+        "AvanteInput"
+        "TelescopePrompt"
+        "Trouble"
+        "blink-cmp-documentation"
+        "blink-cmp-menu"
+        "blink-cmp-signature"
+        "checkhealth"
+        "copilot-chat"
+        "dashboard"
+        "fzf"
+        "help"
+        "ministarter"
+        "snacks_dashboard"
+      ];
+      ignore_terminal = true;
+      return_cursor = true;
+    };
+  };
 
-  # TODO: upstream module
-  extraConfigLua = ''
-    require('whitespace-nvim').setup({
-        highlight = 'DiffDelete',
-
-        ignored_filetypes = {
-          'Avante',
-          'AvanteInput',
-          'TelescopePrompt',
-          'Trouble',
-          'blink-cmp-documentation',
-          'blink-cmp-menu',
-          'blink-cmp-signature',
-          "checkhealth",
-          'copilot-chat',
-          'dashboard',
-          'fzf',
-          'help',
-          'ministarter',
-          'snacks_dashboard'
-        },
-
-        ignore_terminal = true,
-
-        return_cursor = true,
-    })
-  '';
-
-  keymaps = [
+  keymaps = lib.mkIf config.plugins.whitespace.enable [
     {
       mode = "n";
       key = "<leader>lw";
