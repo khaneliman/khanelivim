fb186eefd9b9310d5a6483b610595a3084e3287c
Austin Horstman <khaneliman12@gmail.com>
2025-05-30 08:49:41 -0500

    packages/git-conflict: apply pr patches
    Seems to be unmaintained? Apply some fixes from PRs while looking for
    alternative solution for plugin.

--- packages/git-conflict/package.nix
+++ packages/git-conflict/package.nix
@@ -12,4 +12,17 @@ vimUtils.buildVimPlugin {
     tag = "v2.1.0";
     hash = "sha256-1t0kKxTGLuOvuRkoLgkoqMZpF+oKo8+gMsTdgPsSb+8=";
   };
+
+  patches = [
+    (pkgs.fetchpatch {
+      name = "disable_diagnostics";
+      url = "https://patch-diff.githubusercontent.com/raw/akinsho/git-conflict.nvim/pull/106.patch";
+      hash = "sha256-RWmEMzb3W7FNjNwkH0wn7HYY7u0S8DzXs8khWzH5MoY=";
+    })
+    (pkgs.fetchpatch {
+      name = "highlight";
+      url = "https://patch-diff.githubusercontent.com/raw/akinsho/git-conflict.nvim/pull/108.patch";
+      hash = "sha256-1jCdRiRrO3eXNhxr7SSxl+rMEK5kG7/kHfWlgF2eO4M=";
+    })
+  ];
 }
