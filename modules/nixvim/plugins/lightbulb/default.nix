399bee78698ebab26f9aaf551171c18f6fd7cad6
Austin Horstman <khaneliman12@gmail.com>
2025-06-06 19:07:38 -0500

    lightbulb: add vim.validate patch
    Until https://github.com/kosayoda/nvim-lightbulb/pull/78 is merged

--- modules/nixvim/plugins/lightbulb/default.nix
+++ modules/nixvim/plugins/lightbulb/default.nix
@@ -1,7 +1,16 @@
+{ pkgs, ... }:
 {
   plugins = {
     nvim-lightbulb = {
       enable = true;
+      package = pkgs.vimPlugins.nvim-lightbulb.overrideAttrs (_old: {
+        patches = [
+          (pkgs.fetchpatch {
+            url = "https://github.com/kosayoda/nvim-lightbulb/pull/78.patch";
+            hash = "sha256-Ui28LmA26K+E+byP0lOspAjXjSvokoDpSW7wdnNT1xk=";
+          })
+        ];
+      });
 
       lazyLoad.settings.event = "DeferredUIEnter";
 
