5ce1e31ed684974225030577b665bfe5ecc86466
Austin Horstman <khaneliman12@gmail.com>
2025-06-08 16:02:24 -0500

    dap-view: remove package override
    well past needed

--- modules/nixvim/plugins/dap-view/default.nix
+++ modules/nixvim/plugins/dap-view/default.nix
@@ -1,24 +1,12 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   plugins = {
     dap-view = {
       enable = !config.plugins.dap-ui.enable;
-
-      # TODO: remove once updated upstream
-      package = pkgs.vimPlugins.nvim-dap-view.overrideAttrs {
-        version = "2025-05-01";
-        src = pkgs.fetchFromGitHub {
-          owner = "igorlfs";
-          repo = "nvim-dap-view";
-          rev = "2d68f421fbcf495a5127486bdd5322adf11efe68";
-          sha256 = "0bbrzzvzqjz2cgiiypmhgkwxazsrfnnbicwiklhx9b7xdk8kqxkd";
-        };
-      };
 
       settings = {
         winbar = {
