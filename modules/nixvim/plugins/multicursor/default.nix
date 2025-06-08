60c6571d5d52fb4dce31713a9fca6fa7ef866e00
Austin Horstman <khaneliman12@gmail.com>
2025-06-08 12:04:29 -0500

    multicursor: add module

---
+++ modules/nixvim/plugins/multicursor/default.nix
@@ -0,0 +1,167 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # TODO: Upstream module
+  options.plugins.multicursor.enable = lib.mkEnableOption "multicursor" // {
+    default = true;
+  };
+
+  config =
+    let
+      luaConfig = # Lua
+        ''
+          local mc = require('multicursor-nvim')
+          mc.setup()
+
+          -- Mappings defined in a keymap layer only apply when there are
+          -- multiple cursors. This lets you have overlapping mappings.
+          mc.addKeymapLayer(function(layerSet)
+              -- Select a different cursor as the main one.
+              layerSet({"n", "x"}, "<left>", mc.prevCursor)
+              layerSet({"n", "x"}, "<right>", mc.nextCursor)
+
+              -- Delete the main cursor.
+              layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+
+              -- Enable and clear cursors using escape.
+              layerSet("n", "<esc>", function()
+                  if not mc.cursorsEnabled() then
+                      mc.enableCursors()
+                  else
+                      mc.clearCursors()
+                  end
+              end)
+          end)
+
+          -- Customize how cursors look.
+          local hl = vim.api.nvim_set_hl
+          hl(0, "MultiCursorCursor", { reverse = true })
+          hl(0, "MultiCursorVisual", { link = "Visual" })
+          hl(0, "MultiCursorSign", { link = "SignColumn"})
+          hl(0, "MultiCursorMatchPreview", { link = "Search" })
+          hl(0, "MultiCursorDisabledCursor", { reverse = true })
+          hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+          hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+        '';
+    in
+    lib.mkIf config.plugins.multicursor.enable {
+      extraConfigLua = luaConfig;
+
+      extraPlugins = [
+        pkgs.vimPlugins.multicursor-nvim
+      ];
+
+      keymaps = [
+        # Add or skip cursor above/below the main cursor
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<up>";
+          action.__raw = "function() require('multicursor-nvim').lineAddCursor(-1) end";
+          options.desc = "Add cursor above";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<down>";
+          action.__raw = "function() require('multicursor-nvim').lineAddCursor(1) end";
+          options.desc = "Add cursor below";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader><up>";
+          action.__raw = "function() require('multicursor-nvim').lineSkipCursor(-1) end";
+          options.desc = "Skip cursor above";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader><down>";
+          action.__raw = "function() require('multicursor-nvim').lineSkipCursor(1) end";
+          options.desc = "Skip cursor below";
+        }
+
+        # Add or skip adding a new cursor by matching word/selection
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader>n";
+          action.__raw = "function() require('multicursor-nvim').matchAddCursor(1) end";
+          options.desc = "Add cursor by match (next)";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader>s";
+          action.__raw = "function() require('multicursor-nvim').matchSkipCursor(1) end";
+          options.desc = "Skip cursor by match";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader>N";
+          action.__raw = "function() require('multicursor-nvim').matchAddCursor(-1) end";
+          options.desc = "Add cursor by match (prev)";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<leader>S";
+          action.__raw = "function() require('multicursor-nvim').matchSkipCursor(-1) end";
+          options.desc = "Skip cursor by match (prev)";
+        }
+
+        # Mouse support
+        {
+          mode = "n";
+          key = "<c-leftmouse>";
+          action.__raw = "require('multicursor-nvim').handleMouse";
+          options.desc = "Add cursor with mouse";
+        }
+        {
+          mode = "n";
+          key = "<c-leftdrag>";
+          action.__raw = "require('multicursor-nvim').handleMouseDrag";
+          options.desc = "Drag cursor with mouse";
+        }
+        {
+          mode = "n";
+          key = "<c-leftrelease>";
+          action.__raw = "require('multicursor-nvim').handleMouseRelease";
+          options.desc = "Release mouse cursor";
+        }
+
+        # Toggle cursors
+        {
+          mode = [
+            "n"
+            "x"
+          ];
+          key = "<c-q>";
+          action.__raw = "require('multicursor-nvim').toggleCursor";
+          options.desc = "Toggle cursor";
+        }
+      ];
+    };
+}
