2868d5f531e5653f84e0238e88e4d9f859fe684a
Austin Horstman <khaneliman12@gmail.com>
2025-06-08 11:45:16 -0500

    patterns: add plugin testing options
    Just some dummy options so that i can test the plugin.

--- modules/nixvim/plugins/patterns/default.nix
+++ modules/nixvim/plugins/patterns/default.nix
@@ -6,8 +6,29 @@
 }:
 {
   # TODO: Upstream module
-  options.plugins.patterns.enable = lib.mkEnableOption "patterns" // {
-    default = true;
+  options.plugins.patterns = {
+    enable = lib.mkEnableOption "patterns" // {
+      default = true;
+    };
+
+    # Dummy options with regex patterns for testing plugin functionality
+    sampleSsnPattern = lib.mkOption {
+      type = lib.types.str;
+      default = "d{3}-d{2}-d{4}";
+      description = "Sample SSN pattern (123-45-6789)";
+    };
+
+    sampleEmailPattern = lib.mkOption {
+      type = lib.types.str;
+      default = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+      description = "Sample email pattern";
+    };
+
+    sampleUrlPattern = lib.mkOption {
+      type = lib.types.str;
+      default = "^(https?://)([da-z.-]+).([a-z.]{2,6})([/w .-]*)*/?$";
+      description = "Sample URL pattern";
+    };
   };
 
   config = lib.mkIf config.plugins.patterns.enable {
