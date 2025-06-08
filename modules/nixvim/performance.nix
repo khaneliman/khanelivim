5e73516a4ce562daf053ef6adefe1f63fae7269f
Austin Horstman <khaneliman12@gmail.com>
2025-06-08 10:06:41 -0500

    performance: try out combinePlugins
    See if it helps much

--- modules/nixvim/performance.nix
+++ modules/nixvim/performance.nix
@@ -1,4 +1,10 @@
 {
+  pkgs,
+  self,
+  system,
+  ...
+}:
+{
   performance = {
     byteCompileLua = {
       enable = true;
@@ -6,6 +12,19 @@
       luaLib = true;
       nvimRuntime = true;
       plugins = true;
+    };
+    combinePlugins = {
+      enable = true;
+      standalonePlugins = with pkgs.vimPlugins; [
+        "firenvim"
+        "neotest"
+        "nvim-treesitter"
+        mini-nvim
+        overseer-nvim
+        self.packages.${system}.tree-sitter-norg-meta
+        self.packages.${system}.tree-sitter-nu
+        vs-tasks-nvim
+      ];
     };
   };
 }
