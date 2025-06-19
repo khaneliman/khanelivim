b5c100a3809e9beec7ce388f1b077e1ea0e63b83
Austin Horstman <khaneliman12@gmail.com>
2025-06-19 13:21:02 -0500

    snacks/bufdelete: remove conflicting keymaps when enabled

--- modules/nixvim/plugins/snacks/bufdelete.nix
+++ modules/nixvim/plugins/snacks/bufdelete.nix
@@ -4,6 +4,14 @@
   ...
 }:
 {
+  extraConfigLuaPre =
+    lib.mkIf (config.plugins.snacks.enable && lib.hasAttr "bufdelete" config.plugins.snacks.settings) # Lua
+      ''
+        -- Disable built-in diagnostic keymaps that conflict with <C-W> closing a buffer
+        vim.keymap.del('n', '<C-W>d')
+        vim.keymap.del('n', '<C-W><C-D>')
+      '';
+
   plugins = {
     snacks = {
       enable = true;
