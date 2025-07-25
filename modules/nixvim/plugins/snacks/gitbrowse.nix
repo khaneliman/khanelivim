e1fb3ca2bc061e0282fdac957126e3d0ae54e747
Austin Horstman <khaneliman12@gmail.com>
2025-07-25 16:38:51 -0500

    gitbrowse: add extra keymaps

--- modules/nixvim/plugins/snacks/gitbrowse.nix
+++ modules/nixvim/plugins/snacks/gitbrowse.nix
@@ -30,5 +30,37 @@
             desc = "Open file in browser";
           };
         }
+        {
+          mode = "v";
+          key = "<leader>go";
+          action = "<cmd>lua Snacks.gitbrowse()<CR>";
+          options = {
+            desc = "Open selection in browser";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>gO";
+          action = "<cmd>lua Snacks.gitbrowse({what = 'repo'})<CR>";
+          options = {
+            desc = "Open repository in browser";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>gm";
+          action = "<cmd>lua Snacks.gitbrowse({branch = 'main'})<CR>";
+          options = {
+            desc = "Open file on main branch";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>gM";
+          action = "<cmd>lua Snacks.gitbrowse({branch = 'master'})<CR>";
+          options = {
+            desc = "Open file on master branch";
+          };
+        }
       ];
 }
