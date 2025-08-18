6dcc455e78f6b29cc11c5ab56ff3c770d63d7425
Austin Horstman <khaneliman12@gmail.com>
2025-08-18 13:46:42 -0500

    overlays/nixpkgs-master: disable check on some luapackages
    Flaky tests that i dont' want to wait to succeed

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -9,13 +9,29 @@ let
     };
   };
   # my-packages = flake.packages.${prev.stdenv.system};
-  inherit (nixpkgs-master-packages) vimPlugins;
+  inherit (nixpkgs-master-packages) luaPackages vimPlugins;
 in
 {
   inherit (nixpkgs-master-packages)
     claude-code
-    luaPackages
     ;
+
+  luaPackages = luaPackages // {
+    #
+    # Specific package overlays need to go in here to not get ignored
+    #
+    fzf-lua = luaPackages.fzf-lua.override {
+      doCheck = false;
+    };
+
+    grug-far-nvim = luaPackages.grug-far-nvim.override {
+      doCheck = false;
+    };
+
+    neotest = luaPackages.neotest.override {
+      doCheck = false;
+    };
+  };
   vimPlugins = vimPlugins // {
     #
     # Specific package overlays need to go in here to not get ignored
