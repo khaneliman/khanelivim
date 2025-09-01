64c0b21f9f7e1fea2df6a90f0c2881f7b7b08c13
Austin Horstman <khaneliman12@gmail.com>
2025-09-01 08:08:13 -0500

    codecompanion: add module

---
+++ modules/nixvim/plugins/codecompanion/default.nix
@@ -0,0 +1,103 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  plugins = {
+    codecompanion = {
+      enable = true;
+
+      lazyLoad.settings = {
+        cmd = [
+          "CodeCompanion"
+          "CodeCompanionChat"
+          "CodeCompanionActions"
+          "CodeCompanionAdd"
+        ];
+      };
+
+      settings = {
+        strategies = {
+          chat = {
+            adapter = "gemini_cli";
+          };
+          inline = {
+            adapter = "gemini_cli";
+          };
+          cmd = {
+            adapter = "gemini_cli";
+          };
+        };
+        opts = {
+          send_code = true;
+        };
+      };
+    };
+
+    which-key.settings.spec = lib.optionals config.plugins.codecompanion.enable [
+      {
+        __unkeyed-1 = "<leader>ai";
+        group = "CodeCompanion";
+        icon = "";
+      }
+    ];
+  };
+
+  keymaps = lib.mkIf config.plugins.codecompanion.enable [
+    {
+      mode = "n";
+      key = "<leader>ait";
+      action = "<cmd>CodeCompanionChat Toggle<CR>";
+      options = {
+        desc = "Toggle Chat";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aic";
+      action = "<cmd>CodeCompanionChat<CR>";
+      options = {
+        desc = "New Chat";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>aia";
+      action = "<cmd>CodeCompanionActions<CR>";
+      options = {
+        desc = "Actions";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>aie";
+      action = "<cmd>CodeCompanion<CR>";
+      options = {
+        desc = "Inline Edit";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aiq";
+      action = "<cmd>CodeCompanion /commit<CR>";
+      options = {
+        desc = "Quick Commit Message";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>air";
+      action = "<cmd>CodeCompanionAdd<CR>";
+      options = {
+        desc = "Add to Chat";
+      };
+    }
+  ];
+}
