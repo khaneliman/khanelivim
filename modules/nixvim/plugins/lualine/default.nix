d418649cb210ed149301128edeeb85e147d3615d
Austin Horstman <khaneliman12@gmail.com>
2025-09-11 13:10:42 -0500

    lualine: fix neovim deprecation

--- modules/nixvim/plugins/lualine/default.nix
+++ modules/nixvim/plugins/lualine/default.nix
@@ -105,7 +105,7 @@ in
               function()
                   local msg = ""
                   local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                  local clients = vim.lsp.get_active_clients()
+                  local clients = vim.lsp.get_clients()
                   if next(clients) == nil then
                       return msg
                   end
