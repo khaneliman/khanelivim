add72ae57abc2a1394c4c8281d91ca6a559452b8
Austin Horstman <khaneliman12@gmail.com>
2025-09-16 15:40:55 -0500

    options: winborder set again
    Believe this double issue was fixed in recent neovim

--- modules/nixvim/options.nix
+++ modules/nixvim/options.nix
@@ -129,9 +129,7 @@
     matchtime = 1; # duration of that flashing n deci-seconds
     startofline = true; # motions like "G" also move to the first char
     report = 9001; # disable "x more/fewer lines" messages
-
-    # FIXME: double floating windows?
-    # winborder = "rounded"; # Border style for floating windows
+    winborder = "rounded"; # Border style for floating windows
 
     # Auto-reload files changed externally
     autoread = true;
