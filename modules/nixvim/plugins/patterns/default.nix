691875e7e08afa75f51085c35bba034a524754be
Austin Horstman <khaneliman12@gmail.com>
2025-09-17 13:59:11 -0500

    patterns: use upstreamed module
    Available after https://github.com/nix-community/nixvim/pull/3691

--- modules/nixvim/plugins/patterns/default.nix
+++ modules/nixvim/plugins/patterns/default.nix
@@ -1,42 +1,30 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
-{
-  # TODO: Upstream module
-  options.plugins.patterns = {
-    enable = lib.mkEnableOption "patterns" // {
-      default = true;
-    };
-
-    # Dummy options with regex patterns for testing plugin functionality
-    sampleSsnPattern = lib.mkOption {
-      type = lib.types.str;
-      default = "d{3}-d{2}-d{4}";
-      description = "Sample SSN pattern (123-45-6789)";
-    };
-
-    sampleEmailPattern = lib.mkOption {
-      type = lib.types.str;
-      default = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
-      description = "Sample email pattern";
-    };
-
-    sampleUrlPattern = lib.mkOption {
-      type = lib.types.str;
-      default = "^(https?://)([da-z.-]+).([a-z.]{2,6})([/w .-]*)*/?$";
-      description = "Sample URL pattern";
-    };
+let
+  # Sample patterns for testing plugin functionality
+  samplePatterns = {
+    ssn = "\\d{3}-\\d{2}-\\d{4}"; # Matches: 123-45-6789
+    email = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"; # Matches: user@example.com
+    url = "^(https?://)([\\da-z.-]+)\\.([a-z.]{2,6})([/\\w .-]*)*/?$"; # Matches: https://example.com/path
   };
+in
+{
+  config = {
+    plugins.patterns = {
+      enable = true;
 
-  config = lib.mkIf config.plugins.patterns.enable {
-    extraPlugins = [
-      pkgs.vimPlugins.patterns-nvim
-    ];
+      luaConfig.pre = ''
+        -- Sample patterns for testing patterns.nvim plugin:
+        -- SSN: ${samplePatterns.ssn}
+        -- Email: ${samplePatterns.email}
+        -- URL: ${samplePatterns.url}
+      '';
+    };
 
-    keymaps = [
+    keymaps = lib.mkIf config.plugins.patterns.enable [
       {
         mode = "n";
         key = "<leader>ph";
