43ef45f40b2fbf7b60ce4f922635cf9f28f0a9b1
Austin Horstman <khaneliman12@gmail.com>
2025-09-20 16:59:54 -0500

    conform: simplify formatter config
    Auto installation of packages with
    https://github.com/nix-community/nixvim/pull/3682 so I don't need to
    maintain EVERY mapping.

--- modules/nixvim/plugins/conform/default.nix
+++ modules/nixvim/plugins/conform/default.nix
@@ -9,6 +9,8 @@
     conform-nvim = {
       enable = true;
 
+      autoInstall.enable = true;
+
       lazyLoad.settings = {
         cmd = [
           "ConformInfo"
@@ -141,9 +143,7 @@
         };
 
         formatters = {
-          bicep.command = lib.getExe pkgs.bicep;
           biome = {
-            command = lib.getExe pkgs.biome;
             env = {
               BIOME_CONFIG_PATH = pkgs.writeTextFile {
                 name = "biome.json";
@@ -154,35 +154,7 @@
               };
             };
           };
-          black.command = lib.getExe pkgs.black;
-          cmake-format.command = lib.getExe pkgs.cmake-format;
           csharpier.command = lib.getExe pkgs.csharpier;
-          deno_fmt.command = lib.getExe pkgs.deno;
-          fantomas.command = lib.getExe pkgs.fantomas;
-          gdformat.command = lib.getExe' pkgs.gdtoolkit_4 "gdformat";
-          golines.command = lib.getExe pkgs.golines;
-          google-java-format.command = lib.getExe pkgs.google-java-format;
-          isort.command = lib.getExe pkgs.isort;
-          jq.command = lib.getExe pkgs.jq;
-          kdlfmt.command = lib.getExe pkgs.kdlfmt;
-          kulala-fmt.command = lib.getExe pkgs.kulala-fmt;
-          nixfmt.command = lib.getExe pkgs.nixfmt;
-          prettierd.command = lib.getExe pkgs.prettierd;
-          ruff.command = lib.getExe pkgs.ruff;
-          rustfmt.command = lib.getExe pkgs.rustfmt;
-          shellcheck.command = lib.getExe pkgs.shellcheck;
-          shellharden.command = lib.getExe pkgs.shellharden;
-          shfmt.command = lib.getExe pkgs.shfmt;
-          sqlfluff.command = lib.getExe pkgs.sqlfluff;
-          squeeze_blanks.command = lib.getExe' pkgs.coreutils "cat";
-          stylelint.command = lib.getExe pkgs.stylelint;
-          stylua.command = lib.getExe pkgs.stylua;
-          swift_format.command = lib.getExe pkgs.swift-format;
-          taplo.command = lib.getExe pkgs.taplo;
-          terraform_fmt.command = lib.getExe pkgs.terraform;
-          xmlformat.command = lib.getExe pkgs.xmlformat;
-          yamlfmt.command = lib.getExe pkgs.yamlfmt;
-          zigfmt.command = lib.getExe pkgs.zig;
         };
       };
     };
