2fc599abf97aa8782089076646433be371478b0e
Austin Horstman <khaneliman12@gmail.com>
2025-09-26 18:06:27 -0500

    flake/dev/flake.nix: unfollow inputs not needed for dev

--- flake/dev/flake.nix
+++ flake/dev/flake.nix
@@ -2,7 +2,15 @@
   description = "Private inputs for development purposes. These are used by the top level flake in the `dev` partition, but do not appear in consumers' lock files.";
 
   inputs = {
-    root.url = "path:../../";
+    root = {
+      url = "path:../../";
+      # Unneeded for dev flake
+      inputs = {
+        pkgs-by-name-for-flake-parts.follows = "";
+        neovim-nightly-overlay.follows = "";
+        nixvim.follows = "";
+      };
+    };
     nixpkgs.follows = "root/nixpkgs";
 
     # keep-sorted start block=yes newline_separated=no
