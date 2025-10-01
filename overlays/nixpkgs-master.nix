f2b64441ac6a5c8393b74e0b05f93514ad67d410
Austin Horstman <khaneliman12@gmail.com>
2025-10-01 07:59:25 -0500

    overlays/nixpkgs-master: bump sidekick-nvim
    rapid updates

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -37,12 +37,12 @@ in
     # Specific package overlays need to go in here to not get ignored
     #
     sidekick-nvim = vimPlugins.sidekick-nvim.overrideAttrs {
-      version = "2025-09-30";
+      version = "2025-10-01";
       src = prev.fetchFromGitHub {
         owner = "folke";
         repo = "sidekick.nvim";
-        rev = "7b3d28bbb883e898f6a8b4f2d7a9ab6ad5cef9f8";
-        sha256 = "1m2af27xvagdq6q8kyahcsrbraaw4x2yvrg35xxcpgp0vi0m9h6a";
+        rev = "8c940476cae4e2ba779ed6c73080d097694e406c";
+        sha256 = "0qwi3kcsww7nax5nyi3yrxc20dc6n9qijqf5mrwy771aqv7ch3as";
       };
     };
   };
