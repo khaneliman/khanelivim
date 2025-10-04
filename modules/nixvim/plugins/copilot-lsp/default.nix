73bdf127d18b16b660aeda6769fd84fa1b436fdb
Austin Horstman <khaneliman12@gmail.com>
2025-10-03 23:40:53 -0500

    copilot-lsp: use upstreamed module
    Available after https://github.com/nix-community/nixvim/pull/3763

---
+++ modules/nixvim/plugins/copilot-lsp/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  ...
+}:
+{
+  plugins = {
+    copilot-lsp = {
+      # enable = config.khanelivim.ai.provider == "copilot";
+      enable = true;
+
+      settings = {
+        nes = {
+          move_count_threshold = 3;
+        };
+      };
+    };
+  };
+}
