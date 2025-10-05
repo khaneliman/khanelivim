ad887711272b5e9fbcc297f6c96be720d26888ae
Austin Horstman <khaneliman12@gmail.com>
2025-10-05 13:52:12 -0500

    roslyn: add rzls integration

--- modules/nixvim/plugins/roslyn/default.nix
+++ modules/nixvim/plugins/roslyn/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -10,9 +11,43 @@
 
       lazyLoad.settings.ft = [
         "cs"
-        "fsharp"
-        "xml"
+        "razor"
       ];
+
+      luaConfig.post = lib.mkIf config.plugins.rzls.enable ''
+        vim.lsp.config("roslyn", {
+              cmd = {
+                  "Microsoft.CodeAnalysis.LanguageServer",
+                  "--stdio",
+                  "--logLevel=Information",
+                  "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+                  "--razorSourceGenerator=${pkgs.rzls}/lib/rzls/Microsoft.CodeAnalysis.Razor.Compiler.dll",
+                  "--razorDesignTimePath=${pkgs.rzls}/lib/rzls/Targets/Microsoft.NET.Sdk.Razor.DesignTime.targets"
+              },
+              handlers = require("rzls.roslyn_handlers"),
+              settings = {
+                  ["csharp|inlay_hints"] = {
+                      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+                      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                      csharp_enable_inlay_hints_for_types = true,
+                      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                      dotnet_enable_inlay_hints_for_literal_parameters = true,
+                      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                      dotnet_enable_inlay_hints_for_other_parameters = true,
+                      dotnet_enable_inlay_hints_for_parameters = true,
+                      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                  },
+                  ["csharp|code_lens"] = {
+                      dotnet_enable_references_code_lens = true,
+                  },
+              },
+          })
+          vim.lsp.enable("roslyn")
+      '';
 
       settings = {
       };
