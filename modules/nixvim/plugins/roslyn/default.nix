9fb52a5ae84d933376a15a2f0b3c9d3751c5e396
Austin Horstman <khaneliman12@gmail.com>
2025-10-05 10:37:58 -0500

    roslyn: disable lsp progress
    Due to roslyn not respecting microsoft's own lsp specifications.. https://github.com/dotnet/roslyn/issues/79939

--- modules/nixvim/plugins/roslyn/default.nix
+++ modules/nixvim/plugins/roslyn/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   ...
 }:
 {
@@ -17,4 +18,25 @@
       };
     };
   };
+
+  # TODO: https://github.com/dotnet/roslyn/issues/79939
+  # Roslyn doesn't follow lsp spec and noice expects it to
+  # Just disable progress for now
+  autoCmd = lib.mkIf (config.khanelivim.lsp.csharp == "roslyn") [
+    {
+      event = [ "FileType" ];
+      pattern = [ "cs" ];
+      callback = {
+        __raw = ''
+          function()
+            vim.api.nvim_clear_autocmds {
+              group = "noice_lsp_progress",
+              event = "LspProgress",
+              pattern = "*",
+            }
+          end
+        '';
+      };
+    }
+  ];
 }
