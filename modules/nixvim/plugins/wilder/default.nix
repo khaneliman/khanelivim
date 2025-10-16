35dc108a90d8defb244f0a51849fbe448a5c9453
Austin Horstman <khaneliman12@gmail.com>
2025-10-16 16:03:36 -0500

    wilder: fix deprecations

--- modules/nixvim/plugins/wilder/default.nix
+++ modules/nixvim/plugins/wilder/default.nix
@@ -13,54 +13,55 @@
     wilder = {
       enable = config.khanelivim.editor.commandlineUI == "wilder";
 
-      modes = [
-        "/"
-        "?"
-        ":"
-      ];
+      settings = {
+        modes = [
+          "/"
+          "?"
+          ":"
+        ];
+      };
 
-      pipeline = [
-        # Lua
-        ''
-          wilder.branch(
-              wilder.python_file_finder_pipeline({
-                file_command = function(ctx, arg)
-                  if string.find(arg, '.') ~= nil then
-                    return {'fd', '-tf', '-H'}
-                  else
-                    return {'fd', '-tf'}
-                  end
-                end,
-                dir_command = {'fd', '-td'},
-                filters = {'cpsm_filter'},
-              }),
-              wilder.substitute_pipeline({
-                pipeline = wilder.python_search_pipeline({
-                  skip_cmdtype_check = 1,
-                  pattern = wilder.python_fuzzy_pattern({
-                   start_at_boundary = 0,
+      options = {
+        pipeline = [
+          (lib.nixvim.mkRaw ''
+            wilder.branch(
+                wilder.python_file_finder_pipeline({
+                  file_command = function(ctx, arg)
+                    if string.find(arg, '.') ~= nil then
+                      return {'fd', '-tf', '-H'}
+                    else
+                      return {'fd', '-tf'}
+                    end
+                  end,
+                  dir_command = {'fd', '-td'},
+                  filters = {'cpsm_filter'},
+                }),
+                wilder.substitute_pipeline({
+                  pipeline = wilder.python_search_pipeline({
+                    skip_cmdtype_check = 1,
+                    pattern = wilder.python_fuzzy_pattern({
+                     start_at_boundary = 0,
+                    }),
                   }),
                 }),
-              }),
-              wilder.cmdline_pipeline({
-                language = 'python',
-                fuzzy = 1,
-              }),
-              {
-                wilder.check(function(ctx, x) return x == "" end),
-                wilder.history(),
-              },
-              wilder.python_search_pipeline({
-                pattern = wilder.python_fuzzy_pattern({
-                start_at_boundary = 0,
-              }),
-            })
-          )
-        ''
-      ];
+                wilder.cmdline_pipeline({
+                  language = 'python',
+                  fuzzy = 1,
+                }),
+                {
+                  wilder.check(function(ctx, x) return x == "" end),
+                  wilder.history(),
+                },
+                wilder.python_search_pipeline({
+                  pattern = wilder.python_fuzzy_pattern({
+                  start_at_boundary = 0,
+                }),
+              })
+            )
+          '')
+        ];
 
-      renderer = # Lua
-        ''
+        renderer = lib.nixvim.mkRaw ''
           wilder.renderer_mux({
             [':'] = wilder.popupmenu_renderer(
               wilder.popupmenu_palette_theme({
@@ -119,6 +120,7 @@
             }),
           })
         '';
+      };
     };
   };
 }
