4cba19f4e8872d83710bf84614393de0e27987c9
Austin Horstman <khaneliman12@gmail.com>
2025-10-18 21:30:18 -0500

    neovim-tasks: bump to latest

--- packages/neovim-tasks/package.nix
+++ packages/neovim-tasks/package.nix
@@ -4,13 +4,13 @@
 }:
 vimUtils.buildVimPlugin {
   pname = "neovim-tasks";
-  version = "0-unstable-07-02-2025";
+  version = "0-unstable-09-08-2025";
 
   src = pkgs.fetchFromGitHub {
     owner = "Shatur";
     repo = "neovim-tasks";
-    rev = "decb21552afcb9cb0d4f73eae7e6c3763efbe9a9";
-    hash = "sha256-aU91caLi7agRhMvVcqPejh6g3/ZQUcM7fRz7t5OY3tc=";
+    rev = "820d3b8fb3b7b5e7334bc84cdd4eacc7d1e6f990";
+    hash = "sha256-hEVXnkQJyuLFjHngYtPZ3VQPoSrQ40VW1LjsHlbQGh8=";
   };
 
   dependencies = with pkgs.vimPlugins; [
