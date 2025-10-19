dc44e74cb16b514f3677f0a725af66ee7089f450
Austin Horstman <khaneliman12@gmail.com>
2025-10-18 21:29:06 -0500

    tree-sitter-kulala-http: bump to latest

--- packages/tree-sitter-kulala-http/package.nix
+++ packages/tree-sitter-kulala-http/package.nix
@@ -1,12 +1,12 @@
 { pkgs }:
 pkgs.tree-sitter.buildGrammar {
   language = "kulala_http";
-  version = "5.3.2";
+  version = "5.3.3";
   src = pkgs.fetchFromGitHub {
     owner = "mistweaverco";
     repo = "kulala.nvim";
-    rev = "6b6e1c8b538cce6654cfc5fb3e4a3acfa316ce57";
-    hash = "sha256-cFjYRw93tR439RGSwNeBo3i4ep0jx/Jcfx9UTS32Tx8=";
+    rev = "0d50e9ce5c992fe507743d8641b36125e668aad4";
+    hash = "sha256-4wAZ9ZYK1SfsSuo7TFrm0tHcaahBkwRYNaKVzS8wTXU=";
   };
   location = "lua/tree-sitter";
 
