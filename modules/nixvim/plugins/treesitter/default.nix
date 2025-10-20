662b9a7a845d483b256cf3ade8637f04f5df7287
Austin Horstman <khaneliman12@gmail.com>
2025-10-20 15:25:50 -0500

    treesitter-refactor: move into new module

--- modules/nixvim/plugins/treesitter/default.nix
+++ modules/nixvim/plugins/treesitter/default.nix
@@ -68,28 +68,5 @@
         };
       };
     };
-
-    treesitter-refactor = {
-      inherit (config.plugins.treesitter) enable;
-
-      settings = {
-
-        highlight_definitions = {
-          enable = true;
-          clearOnCursorMove = true;
-        };
-        smart_rename = {
-          enable = true;
-          keymaps = {
-            # NOTE: default is "grr"
-            # Changed from grR to gR to avoid conflict with gr (References)
-            smart_rename = "gR";
-          };
-        };
-        navigation = {
-          enable = true;
-        };
-      };
-    };
   };
 }
