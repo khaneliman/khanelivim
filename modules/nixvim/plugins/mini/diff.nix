bdf801a9c45726ed19bbe5c3a0919578ad68b662
Austin Horstman <khaneliman12@gmail.com>
2025-10-21 16:04:31 -0500

    mini/diff: consistent keymaps and toggling when primary

--- modules/nixvim/plugins/mini/diff.nix
+++ modules/nixvim/plugins/mini/diff.nix
@@ -12,17 +12,42 @@
         };
       };
     };
+
+    which-key.settings.spec =
+      lib.mkIf (config.plugins.mini.enable && lib.hasAttr "diff" config.plugins.mini.modules)
+        [
+          {
+            __unkeyed = "<leader>gd";
+            group = "Diff";
+            icon = " ";
+          }
+        ];
   };
 
-  keymaps = lib.mkIf (config.plugins.mini.enable && lib.hasAttr "diff" config.plugins.mini.modules) [
-    {
-      mode = "n";
-      key = "<leader>ugo";
-      action.__raw = "MiniDiff.toggle_overlay";
-      options = {
-        desc = "Git Overlay toggle";
-        silent = true;
-      };
-    }
-  ];
+  keymaps = lib.mkIf (config.plugins.mini.enable && lib.hasAttr "diff" config.plugins.mini.modules) (
+    [
+      # Mini-diff specific keybind
+      {
+        mode = "n";
+        key = "<leader>gdm";
+        action.__raw = "MiniDiff.toggle_overlay";
+        options = {
+          desc = "Mini-diff Toggle Overlay";
+          silent = true;
+        };
+      }
+    ]
+    ++ lib.optionals (config.khanelivim.editor.diffViewer == "mini-diff") [
+      # Primary diff shortcut when mini-diff is the chosen diff viewer
+      {
+        mode = "n";
+        key = "<leader>gD";
+        action.__raw = "MiniDiff.toggle_overlay";
+        options = {
+          desc = "Toggle Diff (Primary)";
+          silent = true;
+        };
+      }
+    ]
+  );
 }
