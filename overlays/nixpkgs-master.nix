d8fecbbe1f09822d50c4999d3a8f9f7dc80975f9
Austin Horstman <khaneliman12@gmail.com>
2025-10-21 21:25:36 -0500

    overlays/nixpkgs-master: add vim plugin overlays for fixes

--- overlays/nixpkgs-master.nix
+++ overlays/nixpkgs-master.nix
@@ -38,5 +38,19 @@ in
     #
     # Specific package overlays need to go in here to not get ignored
     #
+    snacks-nvim = vimPlugins.snacks-nvim.overrideAttrs (oldAttrs: {
+      src = prev.fetchFromGitHub {
+        inherit (oldAttrs.src) owner repo;
+        rev = "d293b21fe1a603dfb4757feb82ab3e67b78589f2";
+        hash = "sha256-CSiwn1xZNj5Gu03ddhOmCZKjnxEjwzkjpmeV0kJn0uE=";
+      };
+    });
+    noice-nvim = vimPlugins.noice-nvim.overrideAttrs (oldAttrs: {
+      src = prev.fetchFromGitHub {
+        inherit (oldAttrs.src) owner repo;
+        rev = "c86aea584d98be7ee1167ce4d4ef946fbd7f3ae0";
+        hash = "sha256-1BW1yQ8yd/HF127CAqIC7ZayJJ2T+j6YCXQWm3vHrhQ=";
+      };
+    });
   };
 }
