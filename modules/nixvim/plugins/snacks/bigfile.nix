e21da614f2f8b1b5bd5d8e8d717833c77b3b8a61
Austin Horstman <khaneliman12@gmail.com>
2025-10-26 16:52:58 -0500

    snacks/bigfile: add some upstream defaults

--- modules/nixvim/plugins/snacks/bigfile.nix
+++ modules/nixvim/plugins/snacks/bigfile.nix
@@ -16,6 +16,10 @@
           size = 1024 * 1024; # 1MB
           setup.__raw = ''
             function(ctx)
+              if vim.fn.exists(":NoMatchParen") ~= 0 then
+                vim.cmd([[NoMatchParen]])
+              end
+
               ${lib.optionalString config.plugins.indent-blankline.enable ''require("ibl").setup_buffer(0, { enabled = false })''}
               ${lib.optionalString (lib.hasAttr "indentscope" config.plugins.mini.modules) ''vim.b.miniindentscope_disable = true''}
               ${lib.optionalString config.plugins.illuminate.enable ''require("illuminate").pause_buf()''}
@@ -45,6 +49,15 @@
 
               -- Disable mini animate
               vim.b.minianimate_disable = true
+              vim.b.minihipatterns_disable = true
+
+              Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+
+              vim.schedule(function()
+                if vim.api.nvim_buf_is_valid(ctx.buf) then
+                  vim.bo[ctx.buf].syntax = ctx.ft
+                end
+              end)
             end
           '';
         };
