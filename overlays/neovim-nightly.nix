048fc428245d604a2a7696c7519eba6bcbaebcba
Austin Horstman <khaneliman12@gmail.com>
2025-10-26 17:07:08 -0500

    overlays/neovim-nightly: enable nightly again
    See if bugs i had were fixed

--- overlays/neovim-nightly.nix
+++ overlays/neovim-nightly.nix
@@ -1,4 +1,4 @@
-_: _final: _prev: {
-  # NOTE: whenever you want to use nightly
-  # neovim-unwrapped = flake.inputs.neovim-nightly-overlay.packages.${prev.stdenv.system}.default;
+{ flake, ... }:
+_final: prev: {
+  neovim-unwrapped = flake.inputs.neovim-nightly-overlay.packages.${prev.stdenv.system}.default;
 }
