c565059e658e949df878ef90d25c52440c5f04be
Austin Horstman <khaneliman12@gmail.com>
2025-10-31 23:26:57 -0500

    snacks/debug: add some keymaps to try later

--- modules/nixvim/plugins/snacks/debug.nix
+++ modules/nixvim/plugins/snacks/debug.nix
@@ -37,4 +37,83 @@
       };
     };
   };
+
+  keymaps =
+    lib.mkIf
+      (
+        config.plugins.snacks.enable
+        && lib.hasAttr "debug" config.plugins.snacks.settings
+        && config.plugins.snacks.settings.debug.enabled
+      )
+      [
+        {
+          mode = "n";
+          key = "<leader>dR";
+          action = "<cmd>lua Snacks.debug.run()<CR>";
+          options = {
+            desc = "Run Buffer";
+          };
+        }
+        {
+          mode = "x";
+          key = "<leader>dR";
+          action = "<cmd>lua Snacks.debug.run()<CR>";
+          options = {
+            desc = "Run Selection";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>dS";
+          action = "<cmd>lua Snacks.debug.stats()<CR>";
+          options = {
+            desc = "Show Debug Stats";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>dT";
+          action.__raw = ''
+            function()
+              if vim.g.snacks_debug_trace_enabled == nil then
+                vim.g.snacks_debug_trace_enabled = false
+              end
+
+              vim.g.snacks_debug_trace_enabled = not vim.g.snacks_debug_trace_enabled
+
+              if vim.g.snacks_debug_trace_enabled then
+                Snacks.debug.trace()
+                vim.notify("Debug trace started", vim.log.levels.INFO)
+              else
+                Snacks.debug.trace()
+                vim.notify("Debug trace stopped", vim.log.levels.INFO)
+              end
+            end
+          '';
+          options = {
+            desc = "Toggle Debug Trace";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>dP";
+          action.__raw = ''
+            function()
+              vim.ui.input({ prompt = "Profile iterations (default 100): " }, function(input)
+                local count = tonumber(input) or 100
+                local current_line = vim.api.nvim_get_current_line()
+                local fn = loadstring("return " .. current_line)
+                if fn then
+                  Snacks.debug.profile(fn(), { count = count, title = "Profile: " .. current_line:sub(1, 50) })
+                else
+                  vim.notify("Could not load current line as function", vim.log.levels.ERROR)
+                end
+              end)
+            end
+          '';
+          options = {
+            desc = "Profile Current Line";
+          };
+        }
+      ];
 }
