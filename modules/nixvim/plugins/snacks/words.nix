58e6a109871383f18f817eb3d0392bdb6bbfa725
Austin Horstman <khaneliman12@gmail.com>
2025-11-01 01:02:22 -0500

    snacks/words: tweak to match illuminate config more

--- modules/nixvim/plugins/snacks/words.nix
+++ modules/nixvim/plugins/snacks/words.nix
@@ -7,7 +7,49 @@
   plugins = {
     snacks = {
       settings = {
-        words.enabled = config.khanelivim.ui.referenceHighlighting == "snacks-words";
+        words = {
+          enabled = config.khanelivim.ui.referenceHighlighting == "snacks-words";
+
+          debounce = 100;
+
+          # Disable notifications to reduce noise
+          # notify_jump = false;
+          # notify_end = false; # Changed from default true
+
+          foldopen = true;
+          jumplist = true;
+
+          filter.__raw = ''
+            function(buf)
+              if vim.g.snacks_words == false or vim.b[buf].snacks_words == false then
+                return false
+              end
+
+              -- Exclude filetypes (match illuminate denylist)
+              local ft = vim.bo[buf].filetype
+              local deny_filetypes = {
+                "dirvish",
+                "fugitive",
+                "neo-tree",
+                "nvim-tree",
+                "TelescopePrompt",
+              }
+              for _, denied_ft in ipairs(deny_filetypes) do
+                if ft == denied_ft then
+                  return false
+                end
+              end
+
+              -- Disable for large files (match illuminate's 3000 line cutoff)
+              local line_count = vim.api.nvim_buf_line_count(buf)
+              if line_count > 3000 then
+                return false
+              end
+
+              return true
+            end
+          '';
+        };
       };
     };
   };
