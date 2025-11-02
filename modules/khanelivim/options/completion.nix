6dc38a215dc8b65391610c9aed92a967e4465504
Austin Horstman <khaneliman12@gmail.com>
2025-11-02 09:35:40 -0600

    options/completions: remove nvim-cmp
    Removed from config, not usable

--- modules/khanelivim/options/completion.nix
+++ modules/khanelivim/options/completion.nix
@@ -2,12 +2,7 @@
 {
   options.khanelivim.completion = {
     tool = lib.mkOption {
-      type = lib.types.nullOr (
-        lib.types.enum [
-          "blink"
-          "nvim-cmp"
-        ]
-      );
+      type = lib.types.nullOr (lib.types.enum [ "blink" ]);
       default = "blink";
       description = "Completion tool to use";
     };
