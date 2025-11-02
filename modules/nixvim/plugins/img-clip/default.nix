ecdafc1045edc63fec4f9aa88d74b6a7eaf3cd81
Austin Horstman <khaneliman12@gmail.com>
2025-11-01 20:50:06 -0500

    img-clip: disable drag and drop
    Causing annoying `content not an image` warning with CMD + V

--- modules/nixvim/plugins/img-clip/default.nix
+++ modules/nixvim/plugins/img-clip/default.nix
@@ -9,11 +9,11 @@
         };
 
         settings = {
-
           default = {
-            embed_image_as_base64 = false;
             prompt_for_file_name = false;
             drag_and_drop = {
+              # TODO: figure out 'content not an image' warning
+              enabled = false;
               insert_mode = true;
             };
           };
