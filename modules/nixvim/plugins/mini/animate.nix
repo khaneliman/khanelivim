3f572fdd40eb54636cef97b2c92f4503aa244057
Austin Horstman <khaneliman12@gmail.com>
2025-11-06 22:18:37 -0600

    mini-animate: add keybinds

--- modules/nixvim/plugins/mini/animate.nix
+++ modules/nixvim/plugins/mini/animate.nix
@@ -25,4 +25,80 @@
       };
     };
   };
+
+  plugins.which-key.settings.spec = lib.mkIf config.plugins.mini-animate.enable [
+    {
+      __unkeyed-1 = "<leader>ua";
+      group = "Animations";
+      icon = "󰚾";
+    }
+    {
+      __unkeyed-1 = "<leader>uat";
+      desc = "Toggle all animations";
+    }
+  ];
+
+  keymaps = lib.mkIf config.plugins.mini-animate.enable [
+    {
+      mode = "n";
+      key = "<leader>uat";
+      action.__raw = ''
+        function()
+          local animate = require('mini.animate')
+          local current = animate.config
+
+          -- Toggle all animations
+          local new_state = not current.cursor.enable
+
+          animate.config.cursor.enable = new_state
+          animate.config.scroll.enable = new_state
+          animate.config.resize.enable = new_state
+          animate.config.open.enable = new_state
+          animate.config.close.enable = new_state
+
+          vim.notify(
+            "Animations " .. (new_state and "enabled" or "disabled"),
+            vim.log.levels.INFO
+          )
+        end
+      '';
+      options = {
+        desc = "Toggle all animations";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>uac";
+      action.__raw = ''
+        function()
+          local animate = require('mini.animate')
+          animate.config.cursor.enable = not animate.config.cursor.enable
+          vim.notify(
+            "Cursor animation " .. (animate.config.cursor.enable and "enabled" or "disabled"),
+            vim.log.levels.INFO
+          )
+        end
+      '';
+      options = {
+        desc = "Toggle cursor animation";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>uas";
+      action.__raw = ''
+        function()
+          local animate = require('mini.animate')
+          animate.config.scroll.enable = not animate.config.scroll.enable
+          vim.notify(
+            "Scroll animation " .. (animate.config.scroll.enable and "enabled" or "disabled"),
+            vim.log.levels.INFO
+          )
+        end
+      '';
+      options = {
+        desc = "Toggle scroll animation";
+      };
+    }
+  ];
 }
