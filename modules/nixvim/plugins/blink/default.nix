93634bb0e719fbb06d19297fd236582c0c694b2d
Austin Horstman <khaneliman12@gmail.com>
2025-11-08 12:51:23 -0600

    blink: menu moves to show ghost text

--- modules/nixvim/plugins/blink/default.nix
+++ modules/nixvim/plugins/blink/default.nix
@@ -79,6 +79,24 @@
 
         menu = {
           border = "rounded";
+          direction_priority.__raw = ''
+            function()
+              local ctx = require('blink.cmp').get_context()
+              local item = require('blink.cmp').get_selected_item()
+              if ctx == nil or item == nil then return { 's', 'n' } end
+
+              local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+              local is_multi_line = item_text:find('\n') ~= nil
+
+              -- after showing the menu upwards, we want to maintain that direction
+              -- until we re-open the menu, so store the context id in a global variable
+              if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+                vim.g.blink_cmp_upwards_ctx_id = ctx.id
+                return { 'n', 's' }
+              end
+              return { 's', 'n' }
+            end
+          '';
           draw = {
             snippet_indicator = "◦";
             treesitter = [ "lsp" ];
