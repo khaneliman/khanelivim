487fdf38f8d1232c077243c6df82150090fa740f
Austin Horstman <khaneliman12@gmail.com>
2025-11-23 23:24:35 -0600

    img-clip: move to P

--- modules/nixvim/plugins/img-clip/default.nix
+++ modules/nixvim/plugins/img-clip/default.nix
@@ -8,7 +8,7 @@
           event = [ "DeferredUIEnter" ];
           keys = [
             {
-              __unkeyed-1 = "<leader>p";
+              __unkeyed-1 = "<leader>P";
               __unkeyed-2 = "<cmd>PasteImage<cr>";
               desc = "Paste image from system clipboard";
             }
