bdf25d419b4884d300ac139f0386bb01e16a6efa
Austin Horstman <khaneliman12@gmail.com>
2025-11-24 22:45:40 -0600

    neoconf: lazy load on DeferredUIEnter
    Defer project config loading until after UI initialization.

--- modules/nixvim/plugins/neoconf/default.nix
+++ modules/nixvim/plugins/neoconf/default.nix
@@ -3,6 +3,7 @@
   plugins = {
     neoconf = {
       enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
     };
 
     which-key.settings.spec = lib.optionals config.plugins.neoconf.enable [
