42bcd116c4a599aad01572fcfa45bf0ec496f757
Austin Horstman <khaneliman12@gmail.com>
2025-11-25 21:57:06 -0600

    profiler: add PROF_EVENT support for DeferredUIEnter and VeryLazy

--- modules/nixvim/plugins/snacks/profiler.nix
+++ modules/nixvim/plugins/snacks/profiler.nix
@@ -14,17 +14,30 @@
       )
       (
         # NOTE: Allows profiling
-        # `PROF=1 nvim`
+        # `PROF=1 nvim` - profiles to UIEnter (default)
+        # `PROF=1 PROF_EVENT=deferred nvim` - profiles to DeferredUIEnter (lz.n lazy loads)
+        # `PROF=1 PROF_EVENT=lazy nvim` - profiles to VeryLazy
         lib.mkOrder 1 # Lua
           ''
             if vim.env.PROF then
               local snacks = "${pkgs.vimPlugins.snacks-nvim}"
               vim.opt.rtp:append(snacks)
+
+              local event = "UIEnter"
+              local pattern = nil
+
+              if vim.env.PROF_EVENT == "deferred" then
+                event = "User"
+                pattern = "DeferredUIEnter"
+              elseif vim.env.PROF_EVENT == "lazy" then
+                event = "User"
+                pattern = "VeryLazy"
+              end
+
               require("snacks.profiler").startup({
                 startup = {
-                  -- event = "VimEnter", -- stop profiler on this event. Defaults to `VimEnter`
-                  event = "UIEnter",
-                  -- event = "VeryLazy",
+                  event = event,
+                  pattern = pattern,
                 },
               })
             end
