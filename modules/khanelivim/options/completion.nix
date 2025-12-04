6519a99c7abb9b139d45c55f822401310c7b8eaf
Austin Horstman <khaneliman12@gmail.com>
2025-12-03 20:48:10 -0600

    blink: add blink-cmp-words
    Gated behind new option for selecting which plugin to use for words
    completion.

--- modules/khanelivim/options/completion.nix
+++ modules/khanelivim/options/completion.nix
@@ -6,5 +6,16 @@
       default = "blink";
       description = "Completion tool to use";
     };
+
+    wordProvider = lib.mkOption {
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "dictionary"
+          "words"
+        ]
+      );
+      default = "words";
+      description = "Word completion provider (dictionary for synonyms, words for offline word completion)";
+    };
   };
 }
