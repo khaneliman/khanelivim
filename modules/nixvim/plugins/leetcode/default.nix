2ca5d61b05fa89b459a70373165bc9724cf36011
Austin Horstman <khaneliman12@gmail.com>
2025-12-06 22:31:46 -0600

    leetcode: add module

---
+++ modules/nixvim/plugins/leetcode/default.nix
@@ -0,0 +1,58 @@
+{ config, lib, ... }:
+{
+  plugins.leetcode = {
+    enable = true;
+
+    lazyLoad.settings = {
+      cmd = [
+        "Leet"
+      ];
+    };
+
+    settings = {
+      # NOTE:
+      # Language 	lang
+      # C++ 	cpp
+      # Java 	java
+      # Python 	python
+      # Python3 	python3
+      # C 	c
+      # C# 	csharp
+      # JavaScript 	javascript
+      # TypeScript 	typescript
+      # PHP 	php
+      # Swift 	swift
+      # Kotlin 	kotlin
+      # Dart 	dart
+      # Go 	golang
+      # Ruby 	ruby
+      # Scala 	scala
+      # Rust 	rust
+      # Racket 	racket
+      # Erlang 	erlang
+      # Elixir 	elixir
+      # Bash 	bash
+      lang = "rust";
+    };
+  };
+
+  keymaps = lib.optionals config.plugins.leetcode.enable [
+    {
+      mode = "n";
+      key = "<leader>L";
+      action = "<cmd>Leet<CR>";
+      options = {
+        desc = "Leet code";
+      };
+    }
+    # NOTE: just seems to quit neovim.
+    # {
+    #   mode = "n";
+    #   key = "<leader>Lq";
+    #   action = "<cmd>Leet exit<CR>";
+    #   options = {
+    #     desc = "Exit Leet Code";
+    #   };
+    # }
+  ];
+}
