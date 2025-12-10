ac2b79acc9c6c64443eada65fcbf5ccb3add4682
Austin Horstman <khaneliman12@gmail.com>
2025-12-10 17:50:04 -0600

    neorg: refactor and add keymaps and neorg-interim-ls

--- modules/nixvim/plugins/neorg/default.nix
+++ modules/nixvim/plugins/neorg/default.nix
@@ -1,5 +1,14 @@
-{ config, lib, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  extraPlugins = lib.mkIf config.plugins.neorg.enable [
+    pkgs.vimPlugins.neorg-interim-ls
+  ];
+
   autoCmd = lib.optionals config.plugins.neorg.enable [
     {
       event = "FileType";
@@ -13,43 +22,160 @@
     }
   ];
 
-  plugins.neorg = {
-    enable = true;
-    telescopeIntegration.enable = config.plugins.telescope.enable;
+  plugins = {
+    neorg = {
+      enable = true;
+      telescopeIntegration.enable = config.plugins.telescope.enable;
 
-    lazyLoad.settings = {
-      cmd = "Neorg";
-      ft = "norg";
-    };
+      lazyLoad.settings = {
+        ft = "norg";
+        cmd = "Neorg";
+      };
 
-    settings = {
-      lazy_loading = true;
+      settings = {
+        # lazy_loading = true;  # Disabled - requires NeorgStart command
 
-      load = {
-        "core.defaults" = lib.mkIf config.plugins.treesitter.enable { __empty = null; };
+        load = {
+          "core.defaults" = lib.mkIf config.plugins.treesitter.enable { __empty = null; };
 
-        "core.keybinds".config.hook.__raw = ''
-          function(keybinds)
-            keybinds.unmap('norg', 'n', '<C-s>')
+          "core.keybinds".config.hook.__raw = ''
+            function(keybinds)
+              -- Unmap conflicting default keybinds
+              keybinds.unmap('norg', 'n', '<C-s>')
+            end
+          '';
 
-            keybinds.map(
-              'norg',
-              'n',
-              '<leader>c',
-              ':Neorg toggle-concealer<CR>',
-              {silent=true}
-            )
-          end
-        '';
+          "core.dirman".config.workspaces = {
+            notes = "~/notes";
+            nix = "~/perso/nix/notes";
+          };
 
-        "core.dirman".config.workspaces = {
-          notes = "~/notes";
-          nix = "~/perso/nix/notes";
+          "core.concealer".__empty = null;
+
+          # Completion via LSP using neorg-interim-ls for blink.cmp support
+          # See: https://github.com/nvim-neorg/neorg/issues/1603
+          "core.completion".config.engine = {
+            module_name = "external.lsp-completion";
+          };
+
+          "external.interim-ls".config = {
+            completion_provider = {
+              enable = true;
+              documentation = true;
+            };
+          };
+
+          # Enable journal module
+          "core.journal".config = {
+            workspace = "notes";
+            journal_folder = "journal";
+          };
         };
-
-        "core.concealer".__empty = null;
-        "core.completion".config.engine = "nvim-cmp";
       };
     };
+
+    which-key.settings.spec = lib.optionals config.plugins.neorg.enable [
+      {
+        __unkeyed-1 = "<leader>n";
+        group = "Notes";
+        icon = " ";
+      }
+    ];
   };
+
+  keymaps = lib.mkIf config.plugins.neorg.enable (
+    [
+      # Workspace operations
+      {
+        mode = "n";
+        key = "<leader>ni";
+        action = "<cmd>Neorg index<CR>";
+        options.desc = "Open Index";
+      }
+      {
+        mode = "n";
+        key = "<leader>nw";
+        action = "<cmd>Neorg workspace<CR>";
+        options.desc = "Select Workspace";
+      }
+      {
+        mode = "n";
+        key = "<leader>nr";
+        action = "<cmd>Neorg return<CR>";
+        options.desc = "Return to Workspace";
+      }
+
+      # Journal operations
+      {
+        mode = "n";
+        key = "<leader>nj";
+        action = "<cmd>Neorg journal today<CR>";
+        options.desc = "Today's Journal";
+      }
+      {
+        mode = "n";
+        key = "<leader>ny";
+        action = "<cmd>Neorg journal yesterday<CR>";
+        options.desc = "Yesterday's Journal";
+      }
+      {
+        mode = "n";
+        key = "<leader>nt";
+        action = "<cmd>Neorg journal tomorrow<CR>";
+        options.desc = "Tomorrow's Journal";
+      }
+      {
+        mode = "n";
+        key = "<leader>nJ";
+        action.__raw = ''
+          function()
+            vim.ui.input({ prompt = "Journal date (YYYY-MM-DD): " }, function(date)
+              if date and date ~= "" then
+                vim.cmd("Neorg journal custom " .. date)
+              end
+            end)
+          end
+        '';
+        options.desc = "Custom Date Journal";
+      }
+
+      # UI toggles
+      {
+        mode = "n";
+        key = "<leader>nc";
+        action = "<cmd>Neorg toggle-concealer<CR>";
+        options.desc = "Toggle Concealer";
+      }
+
+      # Note creation
+      {
+        mode = "n";
+        key = "<leader>nn";
+        action.__raw = ''
+          function()
+            vim.ui.input({ prompt = "Note name: " }, function(name)
+              if name and name ~= "" then
+                vim.cmd("edit ~/notes/" .. name .. ".norg")
+              end
+            end)
+          end
+        '';
+        options.desc = "New Note";
+      }
+    ]
+    ++ lib.optionals config.plugins.telescope.enable [
+      {
+        mode = "n";
+        key = "<leader>ns";
+        action = "<cmd>Telescope find_files cwd=~/notes<CR>";
+        options.desc = "Search Notes";
+      }
+      {
+        mode = "n";
+        key = "<leader>ng";
+        action = "<cmd>Telescope live_grep cwd=~/notes<CR>";
+        options.desc = "Grep Notes";
+      }
+    ]
+  );
 }
