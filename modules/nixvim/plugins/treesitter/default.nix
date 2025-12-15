7e33cc24d8b0f20b3e9be0e591c7d873a45c2221
Austin Horstman <khaneliman12@gmail.com>
2025-12-15 15:11:47 -0600

    treesitter: remap incremental_selection
    Much more ergonomic

--- modules/nixvim/plugins/treesitter/default.nix
+++ modules/nixvim/plugins/treesitter/default.nix
@@ -54,10 +54,10 @@
         incremental_selection = {
           enable = true;
           keymaps = {
-            init_selection = "gnn";
-            node_incremental = "grn";
-            scope_incremental = "grc";
-            node_decremental = "grm";
+            init_selection = "<A-o>";
+            node_incremental = "<A-o>";
+            scope_incremental = "<A-O>";
+            node_decremental = "<A-i>";
           };
         };
 
