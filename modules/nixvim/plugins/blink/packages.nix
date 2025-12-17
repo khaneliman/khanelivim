5f64b69a4ca070bb227193d65b5036e10130a9c9
Austin Horstman <khaneliman12@gmail.com>
2025-12-16 18:16:24 -0600

    blink: only install extra blink plugins with blink

--- modules/nixvim/plugins/blink/packages.nix
+++ modules/nixvim/plugins/blink/packages.nix
@@ -19,13 +19,15 @@
 
   extraPlugins =
     with pkgs.vimPlugins;
-    [
-      blink-cmp-conventional-commits
-      blink-cmp-npm-nvim
-      blink-cmp-yanky
-      blink-nerdfont-nvim
-    ]
-    ++ lib.optionals config.plugins.avante.enable [
-      blink-cmp-avante
-    ];
+    lib.optionals config.plugins.blink-cmp.enable (
+      [
+        blink-cmp-conventional-commits
+        blink-cmp-npm-nvim
+        blink-cmp-yanky
+        blink-nerdfont-nvim
+      ]
+      ++ lib.optionals config.plugins.avante.enable [
+        blink-cmp-avante
+      ]
+    );
 }
