449148ebf38a89a533ae0238e6376834da2cfefc
Austin Horstman <khaneliman12@gmail.com>
2025-12-16 18:41:23 -0600

    lspconfig: remove redundant border config
    Neovim 0.11+ global winborder option now applies to all floating windows
    including lspconfig UI windows. The manual border override is no longer
    needed since opts.winborder = "rounded" is set globally.

--- modules/nixvim/lsp/lspconfig.nix
+++ modules/nixvim/lsp/lspconfig.nix
@@ -1,15 +1,4 @@
 {
-  config,
-  lib,
-  ...
-}:
-{
-  extraConfigLuaPre = lib.mkIf config.plugins.lspconfig.enable ''
-    require('lspconfig.ui.windows').default_options = {
-      border = "rounded"
-    }
-  '';
-
   plugins = {
     lspconfig.enable = true;
   };
