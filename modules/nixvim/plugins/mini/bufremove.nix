67a51e0b5d0a793c937a12cf67433bf20ce38c7f
Austin Horstman <khaneliman12@gmail.com>
2025-12-16 18:42:17 -0600

    mini-bufremove: use modern vim.bo API
    Replace deprecated nvim_buf_get_option(bufnr, \"buflisted\") with
    vim.bo[bufnr].buflisted. The nvim_buf_get_option function was
    soft-deprecated in Neovim 0.10+.

--- modules/nixvim/plugins/mini/bufremove.nix
+++ modules/nixvim/plugins/mini/bufremove.nix
@@ -32,7 +32,7 @@ in
 
           local get_listed_bufs = function()
             return vim.tbl_filter(function(bufnr)
-             return vim.api.nvim_buf_get_option(bufnr, "buflisted")
+             return vim.bo[bufnr].buflisted
             end, vim.api.nvim_list_bufs())
           end
 
