f672960de32ed9ea99dc61d5a6d2850cb553f5d4
Austin Horstman <khaneliman12@gmail.com>
2025-12-19 10:08:27 -0600

    grug-far: add prefill rename keymaps
    Helpful when you don't have lsp rename support.

--- modules/nixvim/plugins/grug-far/default.nix
+++ modules/nixvim/plugins/grug-far/default.nix
@@ -20,5 +20,55 @@
         desc = "GrugFar toggle";
       };
     }
+    {
+      mode = "n";
+      key = "<leader>rw";
+      action.__raw = ''
+        function()
+          require('grug-far').open({
+            prefills = {
+              search = vim.fn.expand('<cword>'),
+              paths = vim.fn.expand('%'),
+            },
+          })
+        end
+      '';
+      options = {
+        desc = "Rename word in buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>rW";
+      action.__raw = ''
+        function()
+          require('grug-far').open({
+            prefills = {
+              search = vim.fn.expand('<cword>'),
+            },
+          })
+        end
+      '';
+      options = {
+        desc = "Rename word in project";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>rw";
+      action.__raw = ''
+        function()
+          require('grug-far').open({
+            prefills = {
+              search = require('grug-far').get_current_visual_selection(),
+              paths = vim.fn.expand('%'),
+            },
+          })
+        end
+      '';
+      options = {
+        desc = "Rename selection in buffer";
+      };
+    }
   ];
 }
