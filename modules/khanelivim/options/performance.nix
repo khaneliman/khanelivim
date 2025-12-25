b2e871ba0fcb304e12ee91573fa970d1020cf3de
Austin Horstman <khaneliman12@gmail.com>
2025-12-25 15:23:47 -0600

    options/performance: add whitelistMode for treesitter
    Make opt-in instead of opt-out

--- modules/khanelivim/options/performance.nix
+++ modules/khanelivim/options/performance.nix
@@ -37,9 +37,63 @@
     };
 
     treesitter = {
+      whitelistMode = lib.mkEnableOption "whitelist mode for treesitter grammars (only install specified grammars)";
+
+      includedGrammars = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [
+          "tree-sitter-angular"
+          "tree-sitter-bash"
+          "tree-sitter-c"
+          "tree-sitter-css"
+          "tree-sitter-diff"
+          "tree-sitter-dockerfile"
+          "tree-sitter-git_config"
+          "tree-sitter-git_rebase"
+          "tree-sitter-gitattributes"
+          "tree-sitter-gitcommit"
+          "tree-sitter-gitignore"
+          "tree-sitter-go"
+          "tree-sitter-gomod"
+          "tree-sitter-gosum"
+          "tree-sitter-helm"
+          "tree-sitter-html"
+          "tree-sitter-http"
+          "tree-sitter-ini"
+          "tree-sitter-java"
+          "tree-sitter-javascript"
+          "tree-sitter-json"
+          "tree-sitter-just"
+          "tree-sitter-kdl"
+          "tree-sitter-lua"
+          "tree-sitter-make"
+          "tree-sitter-markdown"
+          "tree-sitter-markdown_inline"
+          "tree-sitter-mermaid"
+          "tree-sitter-nginx"
+          "tree-sitter-nix"
+          "tree-sitter-php"
+          "tree-sitter-python"
+          "tree-sitter-query"
+          "tree-sitter-regex"
+          "tree-sitter-rust"
+          "tree-sitter-scss"
+          "tree-sitter-terraform"
+          "tree-sitter-toml"
+          "tree-sitter-tsx"
+          "tree-sitter-typescript"
+          "tree-sitter-vim"
+          "tree-sitter-vimdoc"
+          "tree-sitter-xml"
+          "tree-sitter-yaml"
+        ];
+        description = "List of treesitter grammars to explicitly include when whitelistMode is enabled.";
+      };
+
       excludedGrammars = lib.mkOption {
         type = lib.types.listOf lib.types.str;
         default = [
+          # Size
           "tree-sitter-agda"
           "tree-sitter-apex"
           "tree-sitter-arduino"
@@ -78,6 +132,9 @@
           "tree-sitter-v"
           "tree-sitter-verilog"
           "tree-sitter-vhdl"
+
+          # Never going to use
+
         ];
         description = "List of treesitter grammars to exclude from the installation to save space.";
       };
