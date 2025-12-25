b44accba3e6ce05a0d52bc2dc765b80d8950e85d
Austin Horstman <khaneliman12@gmail.com>
2025-12-25 17:16:00 -0600

    copilot-lsp: minor styling tweak

--- modules/nixvim/plugins/copilot-lsp/default.nix
+++ modules/nixvim/plugins/copilot-lsp/default.nix
@@ -3,18 +3,16 @@
   ...
 }:
 {
-  plugins = {
-    copilot-lsp = {
-      enable = builtins.elem "copilot-lsp" config.khanelivim.ai.plugins;
+  plugins.copilot-lsp = {
+    enable = builtins.elem "copilot-lsp" config.khanelivim.ai.plugins;
 
-      lazyLoad.settings = {
-        event = [ "InsertEnter" ];
-      };
+    lazyLoad.settings = {
+      event = [ "InsertEnter" ];
+    };
 
-      settings = {
-        nes = {
-          move_count_threshold = 3;
-        };
+    settings = {
+      nes = {
+        move_count_threshold = 3;
       };
     };
   };
