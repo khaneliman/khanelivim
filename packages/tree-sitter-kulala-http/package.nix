4cda5c7f62a1f952b205f26d45c5c28a80bc9cf2
Austin Horstman <khaneliman12@gmail.com>
2025-12-26 17:51:00 -0600

    packages/tree-sitter-kulala-http: use vimplugin src
    Don't need to pin it, can just inherit the already updated plugin's
    source.

--- packages/tree-sitter-kulala-http/package.nix
+++ packages/tree-sitter-kulala-http/package.nix
@@ -1,14 +1,6 @@
 { pkgs }:
 pkgs.tree-sitter.buildGrammar {
   language = "kulala_http";
-  version = "5.3.3";
-  src = pkgs.fetchFromGitHub {
-    owner = "mistweaverco";
-    repo = "kulala.nvim";
-    rev = "0d50e9ce5c992fe507743d8641b36125e668aad4";
-    hash = "sha256-4wAZ9ZYK1SfsSuo7TFrm0tHcaahBkwRYNaKVzS8wTXU=";
-  };
+  inherit (pkgs.vimPlugins.kulala-nvim) version src meta;
   location = "lua/tree-sitter";
-
-  meta.homepage = "https://github.com/mistweaverco/kulala.nvim";
 }
