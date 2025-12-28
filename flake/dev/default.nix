f1563f70a1c2aac8160860f225dafbf79839b2e5
Austin Horstman <khaneliman12@gmail.com>
2025-12-28 01:15:55 -0600

    flake/dev: apply pkg overlays

--- flake/dev/default.nix
+++ flake/dev/default.nix
@@ -1,7 +1,28 @@
 {
+  inputs,
+  lib,
+  ...
+}:
+let
+  overlaysConfig = import ../overlays.nix { inherit inputs lib; };
+  allOverlays = lib.attrValues overlaysConfig.flake.overlays;
+in
+{
   imports = [
     ./devshell.nix
     ./git-hooks.nix
     ./treefmt.nix
   ];
+
+  perSystem =
+    { system, ... }:
+    {
+      _module.args.pkgs = lib.mkDefault (
+        import inputs.nixpkgs {
+          inherit system;
+          overlays = allOverlays;
+          config = { };
+        }
+      );
+    };
 }
