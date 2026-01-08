ba7d5ec6cc3a28ffbc7da489b489dd2eb527b544
Austin Horstman <khaneliman12@gmail.com>
2026-01-07 20:59:12 -0600

    treesitter-textobjects: refactor mappings
    Try to avoid conflicts to allow playing around with it without disabling
    everything.

--- modules/nixvim/plugins/treesitter-textobjects/default.nix
+++ modules/nixvim/plugins/treesitter-textobjects/default.nix
@@ -1,152 +1,169 @@
 { config, lib, ... }:
 let
-  mkSelect = key: query: group: {
-    mode = [
-      "x"
-      "o"
-    ];
-    inherit key;
-    action.__raw = "function() require('nvim-treesitter-textobjects.select').select_textobject('${query}', '${group}') end";
-  };
+  cfg = config.plugins.treesitter-textobjects;
 
-  mkSwap = key: func: query: desc: {
-    mode = "n";
-    inherit key;
-    options.desc = desc;
-    action.__raw = "function() require('nvim-treesitter-textobjects.swap').${func}('${query}') end";
-  };
-
-  mkMove = key: func: query: group: {
-    mode = [
-      "n"
-      "x"
-      "o"
-    ];
-    inherit key;
-    action.__raw = "function() require('nvim-treesitter-textobjects.move').${func}('${query}', '${group}') end";
-  };
-
-  mkMoveRaw = key: func: query: group: {
-    mode = [
-      "n"
-      "x"
-      "o"
-    ];
-    inherit key;
-    action.__raw = "function() require('nvim-treesitter-textobjects.move').${func}(${query}, '${group}') end";
-  };
+  hasMiniAi = config.plugins.mini-ai.enable;
 in
 {
   plugins.treesitter-textobjects = {
     enable = lib.elem "treesitter-textobjects" config.khanelivim.editor.textObjects;
 
     settings = {
+      # Disable 'select' if mini-ai is enabled to avoid conflict on 'a'/'i' keys
       select = {
+        enable = !hasMiniAi;
         lookahead = true;
         selection_modes = {
           "@parameter.outer" = "v";
           "@function.outer" = "V";
         };
       };
+
       move = {
+        enable = true;
         set_jumps = true;
+
+        goto_next_start = {
+          "]m" = "@function.outer";
+          "]c" = "@class.outer";
+          "]o" = {
+            query = "@loop.*";
+            query_group = "textobjects";
+          };
+          "]s" = {
+            query = "@local.scope";
+            query_group = "locals";
+          };
+          "]z" = {
+            query = "@fold";
+            query_group = "folds";
+          };
+        };
+        goto_next_end = {
+          "]M" = "@function.outer";
+          "]C" = "@class.outer";
+        };
+        goto_previous_start = {
+          "[m" = "@function.outer";
+          "[c" = "@class.outer";
+          "[o" = {
+            query = "@loop.*";
+            query_group = "textobjects";
+          };
+        };
+        goto_previous_end = {
+          "[M" = "@function.outer";
+          "[C" = "@class.outer";
+        };
       };
     };
   };
 
   keymaps =
-    lib.optionals config.plugins.treesitter-textobjects.enable [
-      # Select
-      (mkSelect "am" "@function.outer" "textobjects")
-      (mkSelect "im" "@function.inner" "textobjects")
-      (mkSelect "ac" "@class.outer" "textobjects")
-      (mkSelect "ic" "@class.inner" "textobjects")
-      (mkSelect "as" "@local.scope" "locals")
+    let
+      # Only create select mappings if NOT handled by mini-ai
+      selectMaps = lib.optionals (!hasMiniAi) (
+        let
+          mkSelect = key: query: group: {
+            mode = [
+              "x"
+              "o"
+            ];
+            inherit key;
+            action.__raw = "function() require('nvim-treesitter-textobjects.select').select_textobject('${query}', '${group}') end";
+          };
+        in
+        [
+          (mkSelect "am" "@function.outer" "textobjects")
+          (mkSelect "im" "@function.inner" "textobjects")
+          (mkSelect "ac" "@class.outer" "textobjects")
+          (mkSelect "ic" "@class.inner" "textobjects")
+          (mkSelect "as" "@local.scope" "locals")
+        ]
+      );
 
-      # Swap
-      (mkSwap "<leader>k" "swap_next" "@parameter.inner" "Swap next parameter")
-      (mkSwap "<leader>K" "swap_previous" "@parameter.outer" "Swap previous parameter")
+      mkSwap = key: func: query: desc: {
+        mode = "n";
+        inherit key;
+        options.desc = desc;
+        action.__raw = "function() require('nvim-treesitter-textobjects.swap').${func}('${query}') end";
+      };
 
-      # Move
-      (mkMove "]m" "goto_next_start" "@function.outer" "textobjects")
-      (mkMove "]]" "goto_next_start" "@class.outer" "textobjects")
-      (mkMoveRaw "]o" "goto_next_start" "{ \"@loop.inner\", \"@loop.outer\" }" "textobjects")
-      (mkMove "]s" "goto_next_start" "@local.scope" "locals")
-      (mkMove "]z" "goto_next_start" "@fold" "folds")
+      # Repeatable move bindings
+      repeatMaps = [
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          key = ";";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').repeat_last_move_next";
+        }
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          # ',' conflicts with Window Up (<C-W>k) map. Using Backspace as safe fallback.
+          key = "<BS>";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').repeat_last_move_previous";
+        }
+      ];
 
-      (mkMove "]M" "goto_next_end" "@function.outer" "textobjects")
-      (mkMove "][" "goto_next_end" "@class.outer" "textobjects")
-
-      (mkMove "[m" "goto_previous_start" "@function.outer" "textobjects")
-      (mkMove "[[" "goto_previous_start" "@class.outer" "textobjects")
-
-      (mkMove "[M" "goto_previous_end" "@function.outer" "textobjects")
-      (mkMove "[]" "goto_previous_end" "@class.outer" "textobjects")
-
-      # Conflict with mini-bracketed (diagnostics)
-      # (mkMove "]d" "goto_next" "@conditional.outer" "textobjects")
-      # (mkMove "[d" "goto_previous" "@conditional.outer" "textobjects")
-
-      # Repeatable
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = ";";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').repeat_last_move_next";
-      }
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = ",";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').repeat_last_move_previous";
-      }
-    ]
-    ++ lib.optionals (config.plugins.treesitter-textobjects.enable && !config.plugins.flash.enable) [
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = "f";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_f_expr";
-        options.expr = true;
-      }
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = "F";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_F_expr";
-        options.expr = true;
-      }
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = "t";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_t_expr";
-        options.expr = true;
-      }
-      {
-        mode = [
-          "n"
-          "x"
-          "o"
-        ];
-        key = "T";
-        action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_T_expr";
-        options.expr = true;
-      }
-    ];
+      # 'f' / 't' functionality (if flash is disabled)
+      ftMaps = lib.optionals (cfg.enable && !config.plugins.flash.enable) [
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          key = "f";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_f_expr";
+          options.expr = true;
+        }
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          key = "F";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_F_expr";
+          options.expr = true;
+        }
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          key = "t";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_t_expr";
+          options.expr = true;
+        }
+        {
+          mode = [
+            "n"
+            "x"
+            "o"
+          ];
+          key = "T";
+          action.__raw = "require('nvim-treesitter-textobjects.repeatable_move').builtin_T_expr";
+          options.expr = true;
+        }
+      ];
+    in
+    lib.optionals cfg.enable (
+      selectMaps
+      ++ repeatMaps
+      ++ ftMaps
+      ++ [
+        # Swap (Always useful, no major conflicts with standard mini plugins)
+        (mkSwap "<leader>k" "swap_next" "@parameter.inner" "Swap next parameter")
+        (mkSwap "<leader>K" "swap_previous" "@parameter.outer" "Swap previous parameter")
+      ]
+    );
 }
