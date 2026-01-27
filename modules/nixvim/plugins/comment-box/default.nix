2e14e342c119fb8db3adcdec9688a4951a1d92ff
Austin Horstman <khaneliman12@gmail.com>
2026-01-26 22:17:17 -0600

    feat(comment-box)!: refactor comment-box keymaps
    Add a ton more keymaps for comment-box to make it easier to play around with styling. Nested so we don't steal too many easier to access mappings.

--- modules/nixvim/plugins/comment-box/default.nix
+++ modules/nixvim/plugins/comment-box/default.nix
@@ -7,10 +7,35 @@
       lazyLoad = {
         settings = {
           cmd = [
+            # Utils
             "CBd"
+            "CBy"
+            "CBcatalog"
+            # Boxes
+            "CBllbox"
+            "CBlcbox"
+            "CBlrbox"
+            "CBclbox"
             "CBccbox"
-            "CBllline"
+            "CBcrbox"
+            "CBrlbox"
+            "CBrcbox"
+            "CBrrbox"
+            # Lines
             "CBline"
+            "CBlline"
+            "CBcline"
+            "CBrline"
+            # Titled Lines
+            "CBllline"
+            "CBlcline"
+            "CBlrline"
+            "CBclline"
+            "CBccline"
+            "CBcrline"
+            "CBrlline"
+            "CBrcline"
+            "CBrrline"
           ];
         };
       };
@@ -35,39 +60,313 @@
       };
     };
 
+    which-key.settings.spec = lib.mkIf config.plugins.comment-box.enable [
+      {
+        __unkeyed-1 = "<leader>cb";
+        group = "Boxes (left)";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+      {
+        __unkeyed-1 = "<leader>cB";
+        group = "Boxes (center/right)";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+      {
+        __unkeyed-1 = "<leader>ct";
+        group = "Titled Lines (left)";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+      {
+        __unkeyed-1 = "<leader>cT";
+        group = "Titled Lines (center/right)";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+    ];
   };
 
   keymaps = lib.mkIf config.plugins.comment-box.enable [
+    # ── Utils ─────────────────────────────────────────────────────────────
     {
-      mode = "n";
+      mode = [
+        "n"
+        "v"
+      ];
       key = "<leader>cd";
       action = "<cmd>CBd<cr>";
       options = {
-        desc = "Delete a box";
+        desc = "Delete box/line";
       };
     }
     {
-      mode = "n";
-      key = "<leader>cb";
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cy";
+      action = "<cmd>CBy<cr>";
+      options = {
+        desc = "Yank content";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>co";
+      action = "<cmd>CBcatalog<cr>";
+      options = {
+        desc = "Open catalog";
+      };
+    }
+    # ── Boxes: Left Aligned ───────────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cbl";
+      action = "<cmd>CBllbox<cr>";
+      options = {
+        desc = "Box: left/left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cbc";
+      action = "<cmd>CBlcbox<cr>";
+      options = {
+        desc = "Box: left/center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cbr";
+      action = "<cmd>CBlrbox<cr>";
+      options = {
+        desc = "Box: left/right";
+      };
+    }
+    # ── Boxes: Centered ───────────────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBl";
+      action = "<cmd>CBclbox<cr>";
+      options = {
+        desc = "Box: center/left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBc";
       action = "<cmd>CBccbox<cr>";
       options = {
-        desc = "Box Title";
+        desc = "Box: center/center";
       };
     }
     {
-      mode = "n";
-      key = "<leader>ct";
-      action = "<cmd>CBllline<cr>";
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBr";
+      action = "<cmd>CBcrbox<cr>";
       options = {
-        desc = "Titled Line";
+        desc = "Box: center/right";
+      };
+    }
+    # ── Boxes: Right Aligned ──────────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBL";
+      action = "<cmd>CBrlbox<cr>";
+      options = {
+        desc = "Box: right/left";
       };
     }
     {
-      mode = "n";
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBC";
+      action = "<cmd>CBrcbox<cr>";
+      options = {
+        desc = "Box: right/center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cBR";
+      action = "<cmd>CBrrbox<cr>";
+      options = {
+        desc = "Box: right/right";
+      };
+    }
+    # ── Lines ─────────────────────────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
       key = "<leader>cl";
       action = "<cmd>CBline<cr>";
       options = {
-        desc = "Simple Line";
+        desc = "Line: left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cL";
+      action = "<cmd>CBcline<cr>";
+      options = {
+        desc = "Line: center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cr";
+      action = "<cmd>CBrline<cr>";
+      options = {
+        desc = "Line: right";
+      };
+    }
+    # ── Titled Lines: Left Aligned ────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>ctl";
+      action = "<cmd>CBllline<cr>";
+      options = {
+        desc = "Titled line: left/left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>ctc";
+      action = "<cmd>CBlcline<cr>";
+      options = {
+        desc = "Titled line: left/center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>ctr";
+      action = "<cmd>CBlrline<cr>";
+      options = {
+        desc = "Titled line: left/right";
+      };
+    }
+    # ── Titled Lines: Centered ────────────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTl";
+      action = "<cmd>CBclline<cr>";
+      options = {
+        desc = "Titled line: center/left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTc";
+      action = "<cmd>CBccline<cr>";
+      options = {
+        desc = "Titled line: center/center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTr";
+      action = "<cmd>CBcrline<cr>";
+      options = {
+        desc = "Titled line: center/right";
+      };
+    }
+    # ── Titled Lines: Right Aligned ───────────────────────────────────────
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTL";
+      action = "<cmd>CBrlline<cr>";
+      options = {
+        desc = "Titled line: right/left";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTC";
+      action = "<cmd>CBrcline<cr>";
+      options = {
+        desc = "Titled line: right/center";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>cTR";
+      action = "<cmd>CBrrline<cr>";
+      options = {
+        desc = "Titled line: right/right";
       };
     }
   ];
