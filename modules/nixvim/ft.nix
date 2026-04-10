7032f6e9e7354afb0cd2cdf3debf079eb5d820eb
Austin Horstman <khaneliman12@gmail.com>
2026-04-10 14:19:07 -0500

    refactor(nixvim): simplify filetype registration
    The LSP health filetype fixes added repeated runtime ftplugin stubs and duplicated YAML filetype mappings.
    
    Generate runtime ftplugin stubs from a filetype list and merge repeated filetype maps with mkMerge so future nvim-lspconfig filetype aliases can be added in one place.

--- modules/nixvim/ft.nix
+++ modules/nixvim/ft.nix
@@ -1,3 +1,7 @@
+{ lib, ... }:
+let
+  mkFiletypeMap = filetype: names: lib.genAttrs names (_: filetype);
+in
 {
   filetype = {
     extension = {
@@ -9,29 +13,39 @@
       ignore = "gitignore";
     };
 
-    filename = {
-      ".gitlab-ci.yaml" = "yaml.gitlab";
-      ".gitlab-ci.yml" = "yaml.gitlab";
-      "compose.yaml" = "yaml.docker-compose";
-      "compose.yml" = "yaml.docker-compose";
-      "docker-compose.yaml" = "yaml.docker-compose";
-      "docker-compose.yml" = "yaml.docker-compose";
-    };
+    filename = lib.mkMerge [
+      (mkFiletypeMap "yaml.gitlab" [
+        ".gitlab-ci.yaml"
+        ".gitlab-ci.yml"
+      ])
+      (mkFiletypeMap "yaml.docker-compose" [
+        "compose.yaml"
+        "compose.yml"
+        "docker-compose.yaml"
+        "docker-compose.yml"
+      ])
+    ];
 
-    pattern = {
-      ".*/hypr/.*%.conf" = "hyprlang";
-      "flake.lock" = "json";
-      ".*helm-chart*.yaml" = "helm";
+    pattern = lib.mkMerge [
+      {
+        ".*/hypr/.*%.conf" = "hyprlang";
+        "flake.lock" = "json";
+        ".*helm-chart*.yaml" = "helm";
+      }
       # These yaml.* subtypes are advertised by nvim-lspconfig's yamlls
       # defaults; register common filenames so those filetypes are usable.
-      ".*/compose%..+%.yaml" = "yaml.docker-compose";
-      ".*/compose%..+%.yml" = "yaml.docker-compose";
-      ".*/docker%-compose%..+%.yaml" = "yaml.docker-compose";
-      ".*/docker%-compose%..+%.yml" = "yaml.docker-compose";
-      ".*/values%.yaml" = "yaml.helm-values";
-      ".*/values%.yml" = "yaml.helm-values";
-      ".*/values%-.+%.yaml" = "yaml.helm-values";
-      ".*/values%-.+%.yml" = "yaml.helm-values";
-    };
+      (mkFiletypeMap "yaml.docker-compose" [
+        ".*/compose%..+%.yaml"
+        ".*/compose%..+%.yml"
+        ".*/docker%-compose%..+%.yaml"
+        ".*/docker%-compose%..+%.yml"
+      ])
+      (mkFiletypeMap "yaml.helm-values" [
+        ".*/values%.yaml"
+        ".*/values%.yml"
+        ".*/values%-.+%.yaml"
+        ".*/values%-.+%.yml"
+      ])
+    ];
   };
 }
--- modules/nixvim/lua.nix
+++ modules/nixvim/lua.nix
@@ -1,44 +1,33 @@
-{
-  extraFiles = {
-    "ftplugin/teal.vim".source = builtins.toFile "teal.vim" ''
-      " Register Teal as a known runtime filetype so vim.lsp health checks stop
-      " flagging it as unknown. Real detection still comes from vim.filetype.add().
+{ lib, ... }:
+let
+  runtimeFiletypes = [
+    "gd"
+    "gdscript3"
+    "gotmpl"
+    "markdown.mdx"
+    "qmljs"
+    "teal"
+    "yaml.docker-compose"
+    "yaml.gitlab"
+    "yaml.helm-values"
+  ];
+
+  mkRuntimeFiletype = filetype: {
+    name = "ftplugin/${filetype}.vim";
+    value.source = builtins.toFile "${filetype}.vim" ''
+      " Register filetypes advertised by nvim-lspconfig so vim.lsp health
+      " does not flag them as unknown. Detection still comes from filetype rules.
     '';
-    "ftplugin/gd.vim".source = builtins.toFile "gd.vim" ''
-      " nvim-lspconfig's gdscript config advertises this legacy GDScript alias.
-      " Keep *.gd detection on Neovim's gdscript filetype.
-    '';
-    "ftplugin/gdscript3.vim".source = builtins.toFile "gdscript3.vim" ''
-      " nvim-lspconfig's gdscript config advertises this Godot 3 GDScript alias.
-      " Keep real detection on Neovim's gdscript filetype.
-    '';
-    "ftplugin/gotmpl.vim".source = builtins.toFile "gotmpl.vim" ''
-      " nvim-lspconfig's gopls config advertises Go template support.
-      " Real detection still comes from vim.filetype.add().
-    '';
-    "ftplugin/markdown.mdx.vim".source = builtins.toFile "markdown.mdx.vim" ''
-      " nvim-lspconfig's marksman config advertises MDX as a Markdown subtype.
-      " Real detection still comes from vim.filetype.add().
-    '';
-    "ftplugin/qmljs.vim".source = builtins.toFile "qmljs.vim" ''
-      " nvim-lspconfig's qmlls config advertises this QML JavaScript alias.
-      " Keep real detection on Neovim's qml filetype unless a qmljs extension is needed.
-    '';
-    "ftplugin/yaml.docker-compose.vim".source = builtins.toFile "yaml.docker-compose.vim" ''
-      " nvim-lspconfig's yamlls config advertises this YAML subtype.
-      " Real detection still comes from vim.filetype.add().
-    '';
-    "ftplugin/yaml.gitlab.vim".source = builtins.toFile "yaml.gitlab.vim" ''
-      " nvim-lspconfig's yamlls config advertises this YAML subtype.
-      " Real detection still comes from vim.filetype.add().
-    '';
-    "ftplugin/yaml.helm-values.vim".source = builtins.toFile "yaml.helm-values.vim" ''
-      " nvim-lspconfig's yamlls config advertises this YAML subtype.
-      " Real detection still comes from vim.filetype.add().
-    '';
-    "lua/khanelivim/tooling_info.lua".source = ./lua/khanelivim/tooling_info.lua;
-    "lua/khanelivim/web_tools.lua".source = ./lua/khanelivim/web_tools.lua;
   };
+in
+{
+  extraFiles = lib.mkMerge [
+    (lib.listToAttrs (map mkRuntimeFiletype runtimeFiletypes))
+    {
+      "lua/khanelivim/tooling_info.lua".source = ./lua/khanelivim/tooling_info.lua;
+      "lua/khanelivim/web_tools.lua".source = ./lua/khanelivim/web_tools.lua;
+    }
+  ];
 
   # Just a small boolean function to convert a boolean to a string
   extraConfigLuaPre = ''
