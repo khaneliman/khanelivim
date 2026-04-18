0137ab4f56176b7793d6f81e537c67bc24d96d69
Austin Horstman <khaneliman12@gmail.com>
2026-04-18 18:57:03 -0500

    chore(overlays): remove disable docheck overrides
    Doesn't seem necessary anymore.

--- overlays/input-packages.nix
+++ overlays/input-packages.nix
@@ -26,17 +26,6 @@ in
     #
     # Specific package overlays need to go in here to not get ignored
     #
-    fzf-lua = luaPackages.fzf-lua.override {
-      doCheck = false;
-    };
-
-    grug-far-nvim = luaPackages.grug-far-nvim.override {
-      doCheck = false;
-    };
-
-    neotest = luaPackages.neotest.override {
-      doCheck = false;
-    };
   };
 
   vimPlugins = vimPlugins.extend (
@@ -44,18 +33,6 @@ in
       #
       # Specific package overlays need to go in here to not get ignored
       #
-      fzf-lua = super.fzf-lua.overrideAttrs {
-        doCheck = false;
-      };
-
-      grug-far-nvim = super.grug-far-nvim.overrideAttrs {
-        doCheck = false;
-      };
-
-      neotest = super.neotest.overrideAttrs {
-        doCheck = false;
-      };
-
       # TODO: remove after upstreamed
       # https://github.com/nvim-java/nvim-java/pull/487
       nvim-java = super.nvim-java.overrideAttrs (old: {
