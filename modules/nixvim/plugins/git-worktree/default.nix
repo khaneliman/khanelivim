f17eae0afa6621373a8eb58b63ae954c8da3d8cb
Austin Horstman <khaneliman12@gmail.com>
2026-04-21 11:54:17 -0500

    fix(git-worktree): refresh buffers on switch
    Set git-worktree.nvim defaults so switching worktrees updates the cwd
    and reloads the current buffer in place while preserving existing
    user config values.

--- modules/nixvim/plugins/git-worktree/default.nix
+++ modules/nixvim/plugins/git-worktree/default.nix
@@ -8,6 +8,19 @@
   # git-worktree.nvim documentation
   # See: https://github.com/ThePrimeagen/git-worktree.nvim
   extraConfigLua = lib.mkIf (lib.elem "git-worktree" config.khanelivim.git.integrations) ''
+    local git_worktree_cfg = vim.g.git_worktree
+    if type(git_worktree_cfg) == "function" then
+      git_worktree_cfg = git_worktree_cfg()
+    end
+    if type(git_worktree_cfg) ~= "table" then
+      git_worktree_cfg = {}
+    end
+    vim.g.git_worktree = vim.tbl_extend("force", git_worktree_cfg, {
+      update_on_change = true,
+      update_on_change_command = "e .",
+      change_directory_command = "cd",
+    })
+
     local Hooks = require("git-worktree.hooks")
     local config = require('git-worktree.config')
     local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
