81fcce29cb39200de4699494c83b676b0b99a9b7
Austin Horstman <khaneliman12@gmail.com>
2026-04-23 18:53:40 -0500

    chore(flake): lock update

--- modules/nixvim/plugins/nix/default.nix
+++ modules/nixvim/plugins/nix/default.nix
@@ -1,8 +1,9 @@
+{ pkgs, ... }:
 {
   plugins = {
     # nixvim (nvim-nix) documentation
     # See: https://github.com/nix-community/nixvim
-    direnv.enable = true;
+    direnv.enable = pkgs.stdenv.hostPlatform.isLinux;
     nix.enable = true;
     nix-develop.enable = true;
   };
