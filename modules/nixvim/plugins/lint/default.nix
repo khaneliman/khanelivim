2da88e3ed05d5e19c9ae0b8e70080036071ffba3
Austin Horstman <khaneliman12@gmail.com>
2026-04-24 15:59:37 -0500

    feat(lint): enable auto install from upstream
    Available after https://github.com/nix-community/nixvim/pull/4257

--- modules/nixvim/plugins/lint/default.nix
+++ modules/nixvim/plugins/lint/default.nix
@@ -11,6 +11,14 @@
       # See: https://github.com/mfussenegger/nvim-lint
       enable = true;
 
+      autoInstall = {
+        enable = true;
+
+        overrides = {
+          swiftlint = lib.mkIf pkgs.stdenv.hostPlatform.isLinux null;
+        };
+      };
+
       lazyLoad.settings.event = "DeferredUIEnter";
 
       autoCmd = {
@@ -69,34 +77,6 @@
         "yaml.ghaction" = [ "actionlint" ];
       };
 
-      linters = {
-        biomejs.cmd = lib.getExe pkgs.biome;
-        checkmake.cmd = lib.getExe pkgs.checkmake;
-        checkstyle.cmd = lib.getExe pkgs.checkstyle;
-        clangtidy.cmd = lib.getExe' pkgs.clang-tools "clang-tidy";
-        cmakelint.cmd = lib.getExe' pkgs.cmake-format "cmake-lint";
-        deadnix.cmd = lib.getExe pkgs.deadnix;
-        fish.cmd = lib.getExe pkgs.fish;
-        gdlint.cmd = lib.getExe' pkgs.gdtoolkit_4 "gdlint";
-        golangcilint.cmd = lib.getExe pkgs.golangci-lint;
-        htmlhint.cmd = lib.getExe pkgs.htmlhint;
-        # FIXME: removed from nixpkgs find altnerative
-        # jsonlint.cmd = lib.getExe pkgs.nodePackages.jsonlint;
-        luacheck.cmd = lib.getExe pkgs.luaPackages.luacheck;
-        actionlint.cmd = lib.getExe pkgs.actionlint;
-        codespell.cmd = lib.getExe pkgs.codespell;
-        markdownlint.cmd = lib.getExe pkgs.markdownlint-cli;
-        markdownlint-cli2.cmd = lib.getExe pkgs.markdownlint-cli2;
-        nix.cmd = lib.getExe' pkgs.nix "nix-instantiate";
-        pylint.cmd = lib.getExe pkgs.pylint;
-        shellcheck.cmd = lib.getExe pkgs.shellcheck;
-        sqlfluff.cmd = lib.getExe pkgs.sqlfluff;
-        statix.cmd = lib.getExe pkgs.statix;
-        stylelint.cmd = lib.getExe pkgs.stylelint;
-        # FIXME: throwing error for some reason
-        # swiftlint.cmd = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin (lib.getExe pkgs.swiftlint);
-        yamllint.cmd = lib.getExe pkgs.yamllint;
-      };
     };
   };
 }
