74db4bb6a9ec857967ac735fa840a7c9811ece79
Austin Horstman <khaneliman12@gmail.com>
2026-05-21 20:49:33 -0500

    chore(flake): lock update

--- modules/nixvim/plugins/claudecode/default.nix
+++ modules/nixvim/plugins/claudecode/default.nix
@@ -1,23 +1,14 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
-  # TODO: Consider upstreaming this module to nixvim
-  options.plugins.claudecode = {
-    enable = lib.mkEnableOption "claudecode" // {
-      default = builtins.elem "claudecode" config.khanelivim.ai.plugins;
-    };
+  plugins = {
+    claudecode = {
+      enable = builtins.elem "claudecode" config.khanelivim.ai.plugins;
 
-    package = lib.mkPackageOption pkgs.vimPlugins "claudecode" {
-      default = "claudecode-nvim";
-    };
-
-    settings = lib.mkOption {
-      type = lib.types.attrsOf lib.types.anything;
-      default = {
+      settings = {
         terminal = {
           split_side = "right";
           split_width_percentage = 0.30;
@@ -30,141 +21,107 @@
           open_in_current_tab = true;
         };
       };
-      description = ''
-        Configuration for claudecode.
 
-        See <https://github.com/coder/claudecode.nvim>
-      '';
-    };
-  };
-
-  config =
-    let
-      luaConfig = ''
-        require('claudecode').setup(${lib.generators.toLua { } config.plugins.claudecode.settings})
-      '';
-    in
-    lib.mkIf config.plugins.claudecode.enable {
-      dependencies.claude-code.enable = lib.mkDefault true;
-
-      extraConfigLua = lib.mkIf (!config.plugins.lz-n.enable) luaConfig;
-
-      extraPlugins = [
-        {
-          plugin = config.plugins.claudecode.package;
-          optional = config.plugins.lz-n.enable;
-        }
-      ];
-
-      plugins = {
-        lz-n = {
-          plugins = [
-            {
-              __unkeyed-1 = "claudecode.nvim";
-              cmd = [
-                "ClaudeCode"
-                "ClaudeCodeFocus"
-                "ClaudeCodeSelectModel"
-                "ClaudeCodeAdd"
-                "ClaudeCodeSend"
-                "ClaudeCodeDiffAccept"
-                "ClaudeCodeDiffDeny"
-              ];
-              after = /* Lua */ ''
-                function()
-                  ${luaConfig}
-                end
-              '';
-            }
+      lazyLoad = lib.mkIf config.plugins.lz-n.enable {
+        settings = {
+          cmd = [
+            "ClaudeCode"
+            "ClaudeCodeFocus"
+            "ClaudeCodeSelectModel"
+            "ClaudeCodeAdd"
+            "ClaudeCodeSend"
+            "ClaudeCodeDiffAccept"
+            "ClaudeCodeDiffDeny"
           ];
         };
-
-        which-key.settings.spec = lib.optionals config.plugins.claudecode.enable [
-          {
-            __unkeyed-1 = "<leader>ac";
-            group = "Claude Code";
-            icon = "";
-            mode = [
-              "n"
-              "v"
-            ];
-          }
-        ];
       };
-
-      keymaps = lib.mkIf config.plugins.claudecode.enable [
-        {
-          mode = "n";
-          key = "<leader>act";
-          action = "<cmd>ClaudeCode<cr>";
-          options = {
-            desc = "Toggle Claude";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acc";
-          action = "<cmd>ClaudeCode --continue<cr>";
-          options = {
-            desc = "Continue Claude";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acr";
-          action = "<cmd>ClaudeCode --resume<cr>";
-          options = {
-            desc = "Resume Claude";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acf";
-          action = "<cmd>ClaudeCodeFocus<cr>";
-          options = {
-            desc = "Focus Claude";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acm";
-          action = "<cmd>ClaudeCodeSelectModel<cr>";
-          options = {
-            desc = "Select Claude model";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acb";
-          action = "<cmd>ClaudeCodeAdd %<cr>";
-          options = {
-            desc = "Add current buffer";
-          };
-        }
-        {
-          mode = "v";
-          key = "<leader>acs";
-          action = "<cmd>ClaudeCodeSend<cr>";
-          options = {
-            desc = "Send to Claude";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>aca";
-          action = "<cmd>ClaudeCodeDiffAccept<cr>";
-          options = {
-            desc = "Accept diff";
-          };
-        }
-        {
-          mode = "n";
-          key = "<leader>acd";
-          action = "<cmd>ClaudeCodeDiffDeny<cr>";
-          options = {
-            desc = "Deny diff";
-          };
-        }
-      ];
     };
+
+    which-key.settings.spec = lib.optionals config.plugins.claudecode.enable [
+      {
+        __unkeyed-1 = "<leader>ac";
+        group = "Claude Code";
+        icon = "";
+        mode = [
+          "n"
+          "v"
+        ];
+      }
+    ];
+  };
+
+  keymaps = lib.mkIf config.plugins.claudecode.enable [
+    {
+      mode = "n";
+      key = "<leader>act";
+      action = "<cmd>ClaudeCode<cr>";
+      options = {
+        desc = "Toggle Claude";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acc";
+      action = "<cmd>ClaudeCode --continue<cr>";
+      options = {
+        desc = "Continue Claude";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acr";
+      action = "<cmd>ClaudeCode --resume<cr>";
+      options = {
+        desc = "Resume Claude";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acf";
+      action = "<cmd>ClaudeCodeFocus<cr>";
+      options = {
+        desc = "Focus Claude";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acm";
+      action = "<cmd>ClaudeCodeSelectModel<cr>";
+      options = {
+        desc = "Select Claude model";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acb";
+      action = "<cmd>ClaudeCodeAdd %<cr>";
+      options = {
+        desc = "Add current buffer";
+      };
+    }
+    {
+      mode = "v";
+      key = "<leader>acs";
+      action = "<cmd>ClaudeCodeSend<cr>";
+      options = {
+        desc = "Send to Claude";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>aca";
+      action = "<cmd>ClaudeCodeDiffAccept<cr>";
+      options = {
+        desc = "Accept diff";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>acd";
+      action = "<cmd>ClaudeCodeDiffDeny<cr>";
+      options = {
+        desc = "Deny diff";
+      };
+    }
+  ];
 }
