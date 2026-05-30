deee8203db9cf89a349cc7ccb35b04d9abbdbaa6
Austin Horstman <khaneliman12@gmail.com>
2026-05-30 18:44:52 -0500

    perf(nixvim): build plugin imports linearly

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,17 +1,14 @@
 { lib, self, ... }:
 let
   inherit (builtins) readDir;
-  inherit (lib.attrsets) foldlAttrs;
-  inherit (lib.lists) optional;
   by-name = ./plugins;
 in
 {
   # Plugin by-name directory imports
   imports =
-    (foldlAttrs (
-      prev: name: type:
-      prev ++ optional (type == "directory") (by-name + "/${name}")
-    ) [ ] (readDir by-name))
+    (lib.mapAttrsToList (name: _: by-name + "/${name}") (
+      lib.filterAttrs (_: type: type == "directory") (readDir by-name)
+    ))
     ++ [
       # keep-sorted start
       ../khanelivim
