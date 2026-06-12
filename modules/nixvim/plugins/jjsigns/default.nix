105a4010a388893db807eac0b156f54440bc005e
Austin Horstman <khaneliman12@gmail.com>
2026-06-11 23:40:50 -0500

    fix(jjsigns): use jujutsu dependency key

--- modules/nixvim/plugins/jjsigns/default.nix
+++ modules/nixvim/plugins/jjsigns/default.nix
@@ -76,8 +76,7 @@
       '';
     in
     lib.mkIf config.plugins.jjsigns.enable {
-      __depPackages.jj.default = lib.mkDefault "jujutsu";
-      dependencies.jj.enable = lib.mkDefault true;
+      dependencies.jujutsu.enable = lib.mkDefault true;
 
       extraPlugins = [
         {
