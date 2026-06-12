49ec027d858791d66ed68bcaa4df8cd50ab002b8
Austin Horstman <khaneliman12@gmail.com>
2026-06-11 22:23:34 -0500

    fix(completion): gate blink satellites

--- modules/nixvim/plugins/blink/community-plugins.nix
+++ modules/nixvim/plugins/blink/community-plugins.nix
@@ -1,12 +1,13 @@
 { config, ... }:
 let
+  blinkEnabled = config.khanelivim.completion.tool == "blink";
   mkBlinkPlugin =
     {
       enable ? true,
       ...
     }@args:
     {
-      inherit enable;
+      enable = blinkEnabled && enable;
       lazyLoad.settings.event = [
         "InsertEnter"
         "CmdlineEnter"
