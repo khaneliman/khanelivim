5d6587aafbee7352ef7aae53eab0d74ee1ab9168
Austin Horstman <khaneliman12@gmail.com>
2026-07-09 12:55:35 -0500

    chore(flake): lock update

--- overlays/neovim-nightly.nix
+++ overlays/neovim-nightly.nix
@@ -5,9 +5,20 @@ _final: prev: {
   # https://github.com/NixOS/nixpkgs/commit/4c1fb8659a (0.12.3 bump removing it).
   neovim-unwrapped =
     flake.inputs.neovim-nightly-overlay.packages.${prev.stdenv.system}.default.overrideAttrs
-      (old: {
-        patches = builtins.filter (patch: !prev.lib.hasInfix "CVE-2026-11487" (toString patch)) (
-          old.patches or [ ]
-        );
-      });
+      (
+        old:
+        {
+          patches = builtins.filter (patch: !prev.lib.hasInfix "CVE-2026-11487" (toString patch)) (
+            old.patches or [ ]
+          );
+        }
+        // prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin {
+          postPatch = (old.postPatch or "") + ''
+            substituteInPlace test/functional/testnvim.lua \
+              --replace-fail \
+                "table.insert(args, _G._nvim_test_id)" \
+                "table.insert(args, './' .. _G._nvim_test_id)"
+          '';
+        }
+      );
 }
