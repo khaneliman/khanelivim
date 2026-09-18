e75100cb8adcf86fcf0404328fbe1ec9787a6592
Austin Horstman <khaneliman12@gmail.com>
2026-09-17 22:04:01 -0500

    chore(nixvim): pin plugin advice patch
    Apply PR 4539 at its reviewed commit so nixpkgs plugin initialization can be enabled without changing the locked Nixvim input.

--- patches/nixvim/default.nix
+++ patches/nixvim/default.nix
@@ -1,4 +1,10 @@
 _: [
+  # https://github.com/nix-community/nixvim/pull/4539
+  # Remove after the locked Nixvim revision contains this commit.
+  {
+    url = "https://github.com/nix-community/nixvim/commit/2e0ceb15cea0c2e49bd29b501212b631743e330e.patch";
+    hash = "sha256-8xNjDn3Te8zH+QSJHMaIN3vjGISd0SdVDxTRis0PyZQ=";
+  }
   # https://github.com/nix-community/nixvim/pull/4542
   # Remove after the locked Nixvim revision contains this commit.
   {
