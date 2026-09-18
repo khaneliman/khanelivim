78e99c64dc7718d30022ac64c8d4c14e2ed155d6
Austin Horstman <khaneliman12@gmail.com>
2026-09-18 09:19:23 -0500

    chore(nixvim): drop merged lsp patch
    Update Nixvim to the merge of upstream PR #4542 and remove its native LSP feature patch. Retain the plugin-advised Lua patch for open PR #4539.

--- patches/nixvim/default.nix
+++ patches/nixvim/default.nix
@@ -5,10 +5,4 @@ _: [
     url = "https://github.com/nix-community/nixvim/commit/2e0ceb15cea0c2e49bd29b501212b631743e330e.patch";
     hash = "sha256-8xNjDn3Te8zH+QSJHMaIN3vjGISd0SdVDxTRis0PyZQ=";
   }
-  # https://github.com/nix-community/nixvim/pull/4542
-  # Remove after the locked Nixvim revision contains this commit.
-  {
-    url = "https://github.com/nix-community/nixvim/commit/249393b931d00ee7096dab1e0e7f33bb0a26a1d2.patch";
-    hash = "sha256-+YcyKUlyb5wMTjv7Ggtzx4XjGAddtltqBI5j+/K8NTs=";
-  }
 ]
